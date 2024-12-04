5846350d256084106302ef435e3956cb450af127
Thomas <thomas@newtomsoft.fr>
2024-12-04 01:00:21 +0100

    wip

---
+++ Puzzles/Skyscrapers/SkyscrapersGame.py
@@ -0,0 +1,131 @@
+﻿from z3 import Solver, sat, Int, Distinct, Implies, And, Not, Or
+
+from Utils.Grid import Grid
+
+
+class SkyscrapersGame:
+    _no_value = 0
+
+    def __init__(self, params: (Grid, dict[str, list[int]])):
+        self._grid: Grid = params[0]
+        self.viewable_skyscrapers: dict[str, list[int]] = params[1]
+        self.rows_number = self._grid.rows_number
+        self.columns_number = self._grid.columns_number
+        self.biggest_skyscraper_levels = self.columns_number
+        if self.rows_number != self.columns_number:
+            raise ValueError("The grid must be square")
+        if self.rows_number < 4:
+            raise ValueError("The grid must be at least 4x4")
+        self.by_east_viewable_skyscrapers = self.viewable_skyscrapers['by_east']
+        if len(self.by_east_viewable_skyscrapers) != self.columns_number:
+            raise ValueError("The 'by_east' viewable skyscrapers list must have the same length as the rows number")
+        self.by_west_viewable_skyscrapers = self.viewable_skyscrapers['by_west']
+        if len(self.by_west_viewable_skyscrapers) != self.columns_number:
+            raise ValueError("The 'by_west' viewable skyscrapers list must have the same length as the rows number")
+        self.by_north_viewable_skyscrapers = self.viewable_skyscrapers['by_north']
+        if len(self.by_north_viewable_skyscrapers) != self.rows_number:
+            raise ValueError("The 'by_north' viewable skyscrapers list must have the same length as the columns number")
+        self.by_south_viewable_skyscrapers = self.viewable_skyscrapers['by_south']
+        if len(self.by_south_viewable_skyscrapers) != self.rows_number:
+            raise ValueError("The 'by_south' viewable skyscrapers list must have the same length as the columns number")
+        self._solver = None
+        self._grid_z3: Grid = Grid.empty()
+        self._last_solution_grid = None
+
+    def _init_solver(self):
+        self._matrix_z3 = [[Int(f"grid{r}_{c}") for c in range(self.columns_number)] for r in range(self.rows_number)]
+        self._grid_z3 = Grid(self._matrix_z3)
+        self._solver = Solver()
+        self._add_constraints()
+
+    def get_solution(self) -> Grid | None:
+        if self._solver is None:
+            self._init_solver()
+        if self._solver.check() != sat:
+            return Grid.empty()
+        model = self._solver.model()
+        grid = Grid([[model.eval(self._matrix_z3[i][j]).as_long() for j in range(self.columns_number)] for i in range(self.rows_number)])
+        self._last_solution_grid = grid
+        return grid
+
+    def get_other_solution(self):
+        self._exclude_solution(self._last_solution_grid)
+        solution = self.get_solution()
+        return solution
+
+    def _exclude_solution(self, solution_grid: Grid):
+        exclude_constraint = Not(And([self._matrix_z3[r][c] == solution_grid.value(r, c) for r in range(self.rows_number) for c in range(self.columns_number) if solution_grid.value(r, c)]))
+        self._solver.add(exclude_constraint)
+
+    def _add_constraints(self):
+        self._add_initials_levels_constraint()
+        self._add_range_levels_constraint()
+        self._add_distinct_level_constraint()
+        self._add_viewable_skyscrapers_constraint()
+
+    def level(self, position):
+        return self._grid_z3[position]
+
+    def _add_initials_levels_constraint(self):
+        for position, level_value in self._grid:
+            if level_value != self._no_value:
+                self._solver.add(self.level(position) == level_value)
+
+    def _add_range_levels_constraint(self):
+        for position, level_value in self._grid:
+            self._solver.add(self.level(position) >= 1)
+            self._solver.add(self.level(position) <= self.columns_number)
+
+    def _add_distinct_level_constraint(self):
+        constraints = []
+        for row in self._grid_z3.matrix:
+            constraints.append(Distinct([row[j] for j in range(self.columns_number)]))
+        for column in zip(*self._grid_z3.matrix):
+            constraints.append(Distinct([column[j] for j in range(self.rows_number)]))
+        self._solver.add(constraints)
+
+    def _add_viewable_skyscrapers_constraint(self):
+        for r, row in enumerate(self._grid_z3.matrix):
+            self._add_viewable_skyscrapers_constraint_for_this_view(row[::-1], self.by_east_viewable_skyscrapers[r])
+            self._add_viewable_skyscrapers_constraint_for_this_view(row, self.by_west_viewable_skyscrapers[r])
+            pass
+
+        for c, column in enumerate(zip(*self._grid_z3.matrix)):
+            self._add_viewable_skyscrapers_constraint_for_this_view(column, self.by_north_viewable_skyscrapers[c])
+            self._add_viewable_skyscrapers_constraint_for_this_view(column[::-1], self.by_south_viewable_skyscrapers[c])
+
+    def _add_viewable_skyscrapers_constraint_for_this_view(self, line, viewable_skyscrapers: int):
+        if viewable_skyscrapers == 0:
+            return
+
+        if viewable_skyscrapers == 1:
+            self._solver.add(line[0] == self.biggest_skyscraper_levels)
+            return
+
+        if viewable_skyscrapers == self.biggest_skyscraper_levels:
+            self._solver.add([line[i] == i + 1 for i in range(self.columns_number)])
+            return
+
+        if viewable_skyscrapers == 2:
+            self._solver.add(line[0] != self.biggest_skyscraper_levels)
+            constraints = set()
+            first_to_or = line[1] == self.biggest_skyscraper_levels
+            for i in range(2, self.columns_number):
+                constraints.add(And([line[i] == self.biggest_skyscraper_levels, And([line[0] > line[j] for j in range(1, i)])]))
+            constraints.add(first_to_or)
+            self._solver.add(Or(constraints))
+            return
+
+        if viewable_skyscrapers == self.biggest_skyscraper_levels - 1:
+            self._solver.add([line[i] != self.biggest_skyscraper_levels for i in range(2)])
+            self._solver.add(Implies(line[-2] == self.biggest_skyscraper_levels, And([line[i + 1] > line[i] for i in range(self.columns_number - 2)])))
+            self._solver.add(Implies(line[-1] == self.biggest_skyscraper_levels, self._all_increase_except_one(line[:-1])))
+            return
+
+    def _all_increase_except_one(self, line):
+        constraints = set()
+        for i_less in range(len(line) - 1):
+            constraint_to_and = [line[i] < line[i + 1] for i in range(self.columns_number - 2) if i != i_less]
+            constraint_to_and.append(line[i_less] > line[i_less + 1])
+            constraints.add(And(constraint_to_and))
+        return Or(constraints)
---
+++ Puzzles/Skyscrapers/tests/SkyscrapersGame_tests.py
@@ -0,0 +1,257 @@
+﻿import unittest
+from unittest import TestCase
+
+from Puzzles.Skyscrapers.SkyscrapersGame import SkyscrapersGame
+from Utils.Grid import Grid
+
+
+class SkyscrapersGameTests(TestCase):
+    def test_grid_must_be_square_raises_value_error(self):
+        grid = Grid([
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+        ])
+        viewable_skyscrapers = {'by_east': [1, 2, 2, 2, 0, 0, 0], 'by_west': [4, 1, 2, 3, 0, 0, 0], 'by_south': [3, 2, 1, 4, 0], 'by_north': [2, 2, 2, 1, 0]}
+        with self.assertRaises(ValueError) as context:
+            SkyscrapersGame((grid, viewable_skyscrapers))
+        self.assertEqual(str(context.exception), "The grid must be square")
+
+    def test_grid_must_be_at_least_4x4_raises_value_error(self):
+        grid = Grid([
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+        ])
+        viewable_skyscrapers = {'by_east': [1, 2, 2], 'by_west': [4, 1, 2], 'by_south': [3, 2, 1], 'by_north': [2, 2, 2]}
+        with self.assertRaises(ValueError) as context:
+            SkyscrapersGame((grid, viewable_skyscrapers))
+        self.assertEqual(str(context.exception), "The grid must be at least 4x4")
+
+    def test_viewable_by_east_skyscrapers_not_compliant(self):
+        grid = Grid([
+            [1, 2, 3, 4],
+            [4, 1, 2, 3],
+            [3, 4, 1, 2],
+            [2, 3, 4, 1],
+        ])
+        viewable_skyscrapers = {'by_east': [1, 2, 2], 'by_west': [4, 1, 2, 3], 'by_south': [3, 2, 1, 4], 'by_north': [2, 2, 2, 1]}
+        with self.assertRaises(ValueError) as context:
+            SkyscrapersGame((grid, viewable_skyscrapers))
+        self.assertEqual(str(context.exception), "The 'by_east' viewable skyscrapers list must have the same length as the rows number")
+
+    def test_viewable_by_west_skyscrapers_not_compliant(self):
+        grid = Grid([
+            [1, 2, 3, 4],
+            [4, 1, 2, 3],
+            [3, 4, 1, 2],
+            [2, 3, 4, 1],
+        ])
+        viewable_skyscrapers = {'by_east': [1, 2, 2, 2], 'by_west': [4, 1, 2], 'by_south': [3, 2, 1, 4], 'by_north': [2, 2, 2, 1]}
+        with self.assertRaises(ValueError) as context:
+            SkyscrapersGame((grid, viewable_skyscrapers))
+        self.assertEqual(str(context.exception), "The 'by_west' viewable skyscrapers list must have the same length as the rows number")
+
+    def test_viewable_by_north_skyscrapers_not_compliant(self):
+        grid = Grid([
+            [1, 2, 3, 4],
+            [4, 1, 2, 3],
+            [3, 4, 1, 2],
+            [2, 3, 4, 1],
+        ])
+        viewable_skyscrapers = {'by_east': [1, 2, 2, 2], 'by_west': [4, 1, 2, 3], 'by_south': [3, 2, 1, 4], 'by_north': [2, 2, 2]}
+        with self.assertRaises(ValueError) as context:
+            SkyscrapersGame((grid, viewable_skyscrapers))
+        self.assertEqual(str(context.exception), "The 'by_north' viewable skyscrapers list must have the same length as the columns number")
+
+    def test_viewable_by_south_skyscrapers_not_compliant(self):
+        grid = Grid([
+            [1, 2, 3, 4],
+            [4, 1, 2, 3],
+            [3, 4, 1, 2],
+            [2, 3, 4, 1],
+        ])
+        viewable_skyscrapers = {'by_east': [1, 2, 2, 2], 'by_west': [4, 1, 2, 3], 'by_south': [3, 2, 1], 'by_north': [2, 2, 2, 1]}
+        with self.assertRaises(ValueError) as context:
+            SkyscrapersGame((grid, viewable_skyscrapers))
+        self.assertEqual(str(context.exception), "The 'by_south' viewable skyscrapers list must have the same length as the columns number")
+
+    def test_solution_with_initials_levels_values(self):
+        grid = Grid([
+            [1, 2, 3, 5],
+            [4, 1, 2, 3],
+            [3, 4, 1, 2],
+            [2, 3, 4, 1],
+        ])
+        viewable_skyscrapers = {'by_east': [1, 2, 2, 2], 'by_west': [4, 1, 2, 3], 'by_south': [3, 2, 1, 4], 'by_north': [2, 2, 2, 1]}
+        skyscrapers_game = SkyscrapersGame((grid, viewable_skyscrapers))
+        solution = skyscrapers_game.get_solution()
+        self.assertEqual(Grid.empty(), solution)
+
+    def test_solution_with_distinct_constraints(self):
+        grid = Grid([
+            [1, 2, 3, 0],
+            [0, 1, 2, 0],
+            [0, 0, 1, 2],
+            [0, 0, 0, 1],
+        ])
+        expected_solution = Grid([
+            [1, 2, 3, 4],
+            [4, 1, 2, 3],
+            [3, 4, 1, 2],
+            [2, 3, 4, 1],
+        ])
+        viewable_skyscrapers = {'by_west': [0, 0, 0, 0], 'by_east': [0, 0, 0, 0], 'by_north': [0, 0, 0, 0], 'by_south': [0, 0, 0, 0]}
+        skyscrapers_game = SkyscrapersGame((grid, viewable_skyscrapers))
+        solution = skyscrapers_game.get_solution()
+        self.assertEqual(expected_solution, solution)
+        other_solution = skyscrapers_game.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+    def test_solution_4x4_with_viewable_skyscrapers_contraint_4(self):
+        grid = Grid([
+            [0, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+        ])
+        expected_solution = Grid([
+            [1, 2, 3, 4],
+            [2, 1, 4, 3],
+            [3, 4, 1, 2],
+            [4, 3, 2, 1],
+        ])
+        viewable_skyscrapers = {'by_west': [4, 0, 0, 0], 'by_east': [0, 0, 0, 0], 'by_north': [4, 0, 0, 0], 'by_south': [0, 0, 0, 4]}
+        skyscrapers_game = SkyscrapersGame((grid, viewable_skyscrapers))
+        solution = skyscrapers_game.get_solution()
+        self.assertEqual(expected_solution, solution)
+        other_solution = skyscrapers_game.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+    def test_solution_4x4_with_viewable_skyscrapers_contraint_1(self):
+        grid = Grid([
+            [0, 0, 0, 0],
+            [0, 2, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 0],
+        ])
+        expected_solution = Grid([
+            [3, 4, 2, 1],
+            [1, 2, 3, 4],
+            [4, 3, 1, 2],
+            [2, 1, 4, 3],
+        ])
+        viewable_skyscrapers = {'by_west': [0, 0, 0, 0], 'by_east': [0, 1, 0, 0], 'by_north': [0, 1, 0, 0], 'by_south': [0, 0, 0, 0]}
+        skyscrapers_game = SkyscrapersGame((grid, viewable_skyscrapers))
+        solution = skyscrapers_game.get_solution()
+        self.assertEqual(expected_solution, solution)
+        other_solution = skyscrapers_game.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+    def test_solution_4x4_with_viewable_skyscrapers_3(self):
+        grid = Grid([
+            [0, 0, 0, 1],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+        ])
+        expected_solution = Grid([
+            [3, 4, 2, 1],
+            [1, 2, 4, 3],
+            [4, 1, 3, 2],
+            [2, 3, 1, 4],
+        ])
+        viewable_skyscrapers = {'by_west': [0, 3, 0, 3], 'by_east': [0, 0, 0, 0], 'by_north': [0, 0, 0, 0], 'by_south': [0, 0, 3, 0]}
+        skyscrapers_game = SkyscrapersGame((grid, viewable_skyscrapers))
+        solution = skyscrapers_game.get_solution()
+        self.assertEqual(expected_solution, solution)
+        other_solution = skyscrapers_game.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+    def test_solution_4x4_with_viewable_skyscrapers_3_and1(self):
+        grid = Grid([
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 1],
+            [0, 1, 0, 0],
+        ])
+        expected_solution = Grid([
+            [3, 2, 1, 4],
+            [1, 4, 3, 2],
+            [2, 3, 4, 1],
+            [4, 1, 2, 3],
+        ])
+        viewable_skyscrapers = {'by_west': [0, 0, 3, 0], 'by_east': [0, 3, 0, 0], 'by_north': [0, 0, 0, 1], 'by_south': [0, 0, 0, 0]}
+        skyscrapers_game = SkyscrapersGame((grid, viewable_skyscrapers))
+        solution = skyscrapers_game.get_solution()
+        self.assertEqual(expected_solution, solution)
+        other_solution = skyscrapers_game.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+    def test_solution_4x4_with_viewable_skyscrapers_2(self):
+        grid = Grid([
+            [0, 3, 0, 0],
+            [2, 0, 0, 0],
+            [0, 0, 3, 2],
+            [3, 0, 0, 0],
+        ])
+        expected_solution = Grid([
+            [4, 3, 2, 1],
+            [2, 1, 4, 3],
+            [1, 4, 3, 2],
+            [3, 2, 1, 4],
+        ])
+        viewable_skyscrapers = {'by_west': [0, 0, 2, 0], 'by_east': [0, 0, 0, 0], 'by_north': [0, 0, 0, 0], 'by_south': [0, 0, 0, 0]}
+        skyscrapers_game = SkyscrapersGame((grid, viewable_skyscrapers))
+        solution = skyscrapers_game.get_solution()
+        self.assertEqual(expected_solution, solution)
+        other_solution = skyscrapers_game.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+    def test_solution_with_viewable_skyscrapers_2_old(self):
+        grid = Grid([
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+        ])
+        expected_solution = Grid([
+            [3, 4, 1, 2],
+            [1, 2, 4, 3],
+            [4, 3, 2, 1],
+            [2, 1, 3, 4],
+        ])
+        viewable_skyscrapers = {'by_west': [0, 3, 0, 0], 'by_east': [0, 0, 4, 0], 'by_north': [0, 0, 0, 0], 'by_south': [0, 0, 2, 0]}
+        skyscrapers_game = SkyscrapersGame((grid, viewable_skyscrapers))
+        solution = skyscrapers_game.get_solution()
+        self.assertEqual(expected_solution, solution)
+        other_solution = skyscrapers_game.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+    def test_solution_4x4(self):
+        grid = Grid([
+            [1, 0, 0, 2],
+            [0, 0, 0, 4],
+            [0, 0, 0, 1],
+            [0, 4, 1, 3],
+        ])
+        expected_solution = Grid([
+            [1, 3, 4, 2],
+            [3, 1, 2, 4],
+            [4, 2, 3, 1],
+            [2, 4, 1, 3],
+        ])
+        viewable_skyscrapers = {'by_west': [0, 0, 0, 0], 'by_east': [0, 0, 0, 2], 'by_north': [3, 0, 0, 2], 'by_south': [2, 0, 3, 0]}
+        skyscrapers_game = SkyscrapersGame((grid, viewable_skyscrapers))
+        solution = skyscrapers_game.get_solution()
+        self.assertEqual(expected_solution, solution)
+        other_solution = skyscrapers_game.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+
+if __name__ == '__main__':
+    unittest.main()
