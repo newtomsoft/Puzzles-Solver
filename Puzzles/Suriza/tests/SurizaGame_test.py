e272c98b9a247651efe2209f1f66a52b70e6326c
Thomas <thomas@newtomsoft.fr>
2025-01-23 17:49:44 +0100

    add some suriza tests

--- Puzzles/Suriza/tests/SurizaGame_test.py
+++ Puzzles/Suriza/tests/SurizaGame_test.py
@@ -13,7 +13,6 @@ class SurizaGameTests(TestCase):
             [' ', 3],
             [0, ' '],
         ])
-
         game = SurizaGame(grid)
         solution = game.get_solution()
         expected_solution_string = (
@@ -34,7 +33,6 @@ class SurizaGameTests(TestCase):
             [3, ' '],
             [1, ' '],
         ])
-
         game = SurizaGame(grid)
         solution = game.get_solution()
         expected_solution_string = (
@@ -53,13 +51,135 @@ class SurizaGameTests(TestCase):
             [2, 2],
             [2, 2]
         ])
-
         game = SurizaGame(grid)
         solution = game.get_solution()
         expected_solution_string = (
             ' ┌─────┐ \n'
             ' │     │ \n'
             ' └─────┘ '
+        )
+        self.assertEqual(expected_solution_string, str(solution))
+        other_solution = game.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+    def test_solution_6x6(self):
+        grid = Grid([
+            [' ', ' ', ' ', ' ', 2],
+            [' ', ' ', ' ', 2, ' '],
+            [' ', ' ', 3, 2, ' '],
+            [' ', ' ', 0, ' ', ' '],
+            [' ', 3, 3, ' ', 3]
+        ])
+        game = SurizaGame(grid)
+        solution = game.get_solution()
+        expected_solution_string = (
+            ' ┌──────────────┐ \n'
+            ' │  ┌────────┐  │ \n'
+            ' │  │  ┌──┐  └──┘ \n'
+            ' │  └──┘  └─────┐ \n'
+            ' │  ┌──┐  ┌──┐  │ \n'
+            ' └──┘  └──┘  └──┘ '
+        )
+        self.assertEqual(expected_solution_string, str(solution))
+        other_solution = game.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+    def test_solution_8x8(self):
+        grid = Grid([
+            [' ', ' ', 3, ' ', ' ', 2, ' '],
+            [2, 1, 3, ' ', 3, ' ', 3],
+            [2, 2, ' ', 2, ' ', ' ', 3],
+            [' ', ' ', ' ', ' ', ' ', ' ', 3],
+            [' ', ' ', ' ', 2, 3, ' ', ' '],
+            [2, ' ', 2, 1, 2, 2, ' '],
+            [' ', 3, 3, ' ', 3, 2, 3]
+        ])
+        game = SurizaGame(grid)
+        solution = game.get_solution()
+        expected_solution_string = (
+            ' ┌────────┐     ┌─────┐ \n'
+            ' │     ┌──┘  ┌──┘  ┌──┘ \n'
+            ' └──┐  └──┐  └──┐  └──┐ \n'
+            '    └──┐  └─────┘  ┌──┘ \n'
+            ' ┌──┐  └──┐  ┌──┐  └──┐ \n'
+            ' │  └─────┘  │  └──┐  │ \n'
+            ' └──┐  ┌──┐  └──┐  │  │ \n'
+            '    └──┘  └─────┘  └──┘ '
+        )
+        self.assertEqual(expected_solution_string, str(solution))
+        other_solution = game.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+    def test_solution_11x11(self):
+        grid = Grid([
+            [' ', 2, 3, ' ', ' ', 1, ' ', 1, ' ', 3],
+            [' ', 3, ' ', 2, ' ', 3, ' ', ' ', 3, ' '],
+            [2, 1, 1, ' ', ' ', ' ', ' ', 2, ' ', ' '],
+            [' ', 2, ' ', ' ', ' ', ' ', ' ', ' ', 1, 3],
+            [' ', 1, ' ', 3, ' ', 3, 0, ' ', ' ', 3],
+            [3, ' ', ' ', ' ', ' ', ' ', ' ', 2, ' ', ' '],
+            [' ', 1, ' ', ' ', 1, 3, ' ', ' ', 3, ' '],
+            [' ', 3, ' ', ' ', 2, 2, ' ', ' ', ' ', 3],
+            [1, ' ', ' ', ' ', ' ', ' ', ' ', ' ', 3, 2],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', 2, ' ', ' ']
+        ])
+        game = SurizaGame(grid)
+        solution = game.get_solution()
+        expected_solution_string = (
+            ' ┌──┐  ┌──┐  ┌─────────────────┐ \n'
+            ' │  │  │  │  └──┐  ┌──┐  ┌─────┘ \n'
+            ' │  └──┘  └──┐  └──┘  │  └──┐    \n'
+            ' └──┐  ┌──┐  └─────┐  └──┐  └──┐ \n'
+            ' ┌──┘  │  └──┐  ┌──┘     │  ┌──┘ \n'
+            ' └──┐  │  ┌──┘  └──┐  ┌──┘  └──┐ \n'
+            ' ┌──┘  │  │     ┌──┘  │  ┌─────┘ \n'
+            ' └──┐  │  └──┐  └──┐  │  └─────┐ \n'
+            '    └──┘     └──┐  └──┘  ┌─────┘ \n'
+            ' ┌──────────────┘  ┌──┐  └─────┐ \n'
+            ' └─────────────────┘  └────────┘ '
+        )
+        self.assertEqual(expected_solution_string, str(solution))
+        other_solution = game.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+    @unittest.skip("This test is too slow (approx. 8 seconds)")
+    def test_solution_16x16(self):
+        grid = Grid([
+            [' ', ' ', 2, 3, ' ', 2, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', 2, ' ', 2, 2, 3, 3, 3, 2, ' ', ' ', 3, ' ', ' '],
+            [' ', ' ', 2, ' ', 3, ' ', ' ', ' ', ' ', 1, ' ', ' ', 2, 2, 2],
+            [3, ' ', 3, 2, ' ', 0, 2, ' ', ' ', 3, 2, 2, ' ', 1, ' '],
+            [' ', 2, ' ', ' ', ' ', 3, ' ', ' ', 2, ' ', ' ', 1, 3, 2, ' '],
+            [' ', ' ', 2, ' ', ' ', 2, ' ', 1, ' ', ' ', ' ', ' ', 2, ' ', ' '],
+            [' ', ' ', ' ', ' ', 2, ' ', 3, ' ', 3, 1, 2, ' ', ' ', ' ', 1],
+            [2, 1, ' ', 2, ' ', 1, ' ', ' ', 2, 3, 1, ' ', 1, 3, ' '],
+            [3, 1, ' ', 1, 2, 2, 3, ' ', ' ', ' ', ' ', ' ', 3, ' ', ' '],
+            [' ', ' ', ' ', ' ', 2, 3, 2, 1, ' ', ' ', ' ', ' ', ' ', 2, 2],
+            [' ', ' ', 2, 3, ' ', ' ', 2, 2, ' ', 2, ' ', 2, ' ', 2, 1],
+            [3, ' ', ' ', ' ', ' ', 2, ' ', 1, ' ', ' ', 2, 3, ' ', 3, ' '],
+            [3, 0, ' ', 3, ' ', 2, ' ', 3, 3, 3, 2, ' ', ' ', ' ', 1],
+            [3, 2, 2, 2, ' ', ' ', 3, ' ', ' ', ' ', ' ', 3, ' ', ' ', 2],
+            [' ', ' ', ' ', ' ', 2, ' ', ' ', 3, ' ', 3, ' ', 2, 2, ' ', 3]
+        ])
+        game = SurizaGame(grid)
+        solution = game.get_solution()
+        expected_solution_string = (
+            ' ┌─────┐  ┌──┐  ┌──────────────┐  ┌───────────┐ \n'
+            ' │  ┌──┘  │  │  │  ┌──┐  ┌──┐  │  │  ┌─────┐  │ \n'
+            ' │  └─────┘  │  │  │  └──┘  │  └──┘  └──┐  └──┘ \n'
+            ' └──┐  ┌──┐  └──┘  └─────┐  │  ┌─────┐  └─────┐ \n'
+            ' ┌──┘  │  └─────┐  ┌──┐  │  └──┘     └──┐  ┌──┘ \n'
+            ' │  ┌──┘  ┌──┐  └──┘  │  └──┐  ┌────────┘  └──┐ \n'
+            ' │  │  ┌──┘  └─────┐  │  ┌──┘  └──┐  ┌────────┘ \n'
+            ' └──┘  └──┐  ┌──┐  └──┘  └─────┐  └──┘  ┌──┐    \n'
+            ' ┌────────┘  │  │  ┌──┐  ┌─────┘  ┌──┐  │  └──┐ \n'
+            ' └──┐  ┌──┐  │  │  │  │  └─────┐  │  └──┘  ┌──┘ \n'
+            ' ┌──┘  │  │  │  └──┘  │  ┌─────┘  └──┐  ┌──┘    \n'
+            ' └──┐  │  └──┘     ┌──┘  └────────┐  │  │  ┌──┐ \n'
+            ' ┌──┘  └─────┐  ┌──┘  ┌──┐  ┌──┐  └──┘  └──┘  │ \n'
+            ' └──┐  ┌─────┘  │  ┌──┘  └──┘  └─────┐  ┌──┐  │ \n'
+            ' ┌──┘  │  ┌─────┘  └─────┐  ┌────────┘  │  │  │ \n'
+            ' └─────┘  └──────────────┘  └───────────┘  └──┘ '
         )
         self.assertEqual(expected_solution_string, str(solution))
         other_solution = game.get_other_solution()
