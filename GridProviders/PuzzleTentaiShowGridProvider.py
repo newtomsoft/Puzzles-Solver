9baa68d9933952fb596effb9e7ae1c114c578f06
Thomas <thomas@newtomsoft.fr>
2025-01-23 19:24:06 +0100

    Tentai show wip

---
+++ GridProviders/PuzzleTentaiShowGridProvider.py
@@ -0,0 +1,14 @@
+﻿from playwright.sync_api import BrowserContext
+
+from GridProviders.GridProvider import GridProvider
+from GridProviders.PlaywrightGridProvider import PlaywrightGridProvider
+from GridProviders.PuzzlesMobileGridProvider import PuzzlesMobileGridProvider
+
+
+# also known as Galaxies
+class PuzzleTentaiShowGridProvider(GridProvider, PlaywrightGridProvider, PuzzlesMobileGridProvider):
+    def get_grid(self, url: str):
+        return self.with_playwright(self.scrap_grid, url)
+
+    def scrap_grid(self, browser: BrowserContext, url):
+        pass
