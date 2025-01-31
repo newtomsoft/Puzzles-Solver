8fd70a450eac25567fcbbcac30ab4c5a7148aaf5
Thomas <thomas@newtomsoft.fr>
2025-01-31 22:17:24 +0100

    Minesweeper

--- GridPlayers/PuzzleMinesweeperGridPlayer.py
+++ GridPlayers/PuzzleMinesweeperGridPlayer.py
@@ -6,13 +6,14 @@ from GridPlayers.GridPlayer import GridPlayer
 from GridPlayers.PuzzlesMobileGridPlayer import PuzzlesMobileGridPlayer
 
 
-class PuzzlePuzzleHitoriGridPlayerGridPlayer(GridPlayer, PuzzlesMobileGridPlayer):
+class PuzzleMinesweeperGridPlayer(GridPlayer, PuzzlesMobileGridPlayer):
     @classmethod
     def play(cls, solution, browser: BrowserContext):
         page = browser.pages[0]
         cells = page.query_selector_all("div.cell.selectable")
         for index, _ in [(solution.get_index(position), value) for position, value in solution if not value]:
-            cells[index].click()
+            cells[index].click(button="right")
 
+        sleep(2)
         cls.submit_score(page)
         sleep(60)
