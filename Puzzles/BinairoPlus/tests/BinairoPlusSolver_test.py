7333b45a919fc5342568ba02fa82f64a2a180178
Thomas <thomas@newtomsoft.fr>
2025-03-08 14:07:31 +0100

    Binairo/Binairo+ refactoring

--- Puzzles/BinairoPlus/tests/BinairoPlusSolver_test.py
+++ Puzzles/BinairoPlus/tests/BinairoPlusSolver_test.py
@@ -4,6 +4,7 @@ from unittest import TestCase
 from Puzzles.BinairoPlus.BinairoPlusSolver import BinairoPlusSolver
 from SolverEngineAdapters.Z3SolverEngine import Z3SolverEngine
 from Utils.Grid import Grid
+from Utils.Position import Position
 
 
 class BinairoPlusSolverTests(TestCase):
@@ -21,9 +22,9 @@ class BinairoPlusSolverTests(TestCase):
             [1, -1, 1, 0, -1, 1],
         ])
         comparison_operators = {
-            'equal_on_columns': [(2, 0), (3, 4)],
-            'equal_on_rows': [(1, 1), (1, 3)],
-            'non_equal_on_columns': [(0, 2), (0, 3), (2, 5), (3, 1)],
+            'equal_on_columns': [Position(2, 0), Position(3, 4)],
+            'equal_on_rows': [Position(1, 1), Position(1, 3)],
+            'non_equal_on_columns': [Position(0, 2), Position(0, 3), Position(2, 5), Position(3, 1)],
             'non_equal_on_rows': []
         }
         expected_grid = Grid([
