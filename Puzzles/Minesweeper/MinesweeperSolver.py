216d2613bc9008ed0030a4ef1695c016e6fca071
Thomas <thomas@newtomsoft.fr>
2025-03-11 18:18:32 +0100

    improve Minesweeper

--- Puzzles/Minesweeper/MinesweeperSolver.py
+++ Puzzles/Minesweeper/MinesweeperSolver.py
@@ -10,15 +10,23 @@ class MinesweeperSolver(GameSolver):
         self.columns_number = self._grid.columns_number
         self._solver = solver_engine
         self._grid_z3: Grid | None = None
-        self._solver = solver_engine
+        self._previous_solution: Grid | None = None
 
-    def get_solution(self) -> (Grid | None, int):
+    def get_solution(self) -> (Grid, int):
         self._grid_z3 = Grid([[self._solver.bool(f"grid_{r}_{c}") for c in range(self.columns_number)] for r in range(self.rows_number)])
         self._add_constraints()
+        self._previous_solution = self._compute_solution()
+        return self._previous_solution
+
+    def get_other_solution(self) -> Grid:
+        self._solver.add(self._solver.Not(self._solver.And([self._grid_z3[position] == value for position, value in self._previous_solution])))
+        self._previous_solution = self._compute_solution()
+        return self._previous_solution
+
+    def _compute_solution(self) -> Grid:
         if not self._solver.has_solution():
-            return None
-        grid = Grid([[(self._solver.eval(self._grid_z3.value(i, j))) for j in range(self.columns_number)] for i in range(self.rows_number)])
-        return grid
+            return Grid.empty()
+        return Grid([[(self._solver.eval(self._grid_z3.value(i, j))) for j in range(self.columns_number)] for i in range(self.rows_number)])
 
     def _add_constraints(self):
         self._add_sum_constraints()
@@ -29,5 +37,3 @@ class MinesweeperSolver(GameSolver):
             constraints.append(self._grid_z3[position])
             constraints.append(self._solver.sum([self._solver.Not(value) for value in self._grid_z3.neighbors_values(position, 'diagonal')]) == cell)
         self._solver.add(self._solver.And(constraints))
-
-
