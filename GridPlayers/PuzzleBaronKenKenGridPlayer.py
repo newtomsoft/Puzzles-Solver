2739ced8acb1a5488516f2a15b8374177a64b439
Thomas <thomas@newtomsoft.fr>
2025-03-15 14:16:16 +0100

    KenKen player, tests

---
+++ GridPlayers/PuzzleBaronKenKenGridPlayer.py
@@ -0,0 +1,19 @@
+﻿from time import sleep
+
+from playwright.sync_api import BrowserContext
+
+from GridPlayers.GridPlayer import GridPlayer
+
+
+class PuzzleBaronKenKenGridPlayer(GridPlayer):
+    @classmethod
+    def play(cls, solution, browser: BrowserContext):
+        page = browser.pages[0]
+        grid_box_divs = page.query_selector_all('div.gridbox')
+
+        for position, digit in solution:
+            index = solution.get_index(position)
+            grid_box_divs[index].click()
+            page.keyboard.press(str(digit))
+
+        sleep(20)
