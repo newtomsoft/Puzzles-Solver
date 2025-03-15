5282b4338bde8f3d802dbf94b1d4be12f3169541
Thomas <thomas@newtomsoft.fr>
2025-03-16 00:08:02 +0100

    Star Battle Baron & refactoring

---
+++ GridPlayers/PuzzleBaronStarBattleGridPlayer.py
@@ -0,0 +1,18 @@
+﻿from time import sleep
+
+from playwright.sync_api import BrowserContext
+
+from GridPlayers.GridPlayer import GridPlayer
+
+
+class PuzzleBaronStarBattleGridPlayer(GridPlayer):
+    @classmethod
+    def play(cls, solution, browser: BrowserContext):
+        page = browser.pages[0]
+        grid_box_divs = page.query_selector_all('div.box')
+
+        for position, _ in [(position, value) for position, value in solution if value]:
+            index = solution.get_index(position)
+            grid_box_divs[index].click(click_count=2)
+
+        sleep(20)
