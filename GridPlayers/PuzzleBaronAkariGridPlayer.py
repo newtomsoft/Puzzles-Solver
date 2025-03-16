e52d7cb2e9457667112e3a1489c7b7bb7962ec2a
Thomas <thomas@newtomsoft.fr>
2025-03-16 22:06:31 +0100

    PuzzleBaronAkariGridPlayer

---
+++ GridPlayers/PuzzleBaronAkariGridPlayer.py
@@ -0,0 +1,18 @@
+﻿from time import sleep
+
+from playwright.sync_api import BrowserContext
+
+from GridPlayers.GridPlayer import GridPlayer
+
+
+class PuzzleBaronAkariGridPlayer(GridPlayer):
+    @classmethod
+    def play(cls, solution, browser: BrowserContext):
+        page = browser.pages[0]
+        grid_box_divs = page.query_selector_all('div.gridbox')
+
+        for position, _ in [(position, value) for position, value in solution if value]:
+            index = solution.get_index(position)
+            grid_box_divs[index].click()
+
+        sleep(20)
