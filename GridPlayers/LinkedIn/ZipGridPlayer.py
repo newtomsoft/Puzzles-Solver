a785da33a34e48b3ef2a86e8a4350e6da326cb28
Thomas <thomas@newtomsoft.fr>
2025-04-14 18:24:32 +0200

    refactoring video screenshot

--- GridPlayers/LinkedIn/ZipGridPlayer.py
+++ GridPlayers/LinkedIn/ZipGridPlayer.py
@@ -14,7 +14,7 @@ class ZipGridPlayer(PlaywrightGridPlayer):
         game_board = frame.wait_for_selector('.game-board')
         cells_divs = game_board.query_selector_all('div.trail-cell')
 
-        video, x1, x2, y1, y2 = cls.get_data_video(frame, page)
+        video, x1, x2, y1, y2 = cls.get_data_video(frame, page, '.grid-board-wrapper', -12, -23, 22, 28)
 
         for position in solution.path:
             cls.mouse_click(page.mouse, solution, position, cells_divs)
@@ -22,19 +22,3 @@ class ZipGridPlayer(PlaywrightGridPlayer):
         sleep(6)
         browser.close()
         cls.process_video(video, x1, y1, x2, y2)
-
-    @classmethod
-    def get_data_video(cls, frame, page):
-        game_board_wrapper = frame.wait_for_selector('.grid-board-wrapper')
-        bounding_box = game_board_wrapper.bounding_box()
-        x1 = int(bounding_box['x']) - 12
-        y1 = int(bounding_box['y']) - 23
-        x2 = int(bounding_box['width']) + x1 + 22
-        y2 = int(bounding_box['height']) + y1 + 28
-        video = page.video
-        return video, x1, x2, y1, y2
-
-    @classmethod
-    def process_video(cls, video, x1, y1, x2, y2):
-        input_video_path = video.path()
-        cls.crop_video(input_video_path, x1, y1, x2, y2)
