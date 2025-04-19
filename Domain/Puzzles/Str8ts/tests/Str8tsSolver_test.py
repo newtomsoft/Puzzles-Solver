291cb1e0af8f93152cbaefb407f215acb251482f
thomas <thomas@newtomsoft.fr>
2025-04-20 01:05:54 +0200

    Refactor Str8tsSolver constraints and add new tests
    Updated constraints to support black cells with negative values and renamed adjacent constraints to consecutive constraints for clarity. Enhanced solution computation to handle black cells correctly. Added test cases to validate the updated behavior, including distinct constraints and valid solutions.

--- Domain/Puzzles/Str8ts/tests/Str8tsSolver_test.py
+++ Domain/Puzzles/Str8ts/tests/Str8tsSolver_test.py
@@ -72,6 +72,29 @@ class Str8tsSolverTests(TestCase):
         other_solution = game_solver.get_other_solution()
         self.assertEqual(Grid.empty(), other_solution)
 
+    def test_many_black_cell_distinct_constraints(self):
+        numbers_grid = Grid([
+            [1, _, 2],
+            [_, _, _],
+            [3, _, _],
+        ])
+        blacks_grid = Grid([
+            [o, x, o],
+            [o, x, x],
+            [o, o, o],
+        ])
+
+        game_solver = Str8tsSolver(numbers_grid, blacks_grid, self.get_solver_engine())
+        solution = game_solver.get_solution()
+        expected_solution = Grid([
+            [1, _, 2],
+            [2, _, _],
+            [3, 2, 1],
+        ])
+        self.assertEqual(expected_solution, solution)
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
     def test_adjacent_constraints(self):
         numbers_grid = Grid([
             [2, 1, _],
@@ -90,6 +113,35 @@ class Str8tsSolverTests(TestCase):
             [2, 1, _],
             [1, 2, 3],
             [3, _, 2],
+        ])
+        self.assertEqual(expected_solution, solution)
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+    def test_solution(self):
+        numbers_grid = Grid([
+            [_, 3, 4, _, _],
+            [3, _, _, 2, 4],
+            [_, 2, 3, _, _],
+            [_, _, _, 5, _],
+            [5, _, 2, _, 1],
+        ])
+        blacks_grid = Grid([
+            [x, o, o, x, x],
+            [o, o, o, o, o],
+            [o, o, o, o, o],
+            [o, o, o, o, o],
+            [x, x, o, o, x],
+        ])
+
+        game_solver = Str8tsSolver(numbers_grid, blacks_grid, self.get_solver_engine())
+        solution = game_solver.get_solution()
+        expected_solution = Grid([
+            [0, 3, 4, 0, 0],
+            [3, 1, 5, 2, 4],
+            [1, 2, 3, 4, 5],
+            [2, 4, 1, 5, 3],
+            [5, 0, 2, 3, 1],
         ])
         self.assertEqual(expected_solution, solution)
         other_solution = game_solver.get_other_solution()
