53652f052313ac285aeeba7357a32ae8515609d8
thomas <thomas@newtomsoft.fr>
2025-04-20 01:05:54 +0200

    Refactor Str8tsSolver constraints and add new tests
    Updated constraints to support black cells with negative values and renamed adjacent constraints to consecutive constraints for clarity. Enhanced solution computation to handle black cells correctly. Added test cases to validate the updated behavior, including distinct constraints and valid solutions.

--- Domain/Puzzles/Str8ts/Str8tsSolver.py
+++ Domain/Puzzles/Str8ts/Str8tsSolver.py
@@ -22,27 +22,31 @@ class Str8tsSolver:
         return self._previous_solution
 
     def get_other_solution(self) -> Grid:
-        self._solver.add(self._solver.Not(self._solver.And([self._grid_z3[position] == value for position, value in self._previous_solution])))
+        self._solver.add(self._solver.Not(self._solver.And([self._grid_z3[position] == value for position, value in self._previous_solution if value > 0])))
         self._previous_solution = self._compute_solution()
         return self._previous_solution
 
     def _compute_solution(self) -> Grid:
         if not self._solver.has_solution():
             return Grid.empty()
-        return Grid([[(self._solver.eval(self._grid_z3.value(i, j))) for j in range(self._columns_number)] for i in range(self._rows_number)])
+        solution_with_black_negative = Grid([[(self._solver.eval(self._grid_z3.value(i, j))) for j in range(self._columns_number)] for i in range(self._rows_number)])
+        solution = Grid([[max(0, solution_with_black_negative.value(i, j)) for j in range(self._columns_number)] for i in range(self._rows_number)])
+        return solution
 
     def _add_constraints(self):
         self._add_initial_constraints()
         self._add_distinct_constraints()
-        self._add_adjacent_constraints()
+        self._add_consecutive_constraints()
 
     def _add_initial_constraints(self):
         for position, value in [(position, value) for position, value in self._numbers_grid if value > 0]:
             self._solver.add(self._grid_z3[position] == value)
 
+        black_count = 0
         for position, is_black in self._blacks_grid:
             if is_black and self._numbers_grid[position] == 0:
-                self._solver.add(self._grid_z3[position] == 0)
+                black_count += 1
+                self._solver.add(self._grid_z3[position] == -black_count)
             else:
                 self._solver.add(self._grid_z3[position] > 0)
                 self._solver.add(self._grid_z3[position] <= self._rows_number)
@@ -55,7 +59,7 @@ class Str8tsSolver:
             column = list(column_tuple)
             self._solver.add(self._solver.distinct(column))
 
-    def _add_adjacent_constraints(self):
+    def _add_consecutive_constraints(self):
         for column_index, row in enumerate(self._grid_z3.matrix):
             groups = self._build_groups(column_index, row, "row")
             self.add_consecutive_for_groups_constraints(groups)
