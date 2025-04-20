120fc2345bb0049cba5360b9f408b52fa84d40e4
thomas <thomas@newtomsoft.fr>
2025-04-20 16:08:46 +0200

    Add KemaruSolver implementation and tests
    Implemented the KemaruSolver class for solving Kemaru puzzles using constraints and a solver engine. Added comprehensive unit tests to verify functionality, including edge cases (like grids with fewer than two regions) and multiple puzzle difficulty levels.

---
+++ Domain/Puzzles/Kemaru/KemaruSolver.py
@@ -0,0 +1,60 @@
+﻿from Domain.Board.Grid import Grid
+from Domain.Board.Position import Position
+from Domain.Ports.SolverEngine import SolverEngine
+from Domain.Puzzles.GameSolver import GameSolver
+
+
+class KemaruSolver(GameSolver):
+    def __init__(self, grid: Grid, region_grid:Grid, solver_engine: SolverEngine):
+        self._grid = grid
+        self.rows_number = self._grid.rows_number
+        self.columns_number = self._grid.columns_number
+        self._region_grid = region_grid
+        self._regions = self._region_grid.get_regions()
+        if len(self._regions) < 2:
+            raise ValueError("The grid must have at least 2 regions")
+        self._solver = solver_engine
+        self._grid_z3: Grid | None = None
+        self._previous_solution: Grid | None = None
+
+    def get_solution(self) -> Grid:
+        self._grid_z3 = Grid([[self._solver.int(f"grid_{r}_{c}") for c in range(self.columns_number)] for r in range(self.rows_number)])
+        self._add_constrains()
+        self._previous_solution = self._compute_solution()
+        return self._previous_solution
+
+    def get_other_solution(self) -> Grid:
+        self._solver.add(self._solver.Not(self._solver.And([self._grid_z3[position] == value for position, value in self._previous_solution])))
+        self._previous_solution = self._compute_solution()
+        return self._previous_solution
+
+    def _compute_solution(self):
+        if not self._solver.has_solution():
+            return Grid.empty()
+        model = self._solver.model()
+        grid = Grid([[self._solver.eval(model.eval(self._grid_z3[Position(i, j)])) for j in range(self.columns_number)] for i in range(self.rows_number)])
+        return grid
+
+    def _add_constrains(self):
+        self._add_initial_constraints()
+        self._add_regions_distinct_and_max_value_constraints()
+
+    def _add_initial_constraints(self):
+        for grid_z3_value, number_value in [(self._grid_z3[position],number_value) for position, number_value in self._grid]:
+            if number_value > 0:
+                self._solver.add(grid_z3_value == number_value)
+            else:
+                self._solver.add(grid_z3_value >= 1)
+
+    def _add_regions_distinct_and_max_value_constraints(self):
+        for region_positions in self._regions.values():
+            self._solver.add(self._solver.distinct([self._grid_z3[position] for position in region_positions]))
+            for position in region_positions:
+                self._add_max_value_constraints(position, len(region_positions))
+                self._add_neighbors_not_same_value_constraint(position)
+
+    def _add_max_value_constraints(self, position, region_positions_len: int):
+        self._solver.add(self._grid_z3[position] <= region_positions_len)
+
+    def _add_neighbors_not_same_value_constraint(self, position):
+        self._solver.add(self._solver.And([self._grid_z3[neighbor_position] != self._grid_z3[position] for neighbor_position in self._grid.neighbors_positions(position, 'all')]))
---
+++ Domain/Puzzles/Kemaru/tests/KemaruSolver_test.py
@@ -0,0 +1,110 @@
+﻿import unittest
+from unittest import TestCase
+
+from Domain.Puzzles.Kemaru.KemaruSolver import KemaruSolver
+from Domain.Board.Grid import Grid
+from SolverEngineAdapters.Z3SolverEngine import Z3SolverEngine
+
+_ = 0
+
+
+class KemaruSolverTests(TestCase):
+    @staticmethod
+    def get_solver_engine():
+        return Z3SolverEngine()
+
+    def test_grid_must_have_at_least_2_regions_raises_value_error(self):
+        grid = Grid([
+            [_, _, _, _, _, _],
+            [_, _, _, _, _, _],
+            [_, _, _, _, _, _],
+            [_, _, _, _, _, _],
+            [_, _, _, _, _, _],
+            [_, _, _, _, _, _],
+        ])
+        region_grid = Grid([
+            [1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1],
+        ])
+        with self.assertRaises(ValueError) as context:
+            KemaruSolver(grid, region_grid, self.get_solver_engine())
+        self.assertEqual(str(context.exception), "The grid must have at least 2 regions")
+
+    def test_get_solution_4x4_easy(self):
+        grid = Grid([
+            [_, 4, _, 3],
+            [_, _, _, _],
+            [2, _, 4, 2],
+            [_, 1, _, _],
+        ])
+        region_grid = Grid([
+            [1, 2, 2, 2],
+            [3, 4, 4, 2],
+            [3, 3, 4, 4],
+            [3, 3, 3, 4],
+        ])
+        expected_grid = Grid([
+            [1, 4, 2, 3],
+            [5, 3, 5, 1],
+            [2, 6, 4, 2],
+            [4, 1, 3, 1],
+        ])
+        game_solver = KemaruSolver(grid, region_grid, self.get_solver_engine())
+        solution = game_solver.get_solution()
+        self.assertEqual(expected_grid, solution)
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+    def test_get_solution_11x9_hard(self):
+        grid = Grid([
+            [4, _, _, _, 1, 2, _, _, 4],
+            [_, _, 4, _, _, _, _, _, _],
+            [3, _, 1, _, _, _, _, _, _],
+            [_, 4, _, _, 1, _, _, _, 3],
+            [_, _, _, _, _, _, _, _, _],
+            [_, _, _, _, 3, _, _, 5, _],
+            [_, 2, _, _, _, _, _, 2, _],
+            [_, _, _, _, _, 5, _, _, _],
+            [_, _, 5, _, _, _, _, 5, _],
+            [_, _, _, _, 4, _, _, _, _],
+            [2, _, _, _, _, _, _, _, 2],
+        ])
+        region_grid = Grid([
+            [1, 1, 1, 1, 2, 2, 3, 3, 3],
+            [4, 4, 4, 1, 2, 2, 2, 3, 3],
+            [4, 4, 5, 6, 6, 7, 7, 7, 8],
+            [9, 5, 5, 6, 6, 6, 7, 7, 8],
+            [9, 5, 5, 10, 11, 11, 11, 12, 8],
+            [9, 9, 10, 10, 11, 11, 12, 12, 12],
+            [9, 13, 13, 10, 10, 14, 14, 22, 12],
+            [13, 13, 15, 15, 15, 14, 14, 22, 22],
+            [13, 16, 15, 15, 17, 14, 22, 22, 18],
+            [16, 16, 19, 19, 19, 20, 21, 21, 18],
+            [16, 16, 19, 19, 20, 20, 21, 21, 21]
+        ])
+        expected_grid = Grid([
+            [4, 3, 1, 2, 1, 2, 1, 2, 4,],
+            [1, 2, 4, 5, 4, 5, 3, 5, 3,],
+            [3, 5, 1, 2, 3, 2, 1, 4, 2,],
+            [2, 4, 3, 5, 1, 4, 3, 5, 3,],
+            [3, 5, 2, 4, 2, 5, 1, 2, 1,],
+            [4, 1, 3, 5, 3, 4, 3, 5, 4,],
+            [5, 2, 4, 2, 1, 2, 1, 2, 1,],
+            [1, 3, 1, 3, 4, 5, 4, 3, 4,],
+            [5, 4, 5, 2, 1, 3, 1, 5, 2,],
+            [1, 3, 1, 3, 4, 2, 4, 3, 1,],
+            [2, 5, 2, 5, 1, 3, 1, 5, 2,],
+        ])
+        game_solver = KemaruSolver(grid, region_grid, self.get_solver_engine())
+        solution = game_solver.get_solution()
+        self.assertEqual(expected_grid, solution)
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+
+if __name__ == '__main__':
+    unittest.main()
