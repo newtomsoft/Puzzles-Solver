52b915f00a28f6642446bbac75f567f345f50560
Thomas <thomas@newtomsoft.fr>
2025-04-23 00:53:16 +0200

    Add No4InARowSolver and corresponding unit tests
    Implemented a solver for the "No 4 in a Row" puzzle with constraints for horizontal, vertical, and diagonal rules to avoid four identical consecutive tiles. Added unit tests to ensure solver accuracy, including edge cases for grid size and constraint validation.

---
+++ Domain/Puzzles/No4InARow/No4InARowSolver.py
@@ -0,0 +1,74 @@
+﻿from Domain.Board.Grid import Grid
+from Domain.Ports.SolverEngine import SolverEngine
+from Domain.Puzzles.GameSolver import GameSolver
+
+
+class No4InARowSolver(GameSolver):
+    def __init__(self, grid: Grid, solver_engine: SolverEngine):
+        self._grid = grid
+        self.rows_number = self._grid.rows_number
+        self.columns_number = self._grid.columns_number
+        if self.rows_number < 4:
+            raise ValueError("No 4 in a Row grid must be at least 4x4")
+        self._solver = solver_engine
+        self._grid_z3: Grid | None = None
+        self._previous_solution: Grid | None = None
+
+    def get_solution(self) -> Grid:
+        self._grid_z3 = Grid([[self._solver.bool(f"matrix_{r}_{c}") for c in range(self.columns_number)] for r in range(self.rows_number)])
+        self._add_constraints()
+
+        self._previous_solution = self._compute_solution()
+        return self._previous_solution
+
+    def get_other_solution(self) -> Grid:
+        self._solver.add(self._solver.Not(self._solver.And([self._grid_z3[position] == value for position, value in self._previous_solution])))
+        self._previous_solution = self._compute_solution()
+        return self._previous_solution
+
+    def _compute_solution(self) -> Grid:
+        if not self._solver.has_solution():
+            return Grid.empty()
+
+        model = self._solver.model()
+        return Grid([[self._solver.is_true(model.eval(self._grid_z3[r][c])) for c in range(self.columns_number)] for r in range(self.rows_number)])
+
+    def _add_constraints(self):
+        self._add_initial_constraints()
+        self._add_not_same_4_adjacent_horizontally_constraints()
+        self._add_not_same_4_adjacent_vertically_constraints()
+        self._add_not_same_4_adjacent_diagonally_constraints()
+
+    def _add_initial_constraints(self):
+        for r in range(self.rows_number):
+            for c in range(self.columns_number):
+                if self._grid.value(r, c) == 0:
+                    self._solver.add(self._solver.Not(self._grid_z3[r][c]))
+                elif self._grid.value(r, c) == 1:
+                    self._solver.add(self._grid_z3[r][c])
+
+    def _add_not_same_4_adjacent_horizontally_constraints(self):
+        for r in range(self.rows_number):
+            for c in range(self.columns_number - 3):
+                self._solver.add(self._solver.Not(self._solver.And(
+                    self._grid_z3[r][c] == self._grid_z3[r][c + 1], self._grid_z3[r][c] == self._grid_z3[r][c + 2], self._grid_z3[r][c] == self._grid_z3[r][c + 3]))
+                )
+
+    def _add_not_same_4_adjacent_vertically_constraints(self):
+        for c in range(self.columns_number):
+            for r in range(self.rows_number - 3):
+                self._solver.add(self._solver.Not(self._solver.And(
+                    self._grid_z3[r][c] == self._grid_z3[r + 1][c], self._grid_z3[r][c] == self._grid_z3[r + 2][c], self._grid_z3[r][c] == self._grid_z3[r + 3][c]))
+                )
+
+    def _add_not_same_4_adjacent_diagonally_constraints(self):
+        for r in range(self.rows_number - 3):
+            for c in range(self.columns_number - 3):
+                self._solver.add(self._solver.Not(self._solver.And(
+                    self._grid_z3[r][c] == self._grid_z3[r + 1][c + 1], self._grid_z3[r][c] == self._grid_z3[r + 2][c + 2], self._grid_z3[r][c] == self._grid_z3[r + 3][c + 3]))
+                )
+        for r in range(self.rows_number - 3):
+            for c in range(3, self.columns_number):
+                self._solver.add(self._solver.Not(self._solver.And(
+                    self._grid_z3[r][c] == self._grid_z3[r + 1][c - 1], self._grid_z3[r][c] == self._grid_z3[r + 2][c - 2], self._grid_z3[r][c] == self._grid_z3[r + 3][c - 3]))
+                )
---
+++ Domain/Puzzles/No4InARow/tests/No4InARowSolver_test.py
@@ -0,0 +1,125 @@
+﻿import unittest
+from unittest import TestCase
+
+from Domain.Board.Grid import Grid
+from Domain.Puzzles.No4InARow.No4InARowSolver import No4InARowSolver
+from SolverEngineAdapters.Z3SolverEngine import Z3SolverEngine
+
+_ = -1
+
+
+class No4InARowSolverTests(TestCase):
+    @staticmethod
+    def get_solver_engine():
+        return Z3SolverEngine()
+
+    def test_solution_grid_too_small(self):
+        grid = Grid([
+            [_, _, _],
+            [_, _, 0],
+            [1, _, _],
+        ])
+
+        with self.assertRaises(ValueError) as context:
+            No4InARowSolver(grid, self.get_solver_engine())
+
+        self.assertEqual("No 4 in a Row grid must be at least 4x4", str(context.exception))
+
+    def test_initial_constraint(self):
+        grid = Grid([
+            [1, 0, 0, 0, 1, 1],
+            [1, 0, 1, 0, 0, 0],
+            [0, 1, 1, 0, 1, 0],
+            [0, 1, 0, 1, 1, 0],
+            [1, 1, 1, 0, 0, 1],
+            [0, 0, 0, 1, 1, 0],
+        ])
+        expected_grid = Grid([
+            [1, 0, 0, 0, 1, 1],
+            [1, 0, 1, 0, 0, 0],
+            [0, 1, 1, 0, 1, 0],
+            [0, 1, 0, 1, 1, 0],
+            [1, 1, 1, 0, 0, 1],
+            [0, 0, 0, 1, 1, 0],
+        ])
+        game_solver = No4InARowSolver(grid, self.get_solver_engine())
+
+        solution = game_solver.get_solution()
+        self.assertEqual(expected_grid, solution)
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+    def test_horizontally_constraint(self):
+        grid = Grid([
+            [1, 0, 0, 0, _, 1],
+            [1, 0, _, 0, 0, 0],
+            [0, 1, 1, 0, 1, 0],
+            [0, 1, 0, 1, 1, 0],
+            [1, 1, 1, _, 0, 1],
+            [0, 0, 0, _, 1, 0],
+        ])
+        expected_grid = Grid([
+            [1, 0, 0, 0, 1, 1],
+            [1, 0, 1, 0, 0, 0],
+            [0, 1, 1, 0, 1, 0],
+            [0, 1, 0, 1, 1, 0],
+            [1, 1, 1, 0, 0, 1],
+            [0, 0, 0, 1, 1, 0],
+        ])
+        game_solver = No4InARowSolver(grid, self.get_solver_engine())
+
+        solution = game_solver.get_solution()
+        self.assertEqual(expected_grid, solution)
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+    def test_vertically_constraint(self):
+        grid = Grid([
+            [1, 0, 0, 0, 1, _],
+            [1, _, 1, 0, 0, 0],
+            [0, 1, 1, 0, 1, 0],
+            [0, 1, 0, _, 1, 0],
+            [1, 1, 1, 0, 0, _],
+            [0, _, 0, 1, 1, 0],
+        ])
+        expected_grid = Grid([
+            [1, 0, 0, 0, 1, 1],
+            [1, 0, 1, 0, 0, 0],
+            [0, 1, 1, 0, 1, 0],
+            [0, 1, 0, 1, 1, 0],
+            [1, 1, 1, 0, 0, 1],
+            [0, 0, 0, 1, 1, 0],
+        ])
+        game_solver = No4InARowSolver(grid, self.get_solver_engine())
+
+        solution = game_solver.get_solution()
+        self.assertEqual(expected_grid, solution)
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+    def test_diagonally_constraint(self):
+        grid = Grid([
+            [1, 0, 0, 0, 1, 1],
+            [1, 0, 1, 0, 0, 0],
+            [_, 1, 1, 0, _, 0],
+            [0, 1, 0, 1, 1, 0],
+            [1, 1, 1, 0, 0, 1],
+            [0, 0, 0, 1, 1, 0],
+        ])
+        expected_grid = Grid([
+            [1, 0, 0, 0, 1, 1],
+            [1, 0, 1, 0, 0, 0],
+            [0, 1, 1, 0, 1, 0],
+            [0, 1, 0, 1, 1, 0],
+            [1, 1, 1, 0, 0, 1],
+            [0, 0, 0, 1, 1, 0],
+        ])
+        game_solver = No4InARowSolver(grid, self.get_solver_engine())
+
+        solution = game_solver.get_solution()
+        self.assertEqual(expected_grid, solution)
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+if __name__ == '__main__':
+    unittest.main()
