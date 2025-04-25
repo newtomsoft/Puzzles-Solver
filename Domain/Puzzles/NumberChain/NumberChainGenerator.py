12c187bc97bfe5924099f83449dd1038eeb98bb3
Thomas <thomas@newtomsoft.fr>
2025-04-25 22:07:20 +0200

    Refactor NumberChainGenerator and add multi-path detection logic
    Refactored NumberChainGenerator to accept separate row and column counts and adjusted related logic. Updated path generation preference logic for clearer behavior. Added `has_multy_path` method in LinearPathGrid to detect multiple valid paths between start and end positions.

--- Domain/Puzzles/NumberChain/NumberChainGenerator.py
+++ Domain/Puzzles/NumberChain/NumberChainGenerator.py
@@ -1,21 +1,20 @@
 ﻿import random
 
+from Board.LinearPathGrid import LinearPathGrid
 from Domain.Board.Direction import Direction
 from Domain.Board.Grid import Grid
 from Domain.Board.Position import Position
 
 
 class NumberChainGenerator:
-    def __init__(self, n: int, way_number: int):
-        self.n = n
+    def __init__(self, row_count: int, column_count: int, way_number: int):
+        self.row_column_count = row_count
+        self.column_count = column_count
         self.way_number = way_number
-
-        min_way_length = 2 * n - 1
-        max_way_length = n * n
-
+        min_way_length = column_count + row_count - 1
+        max_way_length = column_count * row_count
         if not min_way_length <= way_number <= max_way_length:
             raise ValueError(f"La longueur du chemin doit être entre {min_way_length} et {max_way_length}")
-
         self.directions = Direction.orthogonals()
 
     def generate(self):
@@ -27,15 +26,15 @@ class NumberChainGenerator:
         raise Exception("Impossible de générer une grille avec les contraintes données")
 
     def _try_generate(self):
-        grid = [[0 for _ in range(self.n)] for _ in range(self.n)]
+        grid = [[0 for _ in range(self.row_column_count)] for _ in range(self.row_column_count)]
 
         current_pos = Position(0, 0)
-        target_pos = Position(self.n - 1, self.n - 1)
+        target_pos = Position(self.row_column_count - 1, self.row_column_count - 1)
 
         path = [current_pos]
         grid[current_pos.r][current_pos.c] = 1
 
-        preferred_directions = [0, 1]
+        preferred_directions = [2, 3]
 
         while len(path) < self.way_number:
             if current_pos == target_pos:
@@ -50,7 +49,7 @@ class NumberChainGenerator:
             valid_directions = []
             random_directions = list(range(len(self.directions)))
 
-            if random.random() < 0.7:  # 70% de chance de privilégier ces directions
+            if random.random() < 0.2:
                 random_directions = preferred_directions + [d for d in random_directions if d not in preferred_directions]
             else:
                 random.shuffle(random_directions)
@@ -59,7 +58,7 @@ class NumberChainGenerator:
                 direction = self.directions[dir_idx]
                 new_pos = current_pos.after(direction)
 
-                if self.n > new_pos.r >= 0 == grid[new_pos.r][new_pos.c] and 0 <= new_pos.c < self.n:
+                if self.row_column_count > new_pos.r >= 0 == grid[new_pos.r][new_pos.c] and 0 <= new_pos.c < self.row_column_count:
                     if must_go_toward_target:
                         new_dist = abs(new_pos.r - target_pos.r) + abs(new_pos.c - target_pos.c)
                         if new_dist >= manhattan_dist:
@@ -83,8 +82,8 @@ class NumberChainGenerator:
                 for direction in self.directions:
                     new_pos = current_pos.add(direction)
 
-                    if (0 <= new_pos.row < self.n and
-                            0 <= new_pos.col < self.n):
+                    if (0 <= new_pos.row < self.row_column_count and
+                            0 <= new_pos.col < self.row_column_count):
 
                         new_dist = abs(new_pos.row - target_pos.r) + abs(new_pos.col - target_pos.c)
 
@@ -107,6 +106,38 @@ class NumberChainGenerator:
 
 
 if __name__ == "__main__":
-    generator = NumberChainGenerator(4, 11)
-    grid = generator.generate_as_grid()
-    print(grid)
+    row_count = 10
+    column_count = 10
+    way_count = 33
+    is_single_path = False
+    while not is_single_path:
+        generator = NumberChainGenerator(row_count, column_count, way_count)
+        grid = generator.generate_as_grid()
+        grid.set_value(Position(0, 0), 1)
+        grid.set_value(Position(row_count - 1, row_count - 1), 2)
+        has_multy_path = LinearPathGrid.has_multy_path(grid, Position(0, 0), Position(row_count - 1, column_count - 1))
+        if has_multy_path:
+            print("Il y a plusieurs chemins possibles")
+        else:
+            is_single_path = True
+            grid_path = LinearPathGrid.from_grid_and_checkpoints(grid, {1: Position(0, 0), 2: Position(row_count - 1, column_count - 1)})
+            print(grid_path)
+
+    # 
+    # grid_multy = Grid([
+    #     [1, 1, 1],
+    #     [1, 1, 1],
+    #     [1, 1, 2],
+    #     ])
+    # has_many_multy = LinearPathGrid.compute_multy_path(grid_multy, Position(0, 0), Position(2, 2))
+    # 
+    # grid_single = Grid([
+    #     [1, 1, 0],
+    #     [1, 1, 0],
+    #     [1, 1, 2],
+    # ])
+    # has_many_single = LinearPathGrid.compute_multy_path(grid_single, Position(0, 0), Position(2, 2))
+    # print("Il y a plusieurs chemins possibles: OK") if has_many_multy else print("Il y a un seul chemin possible: KO")
+    # print("Il y a un seul chemin possible: OK") if not has_many_single else print("Il y a plusieurs chemins possibles: KO")
+    # print("")
+    # print("result OK") if not has_many_single and has_many_multy else print("result KO")
