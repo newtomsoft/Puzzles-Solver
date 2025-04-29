353bf41f3e5ff6b3af75378ccfbf3ad5b4e41136
Thomas <thomas@newtomsoft.fr>
2025-04-29 21:16:57 +0200

    Refactor constraints to support multiple regions.
    Updated the logic to handle multiple regions by introducing distinct region IDs and steps. This allows the solver to manage constraints for multiple regions simultaneously, improving flexibility and scalability of the puzzle logic.

--- Domain/Puzzles/Renkatsu/poc/poc_region.py
+++ Domain/Puzzles/Renkatsu/poc/poc_region.py
@@ -1,17 +1,27 @@
 ﻿from z3 import *
 
 
-def configure_single_region_constraints(solver, matrix, step, n, m, region_size):
+def configure_single_regions_constraints(solver, matrix, steps, n, m, region_size):
+    for i in range(n):
+        for j in range(m):
+            solver.add(matrix[i][j] >= 0)
+            solver.add(matrix[i][j] <= len(steps))
 
-    solver.add(z3.Sum([matrix[i][j] for i in range(n) for j in range(m)]) == region_size)
+    for index, step in enumerate(steps):
+        region_id = index + 1
+        configure_single_region_constraints(solver, matrix, step, n, m, region_size, region_id)
+
+
+def configure_single_region_constraints(solver, matrix, step, n, m, region_size, region_id):
+    solver.add(z3.Sum([matrix[i][j] == region_id for i in range(n) for j in range(m)]) == region_size)
 
     for i in range(n):
         for j in range(m):
-            solver.add(If(matrix[i][j], step[i][j] >= 1, step[i][j] == 0))
+            solver.add(If(matrix[i][j] == region_id, step[i][j] >= 1, step[i][j] == 0))
     roots = []
     for i in range(n):
         for j in range(m):
-            roots.append(And(matrix[i][j], step[i][j] == 1))
+            roots.append(And(matrix[i][j] == region_id, step[i][j] == 1))
     solver.add(Or(roots))
     for i in range(len(roots)):
         for j in range(i + 1, len(roots)):
@@ -21,22 +31,22 @@ def configure_single_region_constraints(solver, matrix, step, n, m, region_size)
             current_step = step[i][j]
             adjacents = []
             if i > 0:
-                adjacents.append(And(matrix[i - 1][j], step[i - 1][j] == current_step - 1))
+                adjacents.append(And(matrix[i - 1][j] == region_id, step[i - 1][j] == current_step - 1))
             if i < n - 1:
-                adjacents.append(And(matrix[i + 1][j], step[i + 1][j] == current_step - 1))
+                adjacents.append(And(matrix[i + 1][j] == region_id, step[i + 1][j] == current_step - 1))
             if j > 0:
-                adjacents.append(And(matrix[i][j - 1], step[i][j - 1] == current_step - 1))
+                adjacents.append(And(matrix[i][j - 1] == region_id, step[i][j - 1] == current_step - 1))
             if j < m - 1:
-                adjacents.append(And(matrix[i][j + 1], step[i][j + 1] == current_step - 1))
+                adjacents.append(And(matrix[i][j + 1] == region_id, step[i][j + 1] == current_step - 1))
 
-            solver.add(Implies(And(matrix[i][j], current_step > 1), Or(adjacents)))
+            solver.add(Implies(And(matrix[i][j] == region_id, current_step > 1), Or(adjacents)))
 
 
 def print_solved(matrix, n, m):
     for i in range(n):
         row = []
         for j in range(m):
-            row.append('X' if matrix[i][j] else '.')
+            row.append(str(matrix[i][j]) if matrix[i][j] > 0 else '.')
         print(' '.join(row))
     print()
 
@@ -56,19 +66,24 @@ def exclude(solver, matrix, matrix_solved, n, m):
 
 def solve_connected_region(n, m):
     solver = Solver()
-    matrix_z3 = [[Bool(f'cell_{i}_{j}') for j in range(m)] for i in range(n)]
-    step = [[Int(f'step_{i}_{j}') for j in range(m)] for i in range(n)]
+    matrix_z3 = [[Int(f'cell_{i}_{j}') for j in range(m)] for i in range(n)]
+    step0 = [[Int(f'step0_{i}_{j}') for j in range(m)] for i in range(n)]
+    step1 = [[Int(f'step1_{i}_{j}') for j in range(m)] for i in range(n)]
+    step2 = [[Int(f'step2_{i}_{j}') for j in range(m)] for i in range(n)]
+    step3 = [[Int(f'step3_{i}_{j}') for j in range(m)] for i in range(n)]
+    steps = [step0, step1, step2, step3]
     size = 4
-    configure_single_region_constraints(solver, matrix_z3, step, n, m, size)
+    configure_single_regions_constraints(solver, matrix_z3, steps, n, m, size)
 
     count = 0
     while solver.check() == sat:
         count += 1
         model = solver.model()
-        matrix_solved = [[model.evaluate(matrix_z3[i][j]) for j in range(m)] for i in range(n)]
-        step_solved = [[(model.evaluate(step[i][j])).as_long() for j in range(m)] for i in range(n)]
+        matrix_solved = [[(model.evaluate(matrix_z3[i][j])).as_long() for j in range(m)] for i in range(n)]
+        step_solved0 = [[(model.evaluate(step0[i][j])).as_long() for j in range(m)] for i in range(n)]
+        step_solved1 = [[(model.evaluate(step1[i][j])).as_long() for j in range(m)] for i in range(n)]
         # print_solved(matrix_solved, n, m)
-        # print_step_solved(step_solved, n, m)
+        # print_step_solved(step_solved0, n, m)
         exclude(solver, matrix_z3, matrix_solved, n, m)
         # input("Press ENTER to continue...")
     print("solutions count", count)
