1d2d7fde77f23285880e83f8bea933b4e68ffd9d
Thomas <thomas@newtomsoft.fr>
2025-04-29 22:54:30 +0200

    Refactor constraints logic and add new tests for CreeksSolver.
    Reorganized constraint methods for clarity and introduced `_get_positions_in_solution_grid` to replace `map_position`. Added comprehensive unit tests and a new example puzzle case to improve solution verification.
    
    Todo: self._add_adjacent_watter_constraint()

--- Domain/Puzzles/Creeks/tests/CreeksSolver_test.py
+++ Domain/Puzzles/Creeks/tests/CreeksSolver_test.py
@@ -6,6 +6,8 @@ from Domain.Board.Position import Position
 from Domain.Puzzles.Creeks.CreeksSolver import CreeksSolver
 from SolverEngineAdapters.Z3SolverEngine import Z3SolverEngine
 
+_ = -1
+
 
 class CreeksSolverTests(TestCase):
     @staticmethod
@@ -25,26 +27,55 @@ class CreeksSolverTests(TestCase):
 
         game_solver = CreeksSolver(grid, self.get_solver_engine())
         position = Position(0, 0)
-        mapped_position = game_solver.map_position(solution_grid, position)
+        mapped_position = game_solver._get_positions_in_solution_grid(solution_grid, position)
         expected_mapped_position = {Position(0, 0)}
         self.assertEqual(expected_mapped_position, mapped_position)
 
         position = Position(0, 1)
-        mapped_position = game_solver.map_position(solution_grid, position)
+        mapped_position = game_solver._get_positions_in_solution_grid(solution_grid, position)
         expected_mapped_position = {Position(0, 0), Position(0, 1)}
         self.assertEqual(expected_mapped_position, mapped_position)
 
         position = Position(0, 2)
-        mapped_position = game_solver.map_position(solution_grid, position)
+        mapped_position = game_solver._get_positions_in_solution_grid(solution_grid, position)
         expected_mapped_position = {Position(0, 1)}
         self.assertEqual(expected_mapped_position, mapped_position)
 
+        position = Position(1, 0)
+        mapped_position = game_solver._get_positions_in_solution_grid(solution_grid, position)
+        expected_mapped_position = {Position(0, 0), Position(1, 0)}
+        self.assertEqual(expected_mapped_position, mapped_position)
+
+        position = Position(1, 1)
+        mapped_position = game_solver._get_positions_in_solution_grid(solution_grid, position)
+        expected_mapped_position = {Position(0, 0), Position(1, 0), Position(1, 1), Position(0, 1)}
+        self.assertEqual(expected_mapped_position, mapped_position)
+
+        position = Position(1, 2)
+        mapped_position = game_solver._get_positions_in_solution_grid(solution_grid, position)
+        expected_mapped_position = {Position(0, 1), Position(1, 1)}
+        self.assertEqual(expected_mapped_position, mapped_position)
+
+        position = Position(2, 0)
+        mapped_position = game_solver._get_positions_in_solution_grid(solution_grid, position)
+        expected_mapped_position = {Position(1, 0)}
+        self.assertEqual(expected_mapped_position, mapped_position)
+
+        position = Position(2, 1)
+        mapped_position = game_solver._get_positions_in_solution_grid(solution_grid, position)
+        expected_mapped_position = {Position(1, 0), Position(1, 1)}
+        self.assertEqual(expected_mapped_position, mapped_position)
+
+        position = Position(2, 2)
+        mapped_position = game_solver._get_positions_in_solution_grid(solution_grid, position)
+        expected_mapped_position = {Position(1, 1)}
+        self.assertEqual(expected_mapped_position, mapped_position)
 
     def test_solution_basic_grid(self):
         grid = Grid([
             [1, 1, 0],
             [1, 1, 0],
-            [1, 0, 0]
+            [0, 0, 0]
         ])
         game_solver = CreeksSolver(grid, self.get_solver_engine())
         solution = game_solver.get_solution()
@@ -55,6 +86,28 @@ class CreeksSolverTests(TestCase):
         self.assertEqual(expected_solution, solution)
         other_solution = game_solver.get_other_solution()
         self.assertEqual(Grid.empty(), other_solution)
+
+    def test_solution_4x4_3eyv2(self):
+        # https://gridpuzzle.com/creek/3eyv2
+        grid = Grid([
+            [0, _, 0, _, 0],
+            [1, 2, 1, 1, 1],
+            [_, 3, _, 1, _],
+            [1, _, 1, _, 0],
+            [0, 0, 1, 1, 0]
+        ])
+        game_solver = CreeksSolver(grid, self.get_solver_engine())
+        solution = game_solver.get_solution()
+        expected_solution = Grid([
+            [0, 0, 0, 0],
+            [1, 1, 0, 1],
+            [1, 0, 0, 0],
+            [0, 0, 1, 0],
+        ])
+        self.assertEqual(expected_solution, solution)
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
 
 if __name__ == '__main__':
     unittest.main()
