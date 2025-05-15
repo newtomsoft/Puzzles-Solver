63472ea364e4a35faccc227d81942f1b81c59662
Thomas <thomas@newtomsoft.fr>
2025-05-15 23:40:55 +0200

    Switch FutoshikiSolver to use OR-Tools over Z3 for solving
    Replaced Z3 with OR-Tools Constraint Programming (CP) Solver for performance and maintainability improvements in FutoshikiSolver. Updated the implementation and corresponding tests to accommodate the new solver while ensuring existing constraints and functionality are preserved.

--- Domain/Puzzles/Futoshiki/FutoshikiSolver.py
+++ Domain/Puzzles/Futoshiki/FutoshikiSolver.py
@@ -1,4 +1,4 @@
-﻿from z3 import Solver, Not, And, unsat, Int, Distinct
+﻿from ortools.sat.python import cp_model
 
 from Domain.Board.Grid import Grid
 from Domain.Board.Position import Position
@@ -15,56 +15,80 @@ class FutoshikiSolver(GameSolver):
             raise ValueError("The grid must be square")
         if self.rows_number < 4:
             raise ValueError("The grid must be at least 4x4")
-        self._solver = Solver()
-        self._grid_z3: Grid | None = None
+        self._model = None
+        self._grid_vars = None
         self._previous_solution_grid: Grid | None = None
+        self._solver = None
 
     def _init_solver(self):
-        self._grid_z3 = Grid([[Int(f"grid{r}_{c}") for c in range(self.columns_number)] for r in range(self.rows_number)])
+        self._model = cp_model.CpModel()
+        self._grid_vars = {}
+        for r in range(self.rows_number):
+            for c in range(self.columns_number):
+                self._grid_vars[(r, c)] = self._model.NewIntVar(1, self.columns_number, f'grid_{r}_{c}')
         self._add_constraints()
 
     def get_solution(self) -> Grid:
-        if not self._solver.assertions():
+        if self._model is None:
             self._init_solver()
-        if self._solver.check() == unsat:
+
+        self._solver = cp_model.CpSolver()
+        status = self._solver.Solve(self._model)
+
+        if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
+            grid = Grid([[self._solver.Value(self._number(Position(r, c))) for c in range(self.columns_number)] 
+                         for r in range(self.rows_number)])
+            self._previous_solution_grid = grid
+            return grid
+        else:
             return Grid.empty()
-        model = self._solver.model()
-        grid = Grid([[model.eval(self._number(Position(r, c))).as_long() for c in range(self.columns_number)] for r in range(self.rows_number)])
-        self._previous_solution_grid = grid
-        return grid
 
     def get_other_solution(self):
-        exclusion_constraint = Not(
-            And([self._number(Position(r, c)) == self._previous_solution_grid[Position(r, c)] for r in range(self.rows_number) for c in range(self.columns_number) if self._previous_solution_grid.value(r, c)]))
-        self._solver.add(exclusion_constraint)
+        if self._previous_solution_grid is None:
+            return Grid.empty()
+
+        different_cells = []
+        for r in range(self.rows_number):
+            for c in range(self.columns_number):
+                if self._previous_solution_grid.value(r, c) != -1:
+                    is_different = self._model.NewBoolVar(f'diff_{r}_{c}')
+                    self._model.Add(self._number(Position(r, c)) != self._previous_solution_grid.value(r, c)).OnlyEnforceIf(is_different)
+                    self._model.Add(self._number(Position(r, c)) == self._previous_solution_grid.value(r, c)).OnlyEnforceIf(is_different.Not())
+                    different_cells.append(is_different)
+
+        if different_cells:
+            self._model.AddBoolOr(different_cells)
+
         return self.get_solution()
 
     def _number(self, position):
-        return self._grid_z3[position]
+        return self._grid_vars[(position.r, position.c)]
 
     def _add_constraints(self):
-        self._add_range_constraints()
         self._add_initial_constraints()
         self._add_distinct_constraints()
         self._add_higher_constraints()
 
     def _add_initial_constraints(self):
         for position, value in [(p, v) for p, v in self._grid if v != -1]:
-            self._solver.add(self._number(position) == value)
-
-    def _add_range_constraints(self):
-        for position, value in self._grid_z3:
-            self._solver.add(value >= 1)
-            self._solver.add(value <= self.columns_number)
+            self._model.Add(self._number(position) == value)
 
     def _add_distinct_constraints(self):
-        for index, row in enumerate(self._grid_z3.matrix):
-            self._solver.add(Distinct(row))
+        for r in range(self.rows_number):
+            row_vars = [self._number(Position(r, c)) for c in range(self.columns_number)]
+            self._model.AddAllDifferent(row_vars)
 
-        for index, column_tuple in enumerate(zip(*self._grid_z3.matrix)):
-            column = list(column_tuple)
-            self._solver.add(Distinct(column))
+        for c in range(self.columns_number):
+            col_vars = [self._number(Position(r, c)) for r in range(self.rows_number)]
+            self._model.AddAllDifferent(col_vars)
 
     def _add_higher_constraints(self):
-        for first_position, second_position in self._higher_positions:
-            self._solver.add(self._number(first_position) > self._number(second_position))
+        for item in self._higher_positions:
+            if isinstance(item, tuple):
+                first_position, second_position = item
+            else:
+                first_pos_tuple, second_pos_tuple = item
+                first_position = Position(first_pos_tuple[0], first_pos_tuple[1])
+                second_position = Position(second_pos_tuple[0], second_pos_tuple[1])
+
+            self._model.Add(self._number(first_position) > self._number(second_position))
