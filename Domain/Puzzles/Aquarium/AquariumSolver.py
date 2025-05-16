87019c5883fe036a72dc3b265b8e4b3bc5644c32
Thomas <thomas@newtomsoft.fr>
2025-05-16 19:55:39 +0200

    Switch AquariumSolver to OR-Tools for constraint solving
    Replaced Z3 with OR-Tools in the AquariumSolver to improve clarity and maintainability of the constraint-solving logic. Adjusted methods to use OR-Tools' CP-SAT API, refactored constraints to align with the new library, and updated solution computation accordingly.

--- Domain/Puzzles/Aquarium/AquariumSolver.py
+++ Domain/Puzzles/Aquarium/AquariumSolver.py
@@ -1,4 +1,4 @@
-﻿from z3 import Solver, Bool, Not, And, unsat, Or, Implies, is_true
+﻿from ortools.sat.python import cp_model
 
 from Domain.Board.Grid import Grid
 from Domain.Board.Position import Position
@@ -20,26 +20,37 @@ class AquariumSolver(GameSolver):
         self._numbers = numbers
         self.columns_water_numbers = numbers[:grid.rows_number]
         self.rows_water_numbers = numbers[grid.rows_number:]
-        self._solver = Solver()
-        self._grid_z3: Grid | None = None
-        self._previous_solution: Grid | None = None
+        self._model = cp_model.CpModel()
+        self._solver = cp_model.CpSolver()
+        self._grid_vars = None
+        self._previous_solution = None
 
     def get_solution(self) -> Grid:
-        self._grid_z3 = Grid([[Bool(f"grid_{r}_{c}") for c in range(self.columns_number)] for r in range(self.rows_number)])
+        self._grid_vars = Grid([[self._model.NewBoolVar(f"grid_{r}_{c}") for c in range(self.columns_number)] for r in range(self.rows_number)])
         self._add_constrains()
         self._previous_solution = self._compute_solution()
         return self._previous_solution
 
     def get_other_solution(self) -> Grid:
-        self._solver.add(Not(And([self._grid_z3[position] == value for position, value in self._previous_solution])))
+        previous_solution_literals = []
+        for position, value in self._previous_solution:
+            temp_var = self._model.NewBoolVar(f"prev_{position.r}_{position.c}")
+            self._model.Add(self._grid_vars[position] == value).OnlyEnforceIf(temp_var)
+            self._model.Add(self._grid_vars[position] != value).OnlyEnforceIf(temp_var.Not())
+            previous_solution_literals.append(temp_var)
+
+        if previous_solution_literals:
+            self._model.AddBoolOr([lit.Not() for lit in previous_solution_literals])
+
         self._previous_solution = self._compute_solution()
         return self._previous_solution
 
     def _compute_solution(self):
-        if self._solver.check() == unsat:
+        status = self._solver.Solve(self._model)
+        if status != cp_model.OPTIMAL and status != cp_model.FEASIBLE:
             return Grid.empty()
-        model = self._solver.model()
-        grid = Grid([[is_true(model.eval(self._grid_z3[Position(i, j)])) for j in range(self.columns_number)] for i in range(self.rows_number)])
+
+        grid = Grid([[self._solver.Value(self._grid_vars[Position(i, j)]) for j in range(self.columns_number)] for i in range(self.rows_number)])
         return grid
 
     def _add_constrains(self):
@@ -47,10 +58,12 @@ class AquariumSolver(GameSolver):
         self._add_aquariums_constraints()
 
     def _add_sum_constraints(self):
-        for i, row in enumerate(self._grid_z3.matrix):
-            self._solver.add(sum(row) == self.rows_water_numbers[i])
-        for i, column in enumerate(zip(*self._grid_z3.matrix)):
-            self._solver.add(sum(column) == self.columns_water_numbers[i])
+        for i, row in enumerate(self._grid_vars.matrix):
+            self._model.Add(sum(row) == self.rows_water_numbers[i])
+
+        for i in range(self.columns_number):
+            column_vars = [self._grid_vars[Position(r, i)] for r in range(self.rows_number)]
+            self._model.Add(sum(column_vars) == self.columns_water_numbers[i])
 
     def _add_aquariums_constraints(self):
         for positions in self._aquariums.values():
@@ -60,20 +73,25 @@ class AquariumSolver(GameSolver):
                 if r not in cells_by_row_index:
                     cells_by_row_index[r] = []
                 cells_by_row_index[r].append(position)
+
             for aquarium_row_cells in cells_by_row_index.values():
                 if len(positions) > 1:
-                    all_cells_full = And([self._grid_z3[position] for position in aquarium_row_cells])
-                    all_cells_empty = And([Not(self._grid_z3[position]) for position in aquarium_row_cells])
-                    self._solver.add(Or(all_cells_full, all_cells_empty))
+                    first_cell = aquarium_row_cells[0]
+                    for cell in aquarium_row_cells[1:]:
+                        self._model.Add(self._grid_vars[first_cell] == self._grid_vars[cell])
 
                 row = aquarium_row_cells[0][0]
                 column = aquarium_row_cells[0][1]
-                z3_down_cells_full = [self._grid_z3[position] for position in [position for position in positions if position[0] > row]]
-                z3_cell_full = self._grid_z3[row][column]
-                if len(z3_down_cells_full) > 0:
-                    full_implies_down_full = Implies(z3_cell_full, And(z3_down_cells_full))
-                    self._solver.add(full_implies_down_full)
-                z3_up_cells_empty = [Not(self._grid_z3[position]) for position in [position for position in positions if position[0] < row]]
-                if len(z3_up_cells_empty) > 0:
-                    empty_implies_up_empty = Implies(Not(z3_cell_full), And(z3_up_cells_empty))
-                    self._solver.add(empty_implies_up_empty)
+                cell_var = self._grid_vars[Position(row, column)]
+
+                down_cells = [position for position in positions if position[0] > row]
+                for down_cell in down_cells:
+                    down_cell_var = self._grid_vars[down_cell]
+                    self._model.AddImplication(cell_var, down_cell_var)
+
+                up_cells = [position for position in positions if position[0] < row]
+                for up_cell in up_cells:
+                    up_cell_var = self._grid_vars[up_cell]
+                    not_cell_var = self._model.NewBoolVar(f"not_cell_{row}_{column}")
+                    self._model.AddBoolXOr([cell_var, not_cell_var])
+                    self._model.AddImplication(not_cell_var, up_cell_var.Not())
