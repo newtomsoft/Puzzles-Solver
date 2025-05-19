f9f9f394ab1453475b607204c188c2f11b6b7577
Thomas <thomas@newtomsoft.fr>
2025-05-19 13:52:11 +0200

    Refactor StarBattleSolver tests for clarity and consistency
    Updated test grids to use consistent numbering and underscore (`_`) for empty cells, improving readability and uniformity across all test cases. Added a new test case for scenarios with three stars per region/column/row. These changes ensure better test maintainability and enhanced clarity in expected solutions.

--- Domain/Puzzles/StarBattle/tests/StarBattleSolver_test.py
+++ Domain/Puzzles/StarBattle/tests/StarBattleSolver_test.py
@@ -4,14 +4,14 @@ from unittest import TestCase
 from Domain.Board.Grid import Grid
 from Domain.Puzzles.StarBattle.StarBattleSolver import StarBattleSolver
 
+_ = 0
+
 
 class StarBattleSolverTests(TestCase):
-
-
     def test_solution_grid_not_a_square(self):
         grid = Grid([
-            [0, 1, 1],
-            [0, 2, 2],
+            [3, 1, 1],
+            [3, 2, 2],
         ])
         with self.assertRaises(ValueError) as context:
             StarBattleSolver(grid, 1)
@@ -19,9 +19,9 @@ class StarBattleSolverTests(TestCase):
 
     def test_solution_grid_size_less_than_4(self):
         grid = Grid([
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0],
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
         ])
         with self.assertRaises(ValueError) as context:
             StarBattleSolver(grid, 1)
@@ -29,10 +29,10 @@ class StarBattleSolverTests(TestCase):
 
     def test_solution_color_less_than_columns_number(self):
         grid = Grid([
-            [0, 1, 2, 2],
-            [0, 1, 2, 2],
-            [0, 1, 2, 2],
-            [0, 1, 2, 2],
+            [3, 1, 2, 2],
+            [3, 1, 2, 2],
+            [3, 1, 2, 2],
+            [3, 1, 2, 2],
         ])
         with self.assertRaises(ValueError) as context:
             StarBattleSolver(grid, 1)
@@ -40,10 +40,10 @@ class StarBattleSolverTests(TestCase):
 
     def test_solution_1(self):
         grid = Grid([
-            [0, 1, 2, 3],
-            [0, 0, 0, 0],
-            [0, 0, 0, 0],
-            [0, 0, 0, 0],
+            [4, 1, 2, 3],
+            [4, 4, 4, 4],
+            [4, 4, 4, 4],
+            [4, 4, 4, 4],
         ])
         game_solver = StarBattleSolver(grid, 1)
         solution = game_solver.get_solution()
@@ -51,16 +51,16 @@ class StarBattleSolverTests(TestCase):
 
     def test_solution_grid_4x4(self):
         grid = Grid([
-            [0, 0, 1, 1],
-            [0, 0, 0, 1],
-            [0, 2, 2, 3],
+            [4, 4, 1, 1],
+            [4, 4, 4, 1],
+            [4, 2, 2, 3],
             [2, 2, 2, 3],
         ])
         expected_solution = Grid([
-            [0, 0, 1, 0],
-            [1, 0, 0, 0],
-            [0, 0, 0, 1],
-            [0, 1, 0, 0],
+            [_, _, 1, _],
+            [1, _, _, _],
+            [_, _, _, 1],
+            [_, 1, _, _],
         ])
         game_solver = StarBattleSolver(grid, 1)
         solution = game_solver.get_solution()
@@ -68,26 +68,26 @@ class StarBattleSolverTests(TestCase):
 
     def test_solution_9x9(self):
         grid = Grid([
-            ['p', 'p', 'p', 'p', 'p', 'o', 'b', 'b', 'b'],
-            ['p', 'p', 'p', 'g', 'o', 'o', 'o', 'b', 'b'],
-            ['p', 'p', 'g', 'g', 'g', 'o', 'r', 'b', 'b'],
-            ['p', 'p', 'p', 'g', 'm', 'r', 'r', 'r', 'b'],
-            ['p', 'p', 'y', 'm', 'm', 'm', 'r', 's', 'b'],
-            ['p', 'y', 'y', 'y', 'm', 'w', 's', 's', 's'],
-            ['p', 'p', 'y', 'p', 'w', 'w', 'w', 's', 'p'],
-            ['p', 'p', 'p', 'p', 'p', 'w', 'p', 'p', 'p'],
-            ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+            [1, 1, 1, 1, 1, 2, 3, 3, 3],
+            [1, 1, 1, 4, 2, 2, 2, 3, 3],
+            [1, 1, 4, 4, 4, 2, 5, 3, 3],
+            [1, 1, 1, 4, 6, 5, 5, 5, 3],
+            [1, 1, 7, 6, 6, 6, 5, 8, 3],
+            [1, 7, 7, 7, 6, 9, 8, 8, 8],
+            [1, 1, 7, 1, 9, 9, 9, 8, 1],
+            [1, 1, 1, 1, 1, 9, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1],
         ])
         expected_solution = Grid([
-            [0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0],
-            [0, 0, 1, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 1, 0, 0],
-            [0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 1, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 1, 0],
-            [0, 0, 0, 0, 0, 1, 0, 0, 0],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0]
+            [_, _, _, _, _, _, _, _, 1],
+            [_, _, _, _, 1, _, _, _, _],
+            [_, _, 1, _, _, _, _, _, _],
+            [_, _, _, _, _, _, 1, _, _],
+            [_, _, _, 1, _, _, _, _, _],
+            [_, 1, _, _, _, _, _, _, _],
+            [_, _, _, _, _, _, _, 1, _],
+            [_, _, _, _, _, 1, _, _, _],
+            [1, _, _, _, _, _, _, _, _]
         ])
         game_solver = StarBattleSolver(grid, 1)
         solution = game_solver.get_solution()
@@ -95,26 +95,26 @@ class StarBattleSolverTests(TestCase):
 
     def test_solution_9x9_2(self):
         grid = Grid([
-            [0, 0, 0, 0, 8, 2, 2, 3, 4],
-            [0, 6, 6, 8, 8, 8, 2, 3, 4],
-            [0, 5, 6, 6, 8, 3, 3, 3, 4],
-            [0, 5, 8, 8, 8, 8, 8, 7, 4],
-            [0, 5, 5, 5, 8, 7, 7, 7, 4],
-            [0, 8, 8, 8, 8, 8, 8, 8, 4],
+            [9, 9, 9, 9, 8, 2, 2, 3, 4],
+            [9, 6, 6, 8, 8, 8, 2, 3, 4],
+            [9, 5, 6, 6, 8, 3, 3, 3, 4],
+            [9, 5, 8, 8, 8, 8, 8, 7, 4],
+            [9, 5, 5, 5, 8, 7, 7, 7, 4],
+            [9, 8, 8, 8, 8, 8, 8, 8, 4],
             [1, 1, 4, 4, 8, 4, 4, 4, 4],
             [1, 1, 4, 8, 8, 8, 4, 4, 4],
             [1, 4, 4, 4, 4, 4, 4, 4, 4]
         ])
         expected_solution = Grid([
-            [0, 0, 0, 0, 0, 0, 1, 0, 0],
-            [0, 0, 1, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 1, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 1, 0],
-            [0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0],
-            [0, 1, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 1]
+            [_, _, _, _, _, _, 1, _, _],
+            [_, _, 1, _, _, _, _, _, _],
+            [_, _, _, _, _, 1, _, _, _],
+            [_, _, _, _, _, _, _, 1, _],
+            [_, _, _, 1, _, _, _, _, _],
+            [1, _, _, _, _, _, _, _, _],
+            [_, _, _, _, 1, _, _, _, _],
+            [_, 1, _, _, _, _, _, _, _],
+            [_, _, _, _, _, _, _, _, 1]
         ])
         game_solver = StarBattleSolver(grid, 1)
         solution = game_solver.get_solution()
@@ -122,17 +122,17 @@ class StarBattleSolverTests(TestCase):
 
     def test_solution_when_stars_count_equal_0(self):
         grid = Grid([
-            [0, 0, 0, 0, 8, 2, 2, 3, 4],
-            [0, 6, 6, 8, 8, 8, 2, 3, 4],
-            [0, 5, 6, 6, 8, 3, 3, 3, 4],
-            [0, 5, 8, 8, 8, 8, 8, 7, 4],
-            [0, 5, 5, 5, 8, 7, 7, 7, 4],
-            [0, 8, 8, 8, 8, 8, 8, 8, 4],
+            [9, 9, 9, 9, 8, 2, 2, 3, 4],
+            [9, 6, 6, 8, 8, 8, 2, 3, 4],
+            [9, 5, 6, 6, 8, 3, 3, 3, 4],
+            [9, 5, 8, 8, 8, 8, 8, 7, 4],
+            [9, 5, 5, 5, 8, 7, 7, 7, 4],
+            [9, 8, 8, 8, 8, 8, 8, 8, 4],
             [1, 1, 4, 4, 8, 4, 4, 4, 4],
             [1, 1, 4, 8, 8, 8, 4, 4, 4],
             [1, 4, 4, 4, 4, 4, 4, 4, 4],
         ])
-        stars_count_by_region_column_row = 0
+        stars_count_by_region_column_row = _
         with self.assertRaises(ValueError) as context:
             StarBattleSolver(grid, stars_count_by_region_column_row)
         self.assertEqual("The stars count by region/column/row must be at least 1", str(context.exception))
@@ -151,18 +151,56 @@ class StarBattleSolverTests(TestCase):
             [4, 4, 10, 10, 10, 8, 8, 6, 7, 7]
         ])
         expected_solution = Grid([
-            [0, 0, 0, 0, 0, 1, 0, 1, 0, 0],
-            [0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
-            [0, 1, 0, 0, 0, 0, 1, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 1, 0],
-            [1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
-            [0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 1, 0, 0, 1, 0],
-            [1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 1, 0, 0, 0],
+            [_, _, _, _, _, 1, _, 1, _, _],
+            [_, _, _, 1, _, _, _, _, _, 1],
+            [_, 1, _, _, _, _, 1, _, _, _],
+            [_, _, _, _, 1, _, _, _, 1, _],
+            [1, _, 1, _, _, _, _, _, _, _],
+            [_, _, _, _, _, _, _, 1, _, 1],
+            [_, 1, _, 1, _, _, _, _, _, _],
+            [_, _, _, _, _, 1, _, _, 1, _],
+            [1, _, 1, _, _, _, _, _, _, _],
+            [_, _, _, _, 1, _, 1, _, _, _],
         ])
         stars_count_by_region_column_row = 2
+        game_solver = StarBattleSolver(grid, stars_count_by_region_column_row)
+        solution = game_solver.get_solution()
+        self.assertEqual(expected_solution, solution)
+
+    def test_solution_when_stars_count_equal_3(self):
+        grid = Grid([
+            [1, 1, 1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+            [1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 4, 4, 4, 3],
+            [1, 1, 2, 2, 1, 1, 2, 4, 4, 4, 4, 4, 5, 5],
+            [6, 1, 2, 2, 1, 2, 2, 2, 2, 7, 7, 4, 4, 5],
+            [6, 1, 1, 1, 1, 1, 2, 8, 8, 7, 4, 4, 4, 5],
+            [6, 6, 6, 6, 2, 2, 2, 8, 7, 7, 4, 4, 4, 5],
+            [6, 6, 6, 6, 9, 2, 8, 8, 8, 7, 7, 4, 4, 5],
+            [6, 6, 9, 9, 9, 10, 8, 11, 8, 8, 7, 7, 12, 5],
+            [6, 9, 9, 9, 10, 10, 10, 11, 8, 8, 7, 7, 12, 12],
+            [6, 6, 9, 9, 10, 10, 10, 11, 8, 11, 11, 12, 12, 13],
+            [6, 6, 9, 9, 9, 9, 10, 11, 11, 11, 12, 12, 13, 13],
+            [14, 6, 14, 14, 14, 14, 10, 14, 11, 12, 12, 12, 12, 13],
+            [14, 14, 14, 14, 10, 10, 10, 14, 11, 12, 12, 12, 13, 13],
+            [14, 14, 14, 14, 14, 14, 14, 14, 14, 13, 13, 13, 13, 13]
+        ])
+        expected_solution = Grid([
+            [_, _, _, _, 1, _, 1, _, 1, _, _, _, _, _],
+            [1, _, 1, _, _, _, _, _, _, _, 1, _, _, _],
+            [_, _, _, _, _, 1, _, 1, _, _, _, _, 1, _],
+            [_, 1, _, 1, _, _, _, _, _, 1, _, _, _, _],
+            [_, _, _, _, _, _, _, 1, _, _, _, 1, _, 1],
+            [1, _, 1, _, 1, _, _, _, _, _, _, _, _, _],
+            [_, _, _, _, _, _, 1, _, _, 1, _, _, _, 1],
+            [_, 1, _, 1, _, _, _, _, _, _, _, 1, _, _],
+            [_, _, _, _, _, 1, _, 1, _, 1, _, _, _, _],
+            [_, _, 1, _, _, _, _, _, _, _, _, 1, _, 1],
+            [_, _, _, _, 1, _, 1, _, 1, _, _, _, _, _],
+            [1, _, _, _, _, _, _, _, _, _, 1, _, 1, _],
+            [_, _, _, 1, _, 1, _, _, 1, _, _, _, _, _],
+            [_, 1, _, _, _, _, _, _, _, _, 1, _, 1, _],
+        ])
+        stars_count_by_region_column_row = 3
         game_solver = StarBattleSolver(grid, stars_count_by_region_column_row)
         solution = game_solver.get_solution()
         self.assertEqual(expected_solution, solution)
