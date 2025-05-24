366c288e39c5fbb04c852fab9be916d04d2ca691
Thomas <thomas@newtomsoft.fr>
2025-05-24 22:38:21 +0200

    Add support for Creek puzzles from GridPuzzle.com
    Integrated the CreekSolver and GridPuzzleCreekGridProvider to handle Creek puzzles. Updated URL mappings in `PuzzleMainConsole` and added a new grid provider for parsing Creek puzzle grids.

---
+++ GridProviders/GridPuzzle/GridPuzzleCreekGridProvider.py
@@ -0,0 +1,44 @@
+import math
+
+from bs4 import BeautifulSoup
+from playwright.sync_api import BrowserContext
+
+from Domain.Board.Grid import Grid
+from GridProviders.GridProvider import GridProvider
+from GridProviders.PlaywrightGridProvider import PlaywrightGridProvider
+from GridProviders.PuzzlesMobile.PuzzlesMobileGridProvider import PuzzlesMobileGridProvider
+
+
+class GridPuzzleCreekGridProvider(GridProvider, PlaywrightGridProvider, PuzzlesMobileGridProvider):
+    def get_grid(self, url: str):
+        return self.with_playwright(self.scrap_grid, url)
+
+    def scrap_grid(self, browser: BrowserContext, url):
+        page = browser.pages[0]
+        page.goto(url)
+        html_page = page.content()
+        soup = BeautifulSoup(html_page, 'html.parser')
+        matrix_cells = soup.find_all('div', class_='g_cell')
+        cells_count = len(matrix_cells)
+        row_count = int(math.sqrt(cells_count))
+        column_count = row_count
+
+        matrix = [[-1 for _ in range(column_count+1)] for _ in range(row_count+1)]
+        tree_cells = soup.find_all('div', class_='tip_q_num')
+        for cell in tree_cells:
+            parent_div = cell.parent
+            data_id = int(parent_div.get('data-id')) - 1
+            row = data_id // column_count
+            col = data_id % column_count
+
+            cell_classes = cell.get('class')
+            if 'top-100' in cell_classes:
+                row += 1
+            if 'start-100' in cell_classes:
+                col += 1
+
+            text = cell.get_text().strip()
+            if text:
+                matrix[row][col] = int(text)
+
+        return Grid(matrix)
