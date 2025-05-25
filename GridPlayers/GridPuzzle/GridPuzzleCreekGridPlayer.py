92da82913e4c6517415100500a7b8855d90ba679
Thomas <thomas@newtomsoft.fr>
2025-05-25 17:52:35 +0200

    Refactor `GridPuzzle` providers to share common functionality
    Introduce `GridPuzzleGridProvider` to centralize shared methods for HTML scraping and grid processing. Update individual puzzle providers to leverage this common functionality, reducing code duplication and improving maintainability. Add viewport-based video processing in `PlaywrightGridPlayer`, and streamline CreekGridPlayer's play logic with video handling.

--- GridPlayers/GridPuzzle/GridPuzzleCreekGridPlayer.py
+++ GridPlayers/GridPuzzle/GridPuzzleCreekGridPlayer.py
@@ -4,16 +4,20 @@ from playwright.sync_api import BrowserContext
 
 from Board.Grid import Grid
 from GridPlayers.GridPlayer import GridPlayer
-from GridPlayers.PuzzleMobiles.PuzzlesMobileGridPlayer import PuzzlesMobileGridPlayer
+from GridPlayers.PlaywrightGridPlayer import PlaywrightGridPlayer
 
 
-class GridPuzzleCreekGridPlayer(GridPlayer, PuzzlesMobileGridPlayer):
+class GridPuzzleCreekGridPlayer(GridPlayer, PlaywrightGridPlayer):
     @classmethod
     def play(cls, grid_solution: Grid, browser: BrowserContext):
         page = browser.pages[0]
+        video, rectangle = cls.get_data_video_viewport(page)
+
         cells = page.query_selector_all("div.g_cell")
         for position, solution_value in [(position, solution_value) for position, solution_value in grid_solution if solution_value]:
             index = position.r * grid_solution.columns_number + position.c
             cells[index].click(click_count=2)
 
-        sleep(6)
+        sleep(2)
+        browser.close()
+        cls.process_video(video, "creek", rectangle, 1)
--- GridProviders/GridPuzzle/GridPuzzleNumberChainGridProvider.py
+++ GridProviders/GridPuzzle/GridPuzzleNumberChainGridProvider.py
@@ -1,28 +1,17 @@
-﻿import base64
-import re
-
-from bs4 import BeautifulSoup
-from playwright.sync_api import BrowserContext
+﻿from playwright.sync_api import BrowserContext
 
 from Domain.Board.Grid import Grid
 from GridProviders.GridProvider import GridProvider
+from GridProviders.GridPuzzle.GridPuzzleGridProvider import GridPuzzleGridProvider
 from GridProviders.PlaywrightGridProvider import PlaywrightGridProvider
-from GridProviders.PuzzlesMobile.PuzzlesMobileGridProvider import PuzzlesMobileGridProvider
 
 
-class GridPuzzleNumberChainGridProvider(GridProvider, PlaywrightGridProvider, PuzzlesMobileGridProvider):
+class GridPuzzleNumberChainGridProvider(GridProvider, PlaywrightGridProvider, GridPuzzleGridProvider):
     def get_grid(self, url: str):
         return self.with_playwright(self.scrap_grid, url)
 
     def scrap_grid(self, browser: BrowserContext, url):
-        page = browser.pages[0]
-        page.goto(url)
-        html_page = page.content()
-        html_string = BeautifulSoup(html_page, 'html.parser').prettify()
-        size = int(re.search(r'gpl\.Size = (\d+);', html_string).group(1))
-        pqq_base64 = re.search(r'gpl\.pqq = "(.*?)";', html_string).group(1)
-        pqq_raw = base64.b64decode(pqq_base64[3:])
-        pqq_string = pqq_raw.decode('utf-8', errors='ignore')
-        pqq_string_list = pqq_string.split('|')
+        html_page = self.get_html(browser, url)
+        pqq_string_list, size = self.get_canvas_data(html_page)
         matrix = [[int(pqq_string_list[i * size + j]) for j in range(size)] for i in range(size)]
         return Grid(matrix)
