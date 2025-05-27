b85e58be10b5ea038ee498c0a3d71233792a9882
Thomas <thomas@newtomsoft.fr>
2025-05-27 23:59:57 +0200

    Add unit tests for PurenrupuSolver with various grid sizes

--- Domain/Puzzles/Purenrupu/tests/PurenrupuSolver_test.py
+++ Domain/Puzzles/Purenrupu/tests/PurenrupuSolver_test.py
@@ -28,3 +28,177 @@ class PurenrupuSolverTests(TestCase):
         self.assertEqual(expected_solution_string, str(solution))
         other_solution = game_solver.get_other_solution()
         self.assertEqual(Grid.empty(), other_solution)
+
+    def test_solution_6x6(self):
+        grid = Grid([
+            [_, _, 1, _, _, _],
+            [_, _, 1, _, _, _],
+            [_, _, _, _, _, 1],
+            [_, 1, 1, 1, _, _],
+            [_, _, _, _, _, _],
+            [1, _, _, 1, _, _],
+        ])
+
+        game_solver = PurenrupuSolver(grid)
+        solution = game_solver.get_solution()
+        expected_solution_string = (
+            ' ┌──┐     ┌─────┐ '
+            ' │  │     │  ┌──┘ '
+            ' │  └─────┘  │    '
+            ' │           └──┐ '
+            ' └──┐  ┌─────┐  │ '
+            '    └──┘     └──┘ '
+        )
+        self.assertEqual(expected_solution_string, str(solution))
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+    def test_solution_7x7(self):
+        grid = Grid([
+            [_, _, _, _, _, _, _],
+            [_, _, _, _, 1, _, _],
+            [_, _, 1, _, _, _, 1],
+            [_, _, _, 1, 1, _, _],
+            [_, 1, _, 1, _, _, _],
+            [_, 1, _, _, _, 1, _],
+            [_, _, _, _, _, _, _],
+        ])
+
+        game_solver = PurenrupuSolver(grid)
+        solution = game_solver.get_solution()
+        expected_solution_string = (
+           ' ┌─────────────────┐ \n'
+           ' │  ┌─────┐     ┌──┘ \n'
+           ' │  │     └─────┘    \n'
+           ' │  └──┐        ┌──┐ \n'
+           ' │     │     ┌──┘  │ \n'
+           ' │     └─────┘     │ \n'
+           ' └─────────────────┘ '
+        )
+        self.assertEqual(expected_solution_string, str(solution))
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+    def test_solution_8x8(self):
+        grid = Grid([
+            [_, _, _, _, _, 1, _, _],
+            [_, 1, _, _, _, _, _, _],
+            [_, _, _, _, 1, _, _, _],
+            [_, _, 1, _, _, _, 1, _],
+            [_, 1, _, _, 1, _, 1, _],
+            [_, _, _, 1, _, _, _, _],
+            [_, _, 1, _, _, 1, _, _],
+            [_, _, 1, _, _, _, _, _],
+        ])
+
+        game_solver = PurenrupuSolver(grid)
+        solution = game_solver.get_solution()
+        expected_solution_string = (
+           ' ┌─────┐  ┌──┐     ┌──┐ \n'
+           ' │     │  │  └──┐  │  │ \n'
+           ' └──┐  └──┘     └──┘  │ \n'
+           ' ┌──┘     ┌─────┐     │ \n'
+           ' │     ┌──┘     │     │ \n'
+           ' │  ┌──┘     ┌──┘  ┌──┘ \n'
+           ' │  │     ┌──┘     └──┐ \n'
+           ' └──┘     └───────────┘ '
+        )
+        self.assertEqual(expected_solution_string, str(solution))
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+    def test_solution_9x9(self):
+        grid = Grid([
+            [_, _, _, _, _, 1, _, _],
+            [_, 1, _, _, _, _, _, _],
+            [_, _, _, _, 1, _, _, _],
+            [_, _, 1, _, _, _, 1, _],
+            [_, 1, _, _, 1, _, 1, _],
+            [_, _, _, 1, _, _, _, _],
+            [_, _, 1, _, _, 1, _, _],
+            [_, _, 1, _, _, _, _, _],
+        ])
+
+        game_solver = PurenrupuSolver(grid)
+        solution = game_solver.get_solution()
+        expected_solution_string = (
+            ' ┌─────┐  ┌──┐     ┌──┐ \n'
+            ' │     │  │  └──┐  │  │ \n'
+            ' └──┐  └──┘     └──┘  │ \n'
+            ' ┌──┘     ┌─────┐     │ \n'
+            ' │     ┌──┘     │     │ \n'
+            ' │  ┌──┘     ┌──┘  ┌──┘ \n'
+            ' │  │     ┌──┘     └──┐ \n'
+            ' └──┘     └───────────┘ '
+        )
+        self.assertEqual(expected_solution_string, str(solution))
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+    def test_solution_10x10(self):
+        grid = Grid([
+            [1, _, _, _, 1, _, _, _, _, _],
+            [_, _, _, _, 1, _, 1, _, _, _],
+            [_, 1, _, 1, _, _, _, _, 1, _],
+            [_, _, _, 1, _, 1, _, _, _, _],
+            [_, _, 1, _, _, _, _, 1, _, _],
+            [_, _, _, _, _, _, _, 1, _, _],
+            [_, 1, _, 1, _, _, _, _, _, 1],
+            [_, _, _, _, 1, _, 1, 1, _, _],
+            [_, _, 1, _, _, _, _, _, _, _],
+            [_, _, _, _, _, _, _, _, _, 1],
+        ])
+
+        game_solver = PurenrupuSolver(grid)
+        solution = game_solver.get_solution()
+        expected_solution_string = (
+           '    ┌─────┐     ┌─────┐  ┌──┐ \n'
+           ' ┌──┘  ┌──┘     │     └──┘  │ \n'
+           ' │     │     ┌──┘  ┌──┐     │ \n'
+           ' │  ┌──┘     │     │  └──┐  │ \n'
+           ' │  │     ┌──┘  ┌──┘     │  │ \n'
+           ' │  └──┐  └──┐  └──┐     └──┘ \n'
+           ' │     │     └──┐  └─────┐    \n'
+           ' └──┐  └──┐     │        └──┐ \n'
+           ' ┌──┘     └─────┘  ┌──┐  ┌──┘ \n'
+           ' └─────────────────┘  └──┘    '
+        )
+        self.assertEqual(expected_solution_string, str(solution))
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+    def test_solution_12x12(self):
+        grid = Grid([
+            [_, _, _, _, 1, _, _, _, _, _, _, 1],
+            [_, _, _, _, 1, _, _, _, _, 1, _, _],
+            [1, _, _, _, _, _, _, 1, _, _, 1, _],
+            [_, _, _, _, 1, 1, _, _, _, _, 1, _],
+            [_, _, _, 1, _, _, _, _, 1, _, _, _],
+            [_, _, _, _, _, _, 1, _, _, _, _, 1],
+            [_, _, 1, _, 1, _, 1, 1, _, 1, _, _],
+            [1, _, _, _, _, _, _, _, _, _, _, _],
+            [_, _, _, 1, _, _, 1, _, 1, _, _, _],
+            [_, 1, _, _, 1, _, _, _, _, 1, 1, _],
+            [_, _, _, _, _, 1, _, _, _, _, _, _],
+            [1, _, _, 1, _, _, _, _, 1, _, _, 1],
+        ])
+
+        game_solver = PurenrupuSolver(grid)
+        solution = game_solver.get_solution()
+        expected_solution_string = (
+           ' ┌────────┐     ┌─────┐  ┌─────┐    \n'
+           ' └──┐  ┌──┘     └──┐  └──┘     └──┐ \n'
+           '    │  │  ┌────────┘     ┌──┐     │ \n'
+           ' ┌──┘  └──┘        ┌─────┘  │     │ \n'
+           ' └─────┐     ┌──┐  └──┐     │  ┌──┘ \n'
+           ' ┌─────┘  ┌──┘  │     └──┐  └──┘    \n'
+           ' └──┐     │     │        │     ┌──┐ \n'
+           '    └──┐  └──┐  └─────┐  └──┐  │  │ \n'
+           ' ┌─────┘     └──┐     │     └──┘  │ \n'
+           ' │     ┌──┐     └──┐  └──┐        │ \n'
+           ' └──┐  │  └──┐     └──┐  └──┐  ┌──┘ \n'
+           '    └──┘     └────────┘     └──┘    '
+        )
+        self.assertEqual(expected_solution_string, str(solution))
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
