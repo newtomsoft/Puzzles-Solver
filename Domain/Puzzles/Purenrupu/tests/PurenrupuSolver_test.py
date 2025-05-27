b42faece5f47c44f9ebab406c1c48e1a800c7b74
Thomas <thomas@newtomsoft.fr>
2025-05-27 20:05:05 +0200

    Add PurenrupuSolver and corresponding unit tests for puzzle solving. First implementation

---
+++ Domain/Puzzles/Purenrupu/tests/PurenrupuSolver_test.py
@@ -0,0 +1,30 @@
+﻿from unittest import TestCase
+
+from Domain.Board.Grid import Grid
+from Puzzles.Purenrupu.PurenrupuSolver import PurenrupuSolver
+
+_ = 0
+
+
+class PurenrupuSolverTests(TestCase):
+    def test_solution_5x5(self):
+        grid = Grid([
+            [_, _, _, _, 1],
+            [_, _, _, _, _],
+            [1, _, _, 1, _],
+            [_, _, 1, 1, _],
+            [_, _, _, _, _],
+        ])
+
+        game_solver = PurenrupuSolver(grid)
+        solution = game_solver.get_solution()
+        expected_solution_string = (
+            ' ┌────────┐    \n'
+            ' └─────┐  └──┐ \n'
+            '    ┌──┘     │ \n'
+            ' ┌──┘        │ \n'
+            ' └───────────┘ '
+        )
+        self.assertEqual(expected_solution_string, str(solution))
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
