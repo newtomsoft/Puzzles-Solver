9ab8a34149c9263365b117becb9888c222a735a3
Thomas <thomas@newtomsoft.fr>
2025-05-31 14:07:20 +0200

    Koburin wip

---
+++ Domain/Puzzles/Koburin/tests/KoburinSolver_test.py
@@ -0,0 +1,30 @@
+﻿from unittest import TestCase
+
+from Domain.Board.Grid import Grid
+from Puzzles.Koburin.KoburinSolver import KoburinSolver
+
+_ = -1
+
+
+class KoburinSolverTests(TestCase):
+    def test_solution_5x5(self):
+        grid = Grid([
+            [_, _, 0, _, _],
+            [_, _, _, _, _],
+            [_, 1, _, 1, _],
+            [_, _, _, _, _],
+            [_, _, _, _, 0],
+        ])
+
+        game_solver = KoburinSolver(grid)
+        solution = game_solver.get_solution()
+        expected_solution_string = (
+            ' ┌──┐  0  ┌──┐ \n'
+            ' │  └─────┘  │ \n'
+            ' │  1  ■  1  │ \n'
+            ' │  ┌──┐  ┌──┘ \n'
+            ' └──┘  └──┘  0 '
+        )
+        self.assertEqual(expected_solution_string, str(solution))
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
