918fddfd1c2079746c6fbf6a1e8fb37bf64aca97
Thomas <thomas@newtomsoft.fr>
2025-06-06 20:36:06 +0200

    Refactor `GridPuzzleCanvasPlayer` to convert methods to static and improve code organization

--- GridPlayers/GridPuzzle/GridPuzzleCanvasPlayer.py
+++ GridPlayers/GridPuzzle/GridPuzzleCanvasPlayer.py
@@ -4,11 +4,6 @@ from Domain.Board.Position import Position
 
 
 class GridPuzzleCanvasPlayer:
-    def _get_positions_from_position0_0(self, solution: IslandGrid) -> list[Position]:
-        connected_positions = solution.follow_path()
-        connected_positions.append(connected_positions[0])
-        return connected_positions
-
     def _draw_path(self, cell_height, cell_width, page, solution, x0, y0):
         connected_positions = self._get_positions_from_position0_0(solution)
         for index, position in enumerate(connected_positions[:-1]):
@@ -16,7 +11,14 @@ class GridPuzzleCanvasPlayer:
             direction = position.direction_to(next_position)
             self._trace_direction_from_position(direction, position, page, cell_width, cell_height, x0, y0)
 
-    def _trace_direction_from_position(self, direction, position, page, cell_width, cell_height, x0, y0):
+    @staticmethod
+    def _get_positions_from_position0_0(solution: IslandGrid) -> list[Position]:
+        connected_positions = solution.follow_path()
+        connected_positions.append(connected_positions[0])
+        return connected_positions
+
+    @staticmethod
+    def _trace_direction_from_position(direction, position, page, cell_width, cell_height, x0, y0):
         if direction == Direction.right():
             page.mouse.move(x0 + cell_width / 2 + position.c * cell_width, y0 + cell_height / 2 + position.r * cell_height)
             page.mouse.down()
