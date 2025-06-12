a229d0463b7837cd82866ce0ae4d3e5d847ac2c0
Thomas <thomas@newtomsoft.fr>
2025-06-12 18:33:52 +0200

    Update `_get_data_video` parameters in `ZipGridPlayer` to adjust rectangle coordinates for accurate video processing

--- GridPlayers/LinkedIn/ZipGridPlayer.py
+++ GridPlayers/LinkedIn/ZipGridPlayer.py
@@ -9,7 +9,7 @@ class ZipPlayer(PlaywrightPlayer):
         game_board = frame.wait_for_selector('.game-board')
         cells_divs = game_board.query_selector_all('div.trail-cell')
 
-        video, rectangle = self._get_data_video(frame, '.grid-board-wrapper', page, -12, -23, 12, 8)
+        video, rectangle = self._get_data_video(frame, '.grid-board-wrapper', page, 12, 23, 12, 8)
 
         for position in solution.path:
             self.mouse_click_on_position(page.mouse, solution, position, cells_divs)
