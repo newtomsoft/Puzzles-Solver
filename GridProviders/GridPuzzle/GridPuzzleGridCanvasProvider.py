fd44b85e1675afb71bb8a71e328940577cc96c00
Thomas <thomas@newtomsoft.fr>
2025-06-16 18:56:45 +0200

    Add GridPuzzleSlitherlinkGridProvider and GridPuzzleSlitherlinkPlayer and integrate Slitherlink support into UrlPatternMatcher and README

--- GridProviders/GridPuzzle/GridPuzzleGridCanvasProvider.py
+++ GridProviders/GridPuzzle/GridPuzzleGridCanvasProvider.py
@@ -10,8 +10,8 @@ class GridPuzzleGridCanvasProvider(GridPuzzleGridProvider):
     @staticmethod
     def _get_canvas_data(html_page: str) -> tuple[list[str], int]:
         html_string = BeautifulSoup(html_page, 'html.parser').prettify()
-        size = int(re.search(r'gpl\.Size = (\d+);', html_string).group(1))
-        pqq_base64 = re.search(r'gpl\.pqq = "(.*?)";', html_string).group(1)
+        size = int(re.search(r'gpl\.([Ss]ize) = (\d+);', html_string).group(2))
+        pqq_base64 = re.search(r'gpl\.pq{1,2} = "(.*?)";', html_string).group(1)
         pqq_raw = base64.b64decode(pqq_base64[3:])
         pqq_string = pqq_raw.decode('utf-8', errors='ignore')
         pqq_string_list = pqq_string.split('|')
--- README.md
+++ README.md
@@ -17,6 +17,7 @@ https://gridpuzzle.com/koburin/evil-12
 https://gridpuzzle.com/no-four-in-row/evil-12  
 https://gridpuzzle.com/number-chain/size-10  
 https://gridpuzzle.com/pure-loop/size-12  
+https://gridpuzzle.com/slitherlink/evil-12  
 https://gridpuzzle.com/snake/evil-12  
 https://gridpuzzle.com/str8ts/evil-9  
 https://gridpuzzle.com/traffic-lights/evil-12  
