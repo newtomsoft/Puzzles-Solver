f03dd402f4538e455e891124141f778df301e3f0
Thomas <thomas@newtomsoft.fr>
2025-06-18 12:54:08 +0200

    fix: add custom base64 decoding for canvas data

--- GridProviders/GridPuzzle/GridPuzzleGridCanvasProvider.py
+++ GridProviders/GridPuzzle/GridPuzzleGridCanvasProvider.py
@@ -11,8 +11,19 @@ class GridPuzzleGridCanvasProvider(GridPuzzleGridProvider):
     def _get_canvas_data(html_page: str) -> tuple[list[str], int]:
         html_string = BeautifulSoup(html_page, 'html.parser').prettify()
         size = int(re.search(r'gpl\.([Ss]ize) = (\d+);', html_string).group(2))
-        pqq_base64 = re.search(r'gpl\.pq{1,2} = "(.*?)";', html_string).group(1)
-        pqq_raw = base64.b64decode(pqq_base64[3:])
-        pqq_string = pqq_raw.decode('utf-8', errors='ignore')
+        pqq = re.search(r'gpl\.pq{1,2} = "(.*?)";', html_string).group(1)
+        pqq_string = GridPuzzleGridCanvasProvider._decode_if_custom_base64(pqq)
         pqq_string_list = pqq_string.split('|')
         return pqq_string_list, size
+
+    @staticmethod
+    def _decode_if_custom_base64(string: str) -> str:
+        if len(string) < 4 or not GridPuzzleGridCanvasProvider._is_valid_base64(string[3:]):
+            return string
+
+        decoded = base64.b64decode(string[3:])
+        return decoded.decode('utf-8', errors='ignore')
+
+    @staticmethod
+    def _is_valid_base64(string: str) -> bool:
+        return len(string) % 4 == 0 and bool(re.match('^[A-Za-z0-9+/]*={0,2}$', string))
