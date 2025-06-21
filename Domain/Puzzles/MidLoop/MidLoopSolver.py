6e66275e1b4e4e1f0354f7236af5c1d8c8097b33
Thomas <thomas@newtomsoft.fr>
2025-06-21 23:22:08 +0200

    feat(MidLoop): add solver, grid provider, and URL integration
    Introduce `MidLoopSolver` for solving Mid-Loop puzzles, along with `GridPuzzleMidLoopGridProvider` for grid parsing. Update `UrlPatternMatcher` to support Mid-Loop puzzle URLs.

---
+++ Domain/Puzzles/MidLoop/MidLoopSolver.py
@@ -0,0 +1,6 @@
+﻿from Domain.Board.Position import Position
+
+
+class MidLoopSolver:
+    def __init__(self, grid_size: tuple[int, int], circles_positions: dict[int, Position]):
+        pass
---
+++ GridProviders/GridPuzzle/GridPuzzleMidLoopGridProvider.py
@@ -0,0 +1,25 @@
+﻿from playwright.sync_api import BrowserContext
+
+from Domain.Board.Grid import Grid
+from Domain.Board.Position import Position
+from GridProviders.GridProvider import GridProvider
+from GridProviders.GridPuzzle.GridPuzzleGridCanvasProvider import GridPuzzleGridCanvasProvider
+from GridProviders.PlaywrightGridProvider import PlaywrightGridProvider
+
+
+class GridPuzzleMidLoopGridProvider(GridProvider, PlaywrightGridProvider, GridPuzzleGridCanvasProvider):
+    def get_grid(self, url: str):
+        return self.with_playwright(self.scrap_grid, url)
+
+    def scrap_grid(self, browser: BrowserContext, url):
+        html_page = self.get_html(browser, url)
+        pqq_string_list, matrix_size = self._get_canvas_data(html_page)
+        large_size = matrix_size * 2 - 1
+        large_matrix = [[False if pqq_string_list[i * large_size + j] == '.' else True for j in range(large_size)] for i in range(large_size)]
+        large_grid = Grid(large_matrix)
+
+        circles_positions: dict[int, Position] = {}
+        for i, position in enumerate([position/2 for position, value in large_grid if value]):
+            circles_positions[i + 1] = position
+
+        return (matrix_size, matrix_size), circles_positions
