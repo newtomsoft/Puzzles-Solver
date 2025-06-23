a41b2066cadf46bb53675def718df1e4d7ce5b61
Thomas <thomas@newtomsoft.fr>
2025-06-23 21:11:01 +0200

    feat(TilePaint): add player support and integrate into UrlPatternMatcher
    Introduce `GridPuzzleTilePaintPlayer` for handling TilePaint puzzles with tile clicking logic. Update `UrlPatternMatcher` to handle TilePaint puzzle URLs. Add TilePaint example to `README.md`.

---
+++ GridPlayers/GridPuzzle/GridPuzzleTilePaintPlayer.py
@@ -0,0 +1,22 @@
+﻿from Domain.Board.Grid import Grid
+from Domain.Board.Position import Position
+from GridPlayers.PlaywrightPlayer import PlaywrightPlayer
+
+
+class GridPuzzleTilePaintPlayer(PlaywrightPlayer):
+    game_name = "tile_paint"
+    def play(self, data: tuple[Grid, dict[int, frozenset[Position]]]):
+        grid_solution = data[0]
+        tiles = data[1]
+
+        page = self.browser.pages[0]
+        video, rectangle = self._get_data_video_viewport(page)
+
+        cells = page.query_selector_all("div.g_cell")
+        for position in [next(iter(positions)) for positions in tiles.values()]:
+            if grid_solution[position]:
+                index = position.r * grid_solution.columns_number + position.c
+                cells[index].click()
+
+        self.close()
+        self._process_video(video, rectangle, 0)
