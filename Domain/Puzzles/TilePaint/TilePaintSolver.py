92f3ec4c180dff9ef7c93a200636d0dd0fdd689c
Thomas <thomas@newtomsoft.fr>
2025-06-23 20:47:38 +0200

    feat(TilePaint): add solver, grid provider, and URL integration
    Introduce `TilePaintSolver` for solving TilePaint puzzles, along with `GridPuzzleTilePaintGridProvider` for grid parsing. Integrate TilePaint support into `UrlPatternMatcher` to handle relevant puzzle URLs. Include unit tests to validate solver functionality with various grid sizes and configurations.

---
+++ Domain/Puzzles/TilePaint/TilePaintSolver.py
@@ -0,0 +1,76 @@
+﻿from ortools.sat.python import cp_model
+
+from Domain.Board.Grid import Grid
+from Domain.Board.Position import Position
+from Domain.Puzzles.GameSolver import GameSolver
+
+
+class TilePaintSolver(GameSolver):
+    def __init__(self, grid: Grid, row_sums: list[int], column_sums: list[int]):
+        self._grid = grid
+        self.row_sums = row_sums
+        self._column_sums = column_sums
+        self.rows_number = self._grid.rows_number
+        self.columns_number = self._grid.columns_number
+        if self.rows_number != self.columns_number:
+            raise ValueError("The grid must be square")
+        if self.rows_number < 4:
+            raise ValueError("The grid must be at least 4x4")
+        self._tiles = self._grid.get_regions()
+        if len(self._tiles) < 2:
+            raise ValueError("The grid must have at least 2 regions")
+        self._model = cp_model.CpModel()
+        self._solver = cp_model.CpSolver()
+        self._grid_vars = None
+        self._previous_solution = None
+
+    def get_solution(self) -> tuple[Grid, dict[int, frozenset[Position]]]:
+        self._grid_vars = Grid([[self._model.NewBoolVar(f"grid_{r}_{c}") for c in range(self.columns_number)] for r in range(self.rows_number)])
+        self._add_constrains()
+        self._previous_solution = self._compute_solution()
+        return self._previous_solution, self._tiles
+
+    def get_other_solution(self) -> tuple[Grid, dict[int, frozenset[Position]]]:
+        previous_solution_literals = []
+        for position, value in self._previous_solution:
+            temp_var = self._model.NewBoolVar(f"prev_{position.r}_{position.c}")
+            self._model.Add(self._grid_vars[position] == value).OnlyEnforceIf(temp_var)
+            self._model.Add(self._grid_vars[position] != value).OnlyEnforceIf(temp_var.Not())
+            previous_solution_literals.append(temp_var)
+
+        if previous_solution_literals:
+            self._model.AddBoolOr([lit.Not() for lit in previous_solution_literals])
+
+        self._previous_solution = self._compute_solution()
+        return self._previous_solution, self._tiles
+
+    def _compute_solution(self) -> Grid :
+        status = self._solver.Solve(self._model)
+        if status != cp_model.OPTIMAL and status != cp_model.FEASIBLE:
+            return Grid.empty()
+
+        grid = Grid([[self._solver.Value(self._grid_vars[Position(i, j)]) for j in range(self.columns_number)] for i in range(self.rows_number)])
+        return grid
+
+    def _add_constrains(self):
+        self._add_sum_constraints()
+        self._add_tiles_constraints()
+
+    def _add_sum_constraints(self):
+        for i, row in enumerate(self._grid_vars.matrix):
+            if self.row_sums[i] == -1:
+                continue
+            self._model.Add(sum(row) == self.row_sums[i])
+
+        for i in range(self.columns_number):
+            if self._column_sums[i] == -1:
+                continue
+            column_vars = [self._grid_vars[Position(r, i)] for r in range(self.rows_number)]
+            self._model.Add(sum(column_vars) == self._column_sums[i])
+
+    def _add_tiles_constraints(self):
+        for positions in self._tiles.values():
+            positions_list = list(positions)
+            first_position = positions_list[0]
+            for position in positions_list[1:]:
+                self._model.Add(self._grid_vars[first_position] == self._grid_vars[position])
---
+++ Domain/Puzzles/TilePaint/tests/TilePaintSolver_test.py
@@ -0,0 +1,108 @@
+﻿import unittest
+from unittest import TestCase
+
+from Domain.Board.Grid import Grid
+from Domain.Puzzles.TilePaint.TilePaintSolver import TilePaintSolver
+
+
+class TilePaintSolverTests(TestCase):
+    def test_solution_basic_grid(self):
+        grid = Grid([
+            [1, 2, 2, 2],
+            [1, 1, 1, 2],
+            [3, 1, 1, 1],
+            [3, 3, 3, 3]
+        ])
+        row_sums = [3, -1, 1, 4]
+        column_sums = [-1, 2, 2, 3]
+        game_solver = TilePaintSolver(grid, row_sums, column_sums)
+        solution, _ = game_solver.get_solution()
+        expected_solution = Grid([
+            [0, 1, 1, 1],
+            [0, 0, 0, 1],
+            [1, 0, 0, 0],
+            [1, 1, 1, 1]
+        ])
+        self.assertEqual(expected_solution, solution)
+        other_solution, _ = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+    def test_solution_8x8_5jenr(self):
+        #  https://gridpuzzle.com/tilepaint/5jenr
+        grid = Grid([
+            [1, 2, 3, 3, 4, 4, 5, 5],
+            [1, 2, 6, 3, 3, 7, 5, 5],
+            [2, 2, 6, 8, 8, 7, 9, 9],
+            [10, 11, 12, 8, 8, 9, 9, 13],
+            [10, 11, 12, 14, 15, 15, 16, 13],
+            [10, 10, 12, 14, 14, 15, 16, 16],
+            [17, 18, 12, 19, 20, 15, 21, 21],
+            [17, 18, 19, 19, 20, 20, 21, 21]
+        ])
+        row_sums = [5, 4, -1, 4, -1, 4, -1, 5]
+        column_sums = [-1, 3, 2, -1, 4, -1, -1, 7]
+        game_solver = TilePaintSolver(grid, row_sums, column_sums)
+        solution, _ = game_solver.get_solution()
+        expected_solution = Grid([
+            [1, 0, 0, 0, 1, 1, 1, 1],
+            [1, 0, 1, 0, 0, 0, 1, 1],
+            [0, 0, 1, 0, 0, 0, 1, 1],
+            [1, 0, 0, 0, 0, 1, 1, 1],
+            [1, 0, 0, 1, 0, 0, 0, 1],
+            [1, 1, 0, 1, 1, 0, 0, 0],
+            [0, 1, 0, 0, 1, 0, 1, 1],
+            [0, 1, 0, 0, 1, 1, 1, 1]
+        ])
+        self.assertEqual(expected_solution, solution)
+        other_solution, _ = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+    def test_solution_16x16_050yg(self):
+        #  https://gridpuzzle.com/tilepaint/050yg
+        grid = Grid([
+            [1, 1, 1, 2, 2, 2, 3, 3, 4, 4, 5, 5, 5, 5, 6, 6],
+            [7, 7, 1, 8, 8, 8, 3, 9, 9, 10, 10, 11, 12, 12, 13, 13],
+            [7, 7, 14, 8, 15, 16, 17, 17, 9, 18, 18, 11, 11, 11, 19, 19],
+            [20, 20, 14, 14, 15, 16, 21, 21, 22, 23, 23, 23, 24, 24, 25, 25],
+            [26, 27, 27, 14, 28, 28, 21, 21, 22, 22, 22, 29, 24, 24, 30, 30],
+            [26, 31, 32, 33, 33, 28, 28, 34, 34, 35, 35, 29, 36, 36, 37, 37],
+            [26, 31, 32, 32, 32, 38, 39, 34, 34, 35, 40, 40, 41, 36, 42, 42],
+            [43, 44, 44, 38, 38, 38, 39, 45, 45, 46, 47, 47, 41, 48, 48, 49],
+            [43, 50, 50, 51, 51, 51, 39, 52, 52, 46, 46, 53, 53, 53, 48, 49],
+            [54, 54, 50, 55, 55, 51, 56, 57, 58, 58, 59, 59, 60, 60, 61, 49],
+            [62, 63, 63, 55, 56, 56, 56, 57, 58, 64, 59, 65, 65, 65, 61, 61],
+            [62, 62, 63, 63, 66, 67, 68, 68, 64, 64, 69, 70, 70, 70, 71, 71],
+            [72, 73, 74, 74, 66, 67, 68, 75, 75, 76, 69, 77, 78, 79, 79, 80],
+            [72, 73, 74, 74, 66, 81, 81, 82, 82, 76, 83, 77, 78, 84, 79, 80],
+            [72, 73, 73, 85, 85, 85, 81, 86, 86, 76, 83, 77, 84, 84, 84, 87],
+            [88, 88, 88, 89, 89, 85, 86, 86, 90, 90, 90, 91, 91, 92, 92, 87]
+        ])
+        row_sums = [12, 3, -1, 6, 12, 5, 9, 8, 9, 9, 4, 10, 5, 7, 13, 11]
+        column_sums = [9, -1, 9, -1, 11, -1, 11, 8, 7, 8, -1, 9, 4, -1, 8, 7]
+        game_solver = TilePaintSolver(grid, row_sums, column_sums)
+        solution, _ = game_solver.get_solution()
+        expected_solution = Grid([
+            [1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1],
+            [0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+            [1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1],
+            [1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+            [1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1],
+            [1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1],
+            [1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1],
+            [0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1],
+            [1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0],
+            [1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1],
+            [0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+            [0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0],
+            [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0],
+            [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0]
+        ])
+        self.assertEqual(expected_solution, solution)
+        other_solution, _ = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+
+if __name__ == '__main__':
+    unittest.main()
