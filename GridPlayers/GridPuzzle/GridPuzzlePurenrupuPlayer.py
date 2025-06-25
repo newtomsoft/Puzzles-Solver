5cbaa4a1a45b452d7770eb7c38116cb1338b60ba
Thomas <thomas@newtomsoft.fr>
2025-06-25 17:49:22 +0200

    refactor(GridPuzzlePlayers): replace `GridPuzzleCanvasPlayer` and `PlaywrightPlayer` with `GridPuzzleLoopAndBlackCellsPlayer` as base class

--- GridPlayers/GridPuzzle/GridPuzzlePurenrupuPlayer.py
+++ GridPlayers/GridPuzzle/GridPuzzlePurenrupuPlayer.py
@@ -1,15 +1,5 @@
-﻿from Domain.Board.IslandsGrid import IslandGrid
-from GridPlayers.GridPuzzle.GridPuzzleCanvasPlayer import GridPuzzleCanvasPlayer
-from GridPlayers.PlaywrightPlayer import PlaywrightPlayer
+﻿from GridPlayers.GridPuzzle.GridPuzzleLoopAndBlackCellsPlayer import GridPuzzleLoopAndBlackCellsPlayer
 
 
-class GridPuzzlePurenrupuPlayer(PlaywrightPlayer, GridPuzzleCanvasPlayer):
+class GridPuzzlePurenrupuPlayer(GridPuzzleLoopAndBlackCellsPlayer):
     game_name = "purenrupu"
-    def play(self, solution: IslandGrid):
-        cell_height, cell_width, page, x0, y0 = self._get_canvas_data(solution)
-        video, rectangle = self._get_data_video_viewport(page)
-
-        self._draw_loop(cell_height, cell_width, page, solution, x0, y0)
-
-        self.close()
-        self._process_video(video, rectangle)
