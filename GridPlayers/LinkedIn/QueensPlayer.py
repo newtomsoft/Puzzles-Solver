021c2d06eab15c8d57598f3a5132b90e369714a3
Thomas <thomas@newtomsoft.fr>
2025-06-25 12:05:38 +0200

    fix(QueensPlayer): update frame index to access the correct game board

--- GridPlayers/LinkedIn/QueensPlayer.py
+++ GridPlayers/LinkedIn/QueensPlayer.py
@@ -6,7 +6,7 @@ class QueensPlayer(PlaywrightPlayer):
     game_name = "queens"
     def play(self, solution: Grid):
         page = self.browser.pages[0]
-        frame = page.frames[0]
+        frame = page.frames[1]
         game_board = frame.wait_for_selector('.game-board')
         cells_divs = game_board.query_selector_all('div.queens-cell-with-border')
 
