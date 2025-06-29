99557f95fcdc550e4b58daf6e4b6764a07735c6c
thomas <thomas@newtomsoft.fr>
2025-06-29 12:24:28 +0200

    feat(GeradewegSolver): enhance length constraints and add extensive test cases
    Refactor length constraint logic in `GeradewegSolver` for improved readability and adaptability. Introduce `_used_directions` for dynamic direction handling. Add comprehensive test cases for validating solver on larger grid layouts and edge cases.

--- Domain/Puzzles/Geradeweg/tests/GeradewegSolver_test.py
+++ Domain/Puzzles/Geradeweg/tests/GeradewegSolver_test.py
@@ -51,6 +51,72 @@ class GeradewegSolverTests(TestCase):
         other_solution = game_solver.get_other_solution()
         self.assertEqual(IslandGrid.empty(), other_solution)
 
+    def test_solution_8x8_0mmy1(self):
+        # https://gridpuzzle.com/straight-loop/0mmy1
+        grid = Grid([
+            [_, 4, _, _, _, _, 1, _],
+            [_, _, _, _, _, _, _, _],
+            [_, _, 2, _, _, 3, _, _],
+            [_, _, _, _, _, _, _, _],
+            [_, 3, _, _, _, _, 4, _],
+            [7, _, _, 3, 3, _, _, 7],
+            [_, _, _, _, _, _, _, _],
+            [_, 2, _, _, _, _, 3, _]
+        ])
+        expected_solution_str = (
+            ' ┌───────────┐     ┌──┐ \n'
+            ' │  ┌──┐     └─────┘  │ \n'
+            ' │  │  │  ┌────────┐  │ \n'
+            ' │  │  └──┘  ┌──┐  │  │ \n'
+            ' │  └────────┘  │  │  │ \n'
+            ' │     ┌────────┘  │  │ \n'
+            ' │     │     ┌─────┘  │ \n'
+            ' └─────┘     └────────┘ '
+        )
+
+        game_solver = GeradewegSolver(grid)
+        solution = game_solver.get_solution()
+        self.assertEqual(expected_solution_str, str(solution))
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(IslandGrid.empty(), other_solution)
+
+    def test_solution_12x12_020r8(self):
+        # https://gridpuzzle.com/straight-loop/020r8
+        grid = Grid([
+            [_, _, 4, _, _, _, _, _, 4, _, _, _],
+            [6, _, _, _, _, 2, 2, 1, _, _, _, _],
+            [_, _, 2, _, _, _, _, 1, _, _, _, _],
+            [_, _, _, 3, _, _, _, _, _, _, 5, _],
+            [_, _, _, _, _, _, _, _, _, _, _, _],
+            [_, _, _, 1, _, _, 1, _, _, 3, _, _],
+            [_, _, _, _, _, _, _, 2, _, _, _, _],
+            [_, _, 2, 2, _, _, _, _, _, 2, _, 5],
+            [_, _, _, _, _, _, _, _, _, _, _, _],
+            [_, _, _, _, _, _, 5, _, _, 3, _, _],
+            [2, 1, _, 2, _, 4, _, _, 4, _, _, _],
+            [_, _, 3, _, _, _, _, _, _, _, _, _]
+        ])
+        expected_solution_str = (
+            ' ┌───────────┐  ┌──┐  ┌───────────┐ \n'
+            ' │        ┌──┘  │  │  └──┐     ┌──┘ \n'
+            ' │  ┌─────┘  ┌──┘  └──┐  │     │    \n'
+            ' │  └────────┘  ┌─────┘  └──┐  │    \n'
+            ' │  ┌─────┐     │  ┌──┐     │  │    \n'
+            ' │  │  ┌──┘     └──┘  │     │  │    \n'
+            ' └──┘  │  ┌────────┐  └─────┘  └──┐ \n'
+            ' ┌─────┘  │  ┌──┐  │     ┌─────┐  │ \n'
+            ' └─────┐  └──┘  │  │     │  ┌──┘  │ \n'
+            ' ┌──┐  │  ┌──┐  │  │     │  │     │ \n'
+            ' │  └──┘  │  │  │  │     │  │     │ \n'
+            ' └────────┘  └──┘  └─────┘  └─────┘ '
+        )
+
+        game_solver = GeradewegSolver(grid)
+        solution = game_solver.get_solution()
+        self.assertEqual(expected_solution_str, str(solution))
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(IslandGrid.empty(), other_solution)
+
 
 if __name__ == '__main__':
     unittest.main()
