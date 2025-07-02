1b7049348684c3ce0099d7c2438726096d8154d2
Thomas <thomas@newtomsoft.fr>
2025-07-02 19:40:11 +0200

    fix(GridPuzzleCanvasPlayer): generalize position path method and improve mouse event handling
    Simplify `connected_positions` logic by generalizing `_get_positions_from_position0_0` to `_get_positions_from_position`. Enhance input flexibility with new parameters. Add `mouse.up()` calls for consistent interaction handling.

--- GridPlayers/GridPuzzle/GridPuzzleCanvasPlayer.py
+++ GridPlayers/GridPuzzle/GridPuzzleCanvasPlayer.py
@@ -5,16 +5,17 @@ from Domain.Board.Position import Position
 
 class GridPuzzleCanvasPlayer:
     def _draw_loop(self, cell_height, cell_width, page, solution, x0, y0):
-        connected_positions = self._get_positions_from_position0_0(solution)
+        connected_positions = self._get_positions_from_position(solution, Position(0, 0), True)
         for index, position in enumerate(connected_positions[:-1]):
             next_position = connected_positions[index + 1]
             direction = position.direction_to(next_position)
             self._trace_direction_from_position(direction, position, page, cell_width, cell_height, x0, y0)
 
     @staticmethod
-    def _get_positions_from_position0_0(solution: IslandGrid) -> list[Position]:
-        connected_positions = solution.follow_path()
-        connected_positions.append(connected_positions[0])
+    def _get_positions_from_position(island_grid: IslandGrid, position: Position, end_with_first: bool=False) -> list[Position]:
+        connected_positions = island_grid.follow_path(position)
+        if end_with_first:
+            connected_positions.append(connected_positions[0])
         return connected_positions
 
     @staticmethod
@@ -35,9 +36,11 @@ class GridPuzzleCanvasPlayer:
             page.mouse.move(x0 + cell_width / 2 + position.c * cell_width, y0 + cell_height / 2 + position.r * cell_height)
             page.mouse.down()
             page.mouse.move(x0 + cell_width / 2 + (position.c - 1) * cell_width, y0 + cell_height / 2 + position.r * cell_height)
+            page.mouse.up()
             return
         if direction == Direction.up():
             page.mouse.move(x0 + cell_width / 2 + position.c * cell_width, y0 + cell_height / 2 + position.r * cell_height)
             page.mouse.down()
             page.mouse.move(x0 + cell_width / 2 + position.c * cell_width, y0 + cell_height / 2 + (position.r - 1) * cell_height)
+            page.mouse.up()
             return
