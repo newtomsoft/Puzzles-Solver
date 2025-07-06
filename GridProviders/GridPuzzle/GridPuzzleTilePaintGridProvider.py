525694b46a52e0ff567f76979c0ff7f8d9bc6dc4
Thomas <thomas@newtomsoft.fr>
2025-07-06 15:06:59 +0200

    refactor(GridPuzzle): extract bounded matrix logic into `make_bounded_matrix` method for improved readability and reusability

--- GridProviders/GridPuzzle/GridPuzzleTilePaintGridProvider.py
+++ GridProviders/GridPuzzle/GridPuzzleTilePaintGridProvider.py
@@ -14,37 +14,11 @@ class GridPuzzleTilePaintGridProvider(GridProvider, PlaywrightGridProvider, Grid
     def scrap_grid(self, browser: BrowserContext, url):
         html_page = self.get_html(browser, url, '.col-lg-12.col-md-12.col-12')
         soup, row_count, column_count, matrix, matrix_cells = self._get_grid_data(html_page)
-
-        opens = {'right', 'left', 'top', 'bottom'}
-        open_matrix = [[set() for _ in range(column_count)] for _ in range(row_count)]
-        cell_borders = [[set() for _ in range(column_count)] for _ in range(row_count)]
-        for i, cell in enumerate(matrix_cells):
-            row = i // column_count
-            col = i % column_count
-            cell_border_right, cell_border_bottom = [cls for cls in cell.get('class', []) if 'border' in cls][0].split('_')[1:3]
-            if row == 0:
-                cell_borders[row][col].add('top')
-            if row == row_count - 1:
-                cell_borders[row][col].add('bottom')
-            if col == 0:
-                cell_borders[row][col].add('top')
-            if col == column_count - 1:
-                cell_borders[row][col].add('right')
-            if cell_border_right == '1':
-                cell_borders[row][col].add('right')
-                if col != column_count - 1:
-                    cell_borders[row][col + 1].add('left')
-            if cell_border_bottom == '1':
-                cell_borders[row][col].add('bottom')
-                if row != row_count - 1:
-                    cell_borders[row + 1][col].add('top')
-
-            open_matrix[row][col] = opens - cell_borders[row][col]
-
+        bounded_matrix = self.make_bounded_matrix(row_count, column_count, matrix_cells)
         sums_v = [self.extract_sum_value('vl', row_count, soup) for row_count in range(1, row_count + 1)]
         sums_h = [self.extract_sum_value('ht', column_count, soup) for column_count in range(1, column_count + 1)]
 
-        return RegionsGrid(open_matrix), sums_v, sums_h
+        return RegionsGrid(bounded_matrix), sums_v, sums_h
 
     @staticmethod
     def extract_sum_value(name: str, column_count: int, soup: BeautifulSoup):
