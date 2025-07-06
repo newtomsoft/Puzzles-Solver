3e1a8778d8273ca0151700d31def285e4036a227
Thomas <thomas@newtomsoft.fr>
2025-07-06 15:06:59 +0200

    refactor(GridPuzzle): extract bounded matrix logic into `make_bounded_matrix` method for improved readability and reusability

--- GridProviders/GridPuzzle/GridPuzzleGridTagProvider.py
+++ GridProviders/GridPuzzle/GridPuzzleGridTagProvider.py
@@ -15,3 +15,32 @@ class GridPuzzleGridTagProvider(GridPuzzleGridProvider):
         column_count = row_count
         matrix = [[0 for _ in range(column_count)] for _ in range(row_count)]
         return soup, row_count, column_count, matrix, matrix_cells
+
+    @staticmethod
+    def make_bounded_matrix(row_count, column_count, matrix_cells):
+        opens = {'right', 'left', 'top', 'bottom'}
+        bounded_matrix = [[set() for _ in range(column_count)] for _ in range(row_count)]
+        cell_borders = [[set() for _ in range(column_count)] for _ in range(row_count)]
+        for i, cell in enumerate(matrix_cells):
+            row = i // column_count
+            col = i % column_count
+            cell_border_right, cell_border_bottom = [cls for cls in cell.get('class', []) if 'border' in cls][0].split('_')[1:3]
+            if row == 0:
+                cell_borders[row][col].add('top')
+            if row == row_count - 1:
+                cell_borders[row][col].add('bottom')
+            if col == 0:
+                cell_borders[row][col].add('top')
+            if col == column_count - 1:
+                cell_borders[row][col].add('right')
+            if cell_border_right == '1':
+                cell_borders[row][col].add('right')
+                if col != column_count - 1:
+                    cell_borders[row][col + 1].add('left')
+            if cell_border_bottom == '1':
+                cell_borders[row][col].add('bottom')
+                if row != row_count - 1:
+                    cell_borders[row + 1][col].add('top')
+
+            bounded_matrix[row][col] = opens - cell_borders[row][col]
+        return bounded_matrix
