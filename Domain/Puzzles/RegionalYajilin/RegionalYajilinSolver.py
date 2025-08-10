fc9dc2b192143a86daa852eecf49330cd1237c59
thomas <thomas@newtomsoft.fr>
2025-08-10 12:18:34 +0200

    feat(RegionalYajilin): integrate `RegionalYajilinSolver` and add comprehensive unit tests
    Introduce the Z3-powered `RegionalYajilinSolver` for solving Regional Yajilin puzzles. Implement solution and alternative solution methods, ensuring accurate handling of bridges and black cell constraints. Add unit tests for various puzzle difficulties to verify solver functionality.

---
+++ Domain/Puzzles/RegionalYajilin/RegionalYajilinSolver.py
@@ -0,0 +1,134 @@
+﻿from typing import Dict
+
+from z3 import ArithRef, Solver, Not, And, Or, Int, sat, Bool, Implies, is_true
+
+from Domain.Board.Direction import Direction
+from Domain.Board.Grid import Grid
+from Domain.Board.Island import Island
+from Domain.Board.IslandsGrid import IslandGrid
+from Domain.Board.Position import Position
+from Domain.Puzzles.GameSolver import GameSolver
+
+
+class RegionalYajilinSolver(GameSolver):
+    def __init__(self, blacks_count_grid: Grid, regions_grid: Grid):
+        self._regions_grid = regions_grid
+        self._positions_by_region_id = regions_grid.get_regions()
+        self._blacks_count_grid = blacks_count_grid
+        self._island_grid: IslandGrid | None = None
+        self._init_island_grid()
+        self._solver = Solver()
+        self._island_bridges_z3: Dict[Position, Dict[Direction, ArithRef]] = {}
+        self._previous_solution: IslandGrid | None = None
+
+    def _init_island_grid(self):
+        self._island_grid = IslandGrid([[Island(Position(r, c), 2) for c in range(self._regions_grid.columns_number)] for r in range(self._regions_grid.rows_number)])
+
+    def _init_solver(self):
+        self._island_bridges_z3 = {island.position: {direction: Int(f"{island.position}_{direction}") for direction in Direction.orthogonals()} for island in self._island_grid.islands.values() if island.bridges_count > 0}
+        for position in [position for position, _ in self._regions_grid if position not in self._island_bridges_z3]:
+            neighbors = self._regions_grid.neighbors_positions(position)
+            for neighbor in [neighbor for neighbor in neighbors if neighbor in self._island_bridges_z3]:
+                direction = neighbor.direction_to(position)
+                self._solver.add(self._island_bridges_z3[neighbor][direction] == 0)
+
+        self._black_cells_z3 = {position: Bool(f'p{position}') for position, _ in self._regions_grid if position in self._island_bridges_z3}
+        self._add_constraints()
+
+    def get_solution(self) -> IslandGrid:
+        if not self._solver.assertions():
+            self._init_solver()
+
+        solution, _ = self._ensure_all_islands_connected()
+        return solution
+
+    def _ensure_all_islands_connected(self) -> tuple[IslandGrid, int]:
+        proposition_count = 0
+        while self._solver.check() == sat:
+            model = self._solver.model()
+            proposition_count += 1
+            for position, direction_bridges in self._island_bridges_z3.items():
+                for direction, bridges_number in [(direction, model.eval(bridges).as_long()) for direction, bridges in direction_bridges.items() if position.after(direction) in self._island_bridges_z3]:
+                    self._island_grid[position].set_bridge_to_position(self._island_grid[position].direction_position_bridges[direction][0], bridges_number)
+                self._island_grid[position].set_bridges_count_according_to_directions_bridges()
+
+            connected_positions = self._island_grid.get_connected_positions(exclude_without_bridge=True)
+            if len(connected_positions) == 1:
+                for position, var in self._black_cells_z3.items():
+                    if is_true(model.eval(var)):
+                        self._island_grid.set_value(position, '■')
+
+                self._previous_solution = self._island_grid
+                return self._island_grid, proposition_count
+
+            not_loop_constraints = []
+            for positions in connected_positions:
+                cell_constraints = []
+                for position in positions:
+                    for direction, (_, value) in self._island_grid[position].direction_position_bridges.items():
+                        cell_constraints.append(self._island_bridges_z3[position][direction] == value)
+                not_loop_constraints.append(Not(And(cell_constraints)))
+            self._solver.add(And(not_loop_constraints))
+            self._init_island_grid()
+
+        return IslandGrid.empty(), proposition_count
+
+    def get_other_solution(self):
+        previous_solution_constraints = []
+        for island in [island for island in self._previous_solution.islands.values() if island.position in self._island_bridges_z3]:
+            for direction, (_, value) in island.direction_position_bridges.items():
+                previous_solution_constraints.append(self._island_bridges_z3[island.position][direction] == value)
+        self._solver.add(Not(And(previous_solution_constraints)))
+
+        self._init_island_grid()
+        return self.get_solution()
+
+    def _add_constraints(self):
+        self._add_initial_constraints()
+        self._add_black_cell_constraints()
+        self._add_no_adjacent_black_constraint()
+        self._add_opposite_bridges_constraints()
+        self._add_bridges_sum_constraints()
+
+    def _add_initial_constraints(self):
+        constraints = [Or(direction_bridges == 0, direction_bridges == 1) for _island_bridges_z3 in self._island_bridges_z3.values() for direction_bridges in _island_bridges_z3.values()]
+        self._solver.add(constraints)
+        constraints_border_up = [self._island_bridges_z3[Position(0, c)][Direction.up()] == 0 for c in range(self._island_grid.columns_number) if Position(0, c) in self._island_bridges_z3]
+        constraints_border_down = [self._island_bridges_z3[Position(self._island_grid.rows_number - 1, c)][Direction.down()] == 0 for c in range(self._island_grid.columns_number) if Position(self._island_grid.rows_number - 1, c) in self._island_bridges_z3]
+        constraints_border_right = [self._island_bridges_z3[Position(r, self._island_grid.columns_number - 1)][Direction.right()] == 0 for r in range(self._island_grid.rows_number) if Position(r, self._island_grid.columns_number - 1) in self._island_bridges_z3]
+        constraints_border_left = [self._island_bridges_z3[Position(r, 0)][Direction.left()] == 0 for r in range(self._island_grid.rows_number) if Position(r, 0) in self._island_bridges_z3]
+        self._solver.add(constraints_border_down + constraints_border_up + constraints_border_right + constraints_border_left)
+
+    def _add_opposite_bridges_constraints(self):
+        for island in [island for island in self._island_grid.islands.values() if island.position in self._island_bridges_z3]:
+            for direction in [Direction.right(), Direction.down(), Direction.left(), Direction.up()]:
+                position_bridges = island.direction_position_bridges.get(direction)
+                if position_bridges is not None:
+                    other_position, _ = position_bridges
+                    if other_position not in self._island_bridges_z3:
+                        continue
+                    self._solver.add(self._island_bridges_z3[island.position][direction] == self._island_bridges_z3[other_position][direction.opposite])
+                else:
+                    self._solver.add(self._island_bridges_z3[island.position][direction] == 0)
+
+    def _add_black_cell_constraints(self):
+        for position, blacks_count in [(position, value) for position, value in self._blacks_count_grid if value >= 0]:
+            region_id = next(region_id for region_id, positions in self._positions_by_region_id.items() if position in positions)
+            region_positions = self._positions_by_region_id[region_id]
+            self._solver.add(sum([self._black_cells_z3[position] for position in region_positions if position in self._island_bridges_z3.keys()]) == blacks_count)
+
+    def _add_bridges_sum_constraints(self):
+        for position in [position for position, value in self._regions_grid]:
+            bridges_count_0 = sum([self._island_bridges_z3[position][direction] for direction in [Direction.right(), Direction.down(), Direction.left(), Direction.up()]]) == 0
+            bridges_count_2 = sum([self._island_bridges_z3[position][direction] for direction in [Direction.right(), Direction.down(), Direction.left(), Direction.up()]]) == 2
+            black_cell = self._black_cells_z3[position]
+            path_cell = Not(black_cell)
+            self._solver.add(Implies(black_cell, bridges_count_0))
+            self._solver.add(Implies(path_cell, bridges_count_2))
+
+    def _add_no_adjacent_black_constraint(self):
+        for position in [position for position, value in self._regions_grid]:
+            for neighbor_position in self._regions_grid.neighbors_positions(position):
+                if neighbor_position not in self._island_bridges_z3:
+                    continue
+                self._solver.add(Implies(self._black_cells_z3[position], Not(self._black_cells_z3[neighbor_position])))
---
+++ Domain/Puzzles/RegionalYajilin/tests/RegionalYajilinSolver_test.py
@@ -0,0 +1,122 @@
+﻿from unittest import TestCase
+
+from Domain.Board.Grid import Grid
+from Domain.Puzzles.RegionalYajilin.RegionalYajilinSolver import RegionalYajilinSolver
+
+_ = -1
+
+
+class RegionalYajilinSolverTests(TestCase):
+    def test_solution_5x5_easy_217gy(self):
+        """https://gridpuzzle.com/regional-yajilin/217gy"""
+        region_grid = Grid([
+            [1, 1, 1, 2, 2],
+            [1, 1, 1, 2, 2],
+            [1, 1, 1, 2, 2],
+            [1, 1, 1, 4, 4],
+            [3, 3, 3, 4, 4],
+        ])
+        blacks_count_grid = Grid([
+            [2, _, _, 2, _],
+            [_, _, _, _, _],
+            [_, _, _, _, _],
+            [_, _, _, 0, _],
+            [1, _, _, _, _],
+        ])
+
+        game_solver = RegionalYajilinSolver(blacks_count_grid, region_grid)
+        solution = game_solver.get_solution()
+        expected_solution_string = (
+            ' ■  ┌─────┐  ■ \n'
+            ' ┌──┘  ■  └──┐ \n'
+            ' │  ┌──┐  ■  │ \n'
+            ' │  │  └──┐  │ \n'
+            ' └──┘  ■  └──┘ '
+        )
+        self.assertEqual(expected_solution_string, str(solution))
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+    def test_solution_8x8_evil_01j11(self):
+        """https://gridpuzzle.com/regional-yajilin/01j11"""
+        region_grid = Grid([
+            [1, 1, 1, 1, 1, 1, 2, 2],
+            [3, 3, 3, 4, 4, 4, 4, 4],
+            [3, 3, 3, 4, 4, 4, 4, 4],
+            [5, 5, 5, 4, 4, 4, 4, 4],
+            [5, 5, 5, 6, 6, 7, 7, 7],
+            [8, 8, 9, 9, 9, 10, 10, 10],
+            [8, 8, 9, 9, 9, 11, 11, 12],
+            [8, 8, 9, 9, 9, 11, 11, 12]
+        ])
+        blacks_count_grid = Grid([
+            [1, _, _, _, _, _, 1, _],
+            [2, _, _, 3, _, _, _, _],
+            [_, _, _, _, _, _, _, _],
+            [_, _, _, _, _, _, _, _],
+            [_, _, _, _, _, 1, _, _],
+            [2, _, 2, _, _, 1, _, _],
+            [_, _, _, _, _, 0, _, 0],
+            [_, _, _, _, _, _, _, _],
+        ])
+
+        game_solver = RegionalYajilinSolver(blacks_count_grid, region_grid)
+        solution = game_solver.get_solution()
+        expected_solution_string = (
+            ' ┌─────┐  ■  ┌─────┐  ■ \n'
+            ' │  ■  └─────┘  ■  └──┐ \n'
+            ' └──┐  ■  ┌─────┐  ■  │ \n'
+            ' ■  │  ┌──┘  ■  └──┐  │ \n'
+            ' ┌──┘  │  ┌──┐  ■  └──┘ \n'
+            ' │  ■  └──┘  └─────┐  ■ \n'
+            ' └──┐  ■  ┌─────┐  └──┐ \n'
+            ' ■  └─────┘  ■  └─────┘ '
+        )
+        self.assertEqual(expected_solution_string, str(solution))
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+    def test_solution_10x10_evil_580d0(self):
+        """https://gridpuzzle.com/regional-yajilin/580d0"""
+        region_grid = Grid([
+            [1, 1, 1, 1, 2, 3, 3, 3, 4, 5],
+            [1, 1, 1, 1, 2, 2, 4, 4, 4, 5],
+            [6, 1, 7, 2, 2, 4, 4, 4, 4, 5],
+            [6, 7, 7, 7, 2, 8, 9, 9, 10, 10],
+            [11, 12, 7, 7, 13, 8, 8, 10, 10, 14],
+            [11, 12, 12, 13, 13, 8, 15, 15, 10, 14],
+            [12, 12, 16, 16, 13, 17, 15, 15, 15, 18],
+            [19, 20, 20, 20, 20, 17, 17, 15, 21, 18],
+            [19, 20, 20, 20, 17, 17, 21, 21, 21, 21],
+            [19, 20, 22, 22, 22, 17, 21, 21, 21, 21]
+        ])
+        blacks_count_grid = Grid([
+            [1, _, _, _, _, 1, _, _, 2, _],
+            [_, _, _, _, _, _, _, _, _, _],
+            [1, _, _, _, _, _, _, _, _, _],
+            [_, _, _, _, _, 1, 1, _, 1, _],
+            [_, 2, _, _, 1, _, _, _, _, 1],
+            [_, _, _, _, _, _, 1, _, _, _],
+            [_, _, 1, _, _, 1, _, _, _, _],
+            [1, 1, _, _, _, _, _, _, 2, _],
+            [_, _, _, _, _, _, _, _, _, _],
+            [_, _, _, _, _, _, _, _, _, _]
+        ])
+
+        game_solver = RegionalYajilinSolver(blacks_count_grid, region_grid)
+        solution = game_solver.get_solution()
+        expected_solution_string = (
+            ' ┌───────────┐  ■  ┌─────┐  ■ \n'
+            ' └─────┐  ■  └─────┘  ■  └──┐ \n'
+            ' ■  ┌──┘  ┌───────────┐  ■  │ \n'
+            ' ┌──┘  ■  └─────┐  ■  └──┐  │ \n'
+            ' │  ■  ┌──┐  ■  └──┐  ■  └──┘ \n'
+            ' └──┐  │  └──┐  ■  └─────┐  ■ \n'
+            ' ■  │  │  ■  └─────┐  ■  └──┐ \n'
+            ' ┌──┘  └─────┐  ■  └──┐  ■  │ \n'
+            ' └──┐  ■  ┌──┘  ┌─────┘  ┌──┘ \n'
+            ' ■  └─────┘  ■  └────────┘  ■ '
+        )
+        self.assertEqual(expected_solution_string, str(solution))
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
