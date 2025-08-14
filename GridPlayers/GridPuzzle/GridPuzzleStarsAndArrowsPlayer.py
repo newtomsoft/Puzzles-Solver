48e59c0faf0cc6dbf477b5d811e6be9a1d77c477
thomas <thomas@newtomsoft.fr>
2025-08-14 15:41:20 +0200

    feat(StarsAndArrows): add `GridPuzzleStarsAndArrowsPlayer`, `GridPuzzleStarsAndArrowsGridProvider`, and URL matcher integration
    Implement `GridPuzzleStarsAndArrowsPlayer` for gameplay logic and `GridPuzzleStarsAndArrowsGridProvider` for grid extraction using Playwright and BeautifulSoup. Extend `UrlPatternMatcher` to support Stars and Arrows puzzles.

---
+++ GridPlayers/GridPuzzle/GridPuzzleStarsAndArrowsPlayer.py
@@ -0,0 +1,17 @@
+﻿from Domain.Board.Grid import Grid
+from Domain.Board.Position import Position
+from GridPlayers.PlaywrightPlayer import PlaywrightPlayer
+
+
+class GridPuzzleStarsAndArrowsPlayer(PlaywrightPlayer):
+    game_name = "stars and arrows"
+    def play(self, solution: Grid):
+        page = self.browser.pages[0]
+        video, rectangle = self._get_data_video_viewport(page)
+
+        cells = page.query_selector_all("div.g_cell")
+        for position_index in [position.r * solution.columns_number + position.c for position, value in solution if value]:
+            cells[position_index].click()
+
+        self.close()
+        self._process_video(video, rectangle, 0)
---
+++ GridProviders/GridPuzzle/GridPuzzleStarsAndArrowsGridProvider.py
@@ -0,0 +1,45 @@
+from bs4 import BeautifulSoup
+from playwright.sync_api import BrowserContext
+
+from Domain.Board.Grid import Grid
+from GridProviders.GridProvider import GridProvider
+from GridProviders.GridPuzzle.GridPuzzleGridTagProvider import GridPuzzleGridTagProvider
+from GridProviders.PlaywrightGridProvider import PlaywrightGridProvider
+
+
+class GridPuzzleStarsAndArrowsGridProvider(GridProvider, PlaywrightGridProvider, GridPuzzleGridTagProvider):
+    def get_grid(self, url: str):
+        return self.with_playwright(self.scrap_grid, url)
+
+    def scrap_grid(self, browser: BrowserContext, url):
+        html_page = self.get_html(browser, url, '.col-lg-12.col-md-12.col-12')
+        soup, row_count, column_count, matrix, matrix_cells = self._get_grid_data(html_page)
+        arrow_map = {
+            'arrow_1': '→',
+            'arrow_2': '↘',
+            'arrow_3': '↓',
+            'arrow_4': '↙',
+            'arrow_5': '←',
+            'arrow_6': '↖',
+            'arrow_7': '↑',
+            'arrow_8': '↗',
+        }
+        for i, cell in enumerate(matrix_cells):
+            row = i // column_count
+            col = i % column_count
+            div = cell.find('div')
+            div_classes = div.get('class', ' ')
+            if div_classes:
+                div_class = div_classes[0]
+                matrix[row][col] = arrow_map.get(div_class, '')
+            else :
+                matrix[row][col] = ''
+
+        count_left = [self.extract_sum_value('vl', row_count, soup) for row_count in range(1, row_count + 1)]
+        count_up = [self.extract_sum_value('ht', column_count, soup) for column_count in range(1, column_count + 1)]
+
+        return Grid(matrix), {'up': count_up, 'left': count_left}
+
+    @staticmethod
+    def extract_sum_value(name: str, column_count: int, soup: BeautifulSoup):
+        return int(soup.find('div', id=f'{name}_{column_count}').text) if soup.find('div', id=f'{name}_{column_count}').text != '\xa0' else -1
