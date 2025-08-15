88f244d8484a7b73084d56a9de8e53096a3647b3
thomas <thomas@newtomsoft.fr>
2025-08-16 00:11:57 +0200

    refactor(CreekPlayer): simplify loop for processing grid solution

--- GridPlayers/GridPuzzle/GridPuzzleCreekPlayer.py
+++ GridPlayers/GridPuzzle/GridPuzzleCreekPlayer.py
@@ -9,7 +9,7 @@ class GridPuzzleCreekPlayer(PlaywrightPlayer):
         video, rectangle = self._get_data_video_viewport(page)
 
         cells = page.query_selector_all("div.g_cell")
-        for position, solution_value in [(position, solution_value) for position, solution_value in grid_solution if solution_value]:
+        for position in [position for position, solution_value in grid_solution if solution_value]:
             index = position.r * grid_solution.columns_number + position.c
             cells[index].click(click_count=2)
 
