b1e1a2de29544a684d53d215d0c20ad8bd546b85
Thomas <thomas@newtomsoft.fr>
2025-08-31 17:46:01 +0200

    feat(TatamibariSolver): add initial solver implementation and unit tests
    Introduce `TatamibariSolver` with methods `get_solution` and `get_other_solution`. Include unit tests to verify solution correctness for 4x4 grids across different puzzle configurations.

---
+++ Domain/Puzzles/Tatamibari/TatamibariSolver.py
@@ -0,0 +1,14 @@
+﻿from Board.Grid import Grid
+from Puzzles.GameSolver import GameSolver
+
+
+class TatamibariSolver(GameSolver):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def get_solution(self) -> Grid:
+        pass
+
+    def get_other_solution(self) -> Grid:
+        pass
+
---
+++ Domain/Puzzles/Tatamibari/tests/TatamibariSolver_test.py
@@ -0,0 +1,58 @@
+﻿import unittest
+from unittest import TestCase
+
+from Domain.Board.Grid import Grid
+from Puzzles.Tatamibari.TatamibariSolver import TatamibariSolver
+
+_ = ''
+M = '-'
+P = '+'
+I = '|'
+
+
+class TatamibariSolverTests(TestCase):
+    def test_solution_4x4_easy_lge62(self):
+        """https://gridpuzzle.com/tatamibari/lge62"""
+        grid = Grid([
+            [I, _, M, _],
+            [_, P, _, I],
+            [_, P, I, P],
+            [M, _, _, _]
+        ])
+        game_solver = TatamibariSolver(grid)
+
+        solution = game_solver.get_solution()
+        expected_solution = Grid([
+            [1, 2, 2, 3],
+            [1, 4, 5, 3],
+            [1, 6, 5, 7],
+            [8, 8, 8, 8]
+        ])
+        self.assertEqual(expected_solution, solution)
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+    def test_solution_4x4_evil_l4xjg(self):
+        """https://gridpuzzle.com/tatamibari/l4xjg"""
+        grid = Grid([
+            [M, _, _, P],
+            [_, _, M, _],
+            [_, _, M, _],
+            [P, _, _, I]
+        ])
+        game_solver = TatamibariSolver(grid)
+
+        solution = game_solver.get_solution()
+        expected_solution = Grid([
+            [1, 1, 1, 2],
+            [3, 3, 3, 3],
+            [4, 4, 4, 5],
+            [4, 4, 4, 5]
+        ])
+        self.assertEqual(expected_solution, solution)
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+
+if __name__ == '__main__':
+    unittest.main()
