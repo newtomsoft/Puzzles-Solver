d1468603e94c390ea5aa0bcc0a9239d5f4999645
Thomas <thomas@newtomsoft.fr>
2025-09-03 21:09:03 +0200

    test(TatamibariSolver): add tests for new Tatamibari puzzles

--- Domain/Puzzles/Tatamibari/tests/TatamibariSolver_test.py
+++ Domain/Puzzles/Tatamibari/tests/TatamibariSolver_test.py
@@ -53,6 +53,63 @@ class TatamibariSolverTests(TestCase):
         other_solution = game_solver.get_other_solution()
         self.assertEqual(Grid.empty(), other_solution)
 
+    def test_solution_6x6_evil_v8rq9(self):
+        """https://gridpuzzle.com/tatamibari/v8rq9"""
+        grid = Grid([
+            [_, _, M, M, _, P],
+            [_, _, M, _, _, _],
+            [_, _, _, _, M, _],
+            [_, _, _, _, _, I],
+            [_, M, P, _, _, M],
+            [P, _, _, M, _, M],
+        ])
+        game_solver = TatamibariSolver(grid)
 
-if __name__ == '__main__':
-    unittest.main()
+        solution = game_solver.get_solution()
+        expected_solution = Grid([
+            [1, 1, 1, 2, 2, 3],
+            [4, 4, 4, 4, 4, 4],
+            [5, 5, 5, 5, 5, 6],
+            [5, 5, 5, 5, 5, 6],
+            [7, 7, 8, 9, 9, 9],
+            [10, 11, 11, 11, 12, 12],
+        ])
+        self.assertEqual(expected_solution, solution)
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+    def test_solution_10x10_evil_095e1(self):
+        """https://gridpuzzle.com/tatamibari/095e1"""
+        grid = Grid([
+            [_, M, _, P, _, _, _, M, _, I],
+            [_, _, _, P, P, _, P, _, _, _],
+            [_, _, _, _, _, _, _, _, _, _],
+            [_, _, M, _, _, I, _, _, _, _],
+            [_, _, M, _, _, _, _, _, _, _],
+            [_, _, _, P, _, P, P, _, _, I],
+            [I, _, _, _, _, _, _, _, M, _],
+            [_, P, P, _, _, _, I, I, P, _],
+            [M, _, _, _, M, _, _, _, I, _],
+            [M, _, _, M, _, P, _, P, _, I],
+        ])
+        game_solver = TatamibariSolver(grid)
+
+        solution = game_solver.get_solution()
+        expected_solution = Grid([
+            [1, 1, 1, 2, 6, 6, 3, 3, 4, 4],
+            [1, 1, 1, 5, 6, 6, 7, 7, 4, 4],
+            [8, 8, 8, 8, 8, 9, 7, 7, 4, 4],
+            [8, 8, 8, 8, 8, 9, 13, 13, 13, 14],
+            [10, 10, 10, 11, 11, 9, 13, 13, 13, 14],
+            [10, 10, 10, 11, 11, 12, 13, 13, 13, 14],
+            [15, 16, 16, 16, 16, 16, 16, 16, 16, 16],
+            [15, 17, 18, 23, 23, 23, 19, 20, 21, 29],
+            [22, 22, 22, 23, 23, 23, 19, 20, 24, 29],
+            [25, 25, 26, 26, 26, 27, 19, 28, 24, 29],
+        ])
+        self.assertEqual(expected_solution, solution)
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+        if __name__ == '__main__':
+            unittest.main()
