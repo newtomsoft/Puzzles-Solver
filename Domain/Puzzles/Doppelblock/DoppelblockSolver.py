d27adad0130b5b1c46e14e7fb4f6390ae13ea940
Thomas <thomas@newtomsoft.fr>
2025-10-25 21:33:24 +0200

    feat(DoppelblockSolver): introduce Z3-based solver and add unit tests
    Add `DoppelblockSolver` class to solve Doppelblock puzzles using Z3 constraints. Implement initial constraints, distinct numbers, black cell placement, and clues handling. Add comprehensive unit tests to validate solutions and ensure uniqueness of alternative solutions.

---
+++ Domain/Puzzles/Doppelblock/DoppelblockSolver.py
@@ -0,0 +1,77 @@
+﻿from z3 import Solver, Not, And, unsat, Int, If, Distinct
+
+from Domain.Board.Grid import Grid
+from Domain.Board.Position import Position
+from Domain.Puzzles.GameSolver import GameSolver
+
+
+class DoppelblockSolver(GameSolver):
+    empty = None
+    black_value = 0
+
+    def __init__(self, grid: Grid, row_sums_clues: list, column_sums_clues: list):
+        self._grid = grid
+        if self._grid.rows_number != self._grid.columns_number:
+            raise ValueError("The grid must be square")
+        self.rows_number = self._grid.rows_number
+        self.columns_number = self._grid.columns_number
+        self._row_sums_clues = row_sums_clues
+        self._column_sums_clues = column_sums_clues
+        self._solver = Solver()
+        self._grid_z3: Grid | None = None
+        self._previous_solution: Grid | None = None
+
+    def get_solution(self) -> Grid:
+        self._grid_z3 = Grid([[Int(f"cell_{r}_{c}") for c in range(self.columns_number)] for r in range(self.rows_number)])
+        self._add_constrains()
+        self._previous_solution = self._compute_solution()
+        return self._previous_solution
+
+    def get_other_solution(self) -> Grid:
+        self._solver.add(Not(And([self._grid_z3[position] == value for position, value in self._previous_solution])))
+        self._previous_solution = self._compute_solution()
+        return self._previous_solution
+
+    def _compute_solution(self):
+        if self._solver.check() == unsat:
+            return Grid.empty()
+        model = self._solver.model()
+        grid = Grid([[model.eval(self._grid_z3[Position(i, j)]).as_long() for j in range(self.columns_number)] for i in range(self.rows_number)])
+        return grid
+
+    def _add_constrains(self):
+        self._add_initials_constraints()
+        self._add_two_black_cells_and_distincts_numbers_constraints()
+        self._add_sums_clues_constraints()
+
+    def _add_initials_constraints(self):
+        for position, value in self._grid:
+            if value != self.empty:
+                self._solver.add(self._grid_z3[position] == value)
+            else:
+                self._solver.add(self._grid_z3[position] >= 0, self._grid_z3[position] <= self.rows_number - 2)
+
+    def _add_two_black_cells_and_distincts_numbers_constraints(self):
+        for r in range(self.rows_number):
+            row_vars = [self._grid_z3[Position(r, c)] for c in range(self.columns_number)]
+            self._solver.add(sum((cell == self.black_value for cell in row_vars)) == 2)
+
+            non_zero_vars = []
+            for c in range(self.columns_number):
+                non_zero_var = If(row_vars[c] != self.black_value, row_vars[c], -c - 1)
+                non_zero_vars.append(non_zero_var)
+            self._solver.add(Distinct(non_zero_vars))
+
+        for c in range(self.columns_number):
+            col_vars = [self._grid_z3[Position(r, c)] for r in range(self.rows_number)]
+            self._solver.add(sum((cell == self.black_value for cell in col_vars)) == 2)
+
+            non_zero_vars = []
+            for r in range(self.rows_number):
+                non_zero_var = If(col_vars[r] != 0, col_vars[r], -r - 1)
+                non_zero_vars.append(non_zero_var)
+            self._solver.add(Distinct(non_zero_vars))
+
+    def _add_sums_clues_constraints(self):
+        for row, clue in ((row, clue) for row, clue in enumerate(self._row_sums_clues) if clue != self.empty):
+            pass
---
+++ Domain/Puzzles/Doppelblock/tests/DoppelblockSolver_test.py
@@ -0,0 +1,56 @@
+﻿import unittest
+from unittest import TestCase
+
+from Domain.Board.Grid import Grid
+from Puzzles.Doppelblock.DoppelblockSolver import DoppelblockSolver
+
+_ = DoppelblockSolver.empty
+B = DoppelblockSolver.black_value
+
+class DoppelblockSolverTests(TestCase):
+    def test_get_solution_without_sums_clues(self):
+        grid = Grid([
+            [1, _, 2, _],
+            [_, 2, _, _],
+            [2, _, 1, _],
+            [_, _, _, _],
+        ])
+        row_sums_clues = [_, _, _, _]
+        column_sums_clues = [_, _, _, _]
+
+        expected_grid = Grid([
+            [1, 0, 2, 0],
+            [0, 2, 0, 1],
+            [2, 0, 1, 0],
+            [0, 1, 0, 2],
+        ])
+        game_solver = DoppelblockSolver(grid, row_sums_clues, column_sums_clues)
+        solution = game_solver.get_solution()
+        self.assertEqual(expected_grid, solution)
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+    def test_get_solution_with_sums_clues(self):
+        grid = Grid([
+            [_, B, _, B],
+            [B, _, B, _],
+            [_, B, _, B],
+            [B, _, B, _],
+        ])
+        row_sums_clues = [2, 2, 1, 1]
+        column_sums_clues = [2, 2, 1, 1]
+
+        expected_grid = Grid([
+            [1, 0, 2, 0],
+            [0, 2, 0, 1],
+            [2, 0, 1, 0],
+            [0, 1, 0, 2],
+        ])
+        game_solver = DoppelblockSolver(grid, row_sums_clues, column_sums_clues)
+        solution = game_solver.get_solution()
+        self.assertEqual(expected_grid, solution)
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+if __name__ == '__main__':
+    unittest.main()
