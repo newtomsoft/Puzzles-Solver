e205a6b48befaeef8a001dea60ce62c348b6f428
Thomas <thomas@newtomsoft.fr>
2025-10-25 17:52:29 +0200

    feat(From1ToXSolver): add solver, support, and integration for From1ToX puzzles
    Introduce `From1ToXSolver` to solve From1ToX puzzles using Z3-based constraints. Add `GridPuzzleFrom1ToXGridProvider` for grid extraction and `GridPuzzleFrom1ToXPlayer` for automated gameplay. Update `UrlPatternMatcher` for puzzle recognition and extend unit tests with comprehensive coverage. Update README with a new puzzle URL.

---
+++ Domain/Puzzles/From1ToX/tests/From1ToXSolver_test.py
@@ -0,0 +1,42 @@
+﻿import unittest
+from unittest import TestCase
+
+from Domain.Board.Grid import Grid
+from Puzzles.From1ToX.From1ToXSolver import From1ToXSolver
+
+_ = 0
+
+
+class From1ToXSolverTests(TestCase):
+    def test_get_solution_4x4_evil_vrdjk(self):
+        """https://gridpuzzle.com/from1tox/vrdjk"""
+        grid = Grid([
+            [_, _, _, _],
+            [_, _, _, _],
+            [_, _, _, _],
+            [_, _, _, _],
+        ])
+        region_grid = Grid([
+            [1, 1, 1, 1],
+            [2, 2, 1, 1],
+            [3, 2, 2, 4],
+            [3, 3, 3, 4]
+        ])
+        row_clues = [16, _, 8, 8]
+        column_clues = [14, 11, 12, _]
+
+        expected_grid = Grid([
+            [6, 2, 5, 3],
+            [4, 3, 4, 1],
+            [3, 2, 1, 2],
+            [1, 4, 2, 1],
+        ])
+        game_solver = From1ToXSolver(grid, region_grid, row_clues, column_clues)
+        solution = game_solver.get_solution()
+        self.assertEqual(expected_grid, solution)
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ GridPlayers/GridPuzzle/GridPuzzleFrom1ToXPlayer.py
@@ -0,0 +1,21 @@
+﻿from Domain.Board.Grid import Grid
+from GridPlayers.PlaywrightPlayer import PlaywrightPlayer
+
+
+class GridPuzzleFrom1ToXPlayer(PlaywrightPlayer):
+    game_name = "From1ToX"
+
+    def play(self, solution: Grid):
+        page = self.browser.pages[0]
+        video, rectangle = self._get_data_video_viewport(page)
+
+        cells = page.query_selector_all("div.g_cell")
+        for position, solution_value in [(position, solution[position]) for position, value in solution if value != 0]:
+            index = position.r * solution.columns_number + position.c
+            if cells[index].text_content().strip() == str(solution_value):
+                continue
+            cells[index].click()
+            page.keyboard.press(str(solution_value))
+
+        self.close()
+        self._process_video(video, rectangle, 0)
