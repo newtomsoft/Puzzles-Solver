f9c1a8ace5c1272fd1ad1c0bbfe8be0a6286f797
Thomas Vuille <tvuille@blitzbs.com>
2025-10-28 15:20:41 +0100

    feat(NumberCross): add support for solving and playing Number Cross puzzles

---
+++ Domain/Puzzles/NumberCross/NumberCrossSolver.py
@@ -0,0 +1,66 @@
+﻿from z3 import Solver, Not, And, unsat, If, Bool, is_true
+
+from Domain.Board.Grid import Grid
+from Domain.Board.Position import Position
+from Domain.Puzzles.GameSolver import GameSolver
+
+
+class NumberCrossSolver(GameSolver):
+    black_value = False  # must stay False
+    empty = None # must stay None
+
+    def __init__(self, input_grid: Grid, row_sums_clues: list, column_sums_clues: list):
+        self._input_grid = input_grid
+        if self._input_grid.rows_number != self._input_grid.columns_number:
+            raise ValueError("The grid must be square")
+        self.rows_number = self._input_grid.rows_number
+        self.columns_number = self._input_grid.columns_number
+        self._row_sums_clues = row_sums_clues
+        self._column_sums_clues = column_sums_clues
+        self._solver = Solver()
+        self._grid_z3: Grid | None = None
+        self._previous_solution: Grid | None = None
+
+    def get_solution(self) -> Grid:
+        self._grid_z3 = Grid([[Bool(f"cell_{r}_{c}") for c in range(self.columns_number)] for r in range(self.rows_number)])
+        self._add_constrains()
+        self._previous_solution = self._compute_solution()
+        return self._previous_solution
+
+    def get_other_solution(self) -> Grid:
+        self._solver.add(Not(And([self._grid_z3[position] == value for position, value in self._previous_solution if value == self.black_value])))
+        self._previous_solution = self._compute_solution()
+        return self._previous_solution
+
+    def _compute_solution(self):
+        if self._solver.check() == unsat:
+            return Grid.empty()
+        model = self._solver.model()
+        grid = Grid(
+            [[self._input_grid[Position(i, j)] if is_true(model.eval(self._grid_z3[Position(i, j)])) else self.black_value for j in range(self.columns_number)]
+             for i in range(self.rows_number)])
+        return grid
+
+    def _add_constrains(self):
+        self._add_initials_constraints()
+        self._add_sums_clues_constraints()
+
+    def _add_initials_constraints(self):
+        for position, value in self._input_grid:
+            if value == self.black_value:
+                self._solver.add(self._grid_z3[position] == self.black_value)
+
+    def _add_sums_clues_constraints(self):
+        for r, clue in enumerate(self._row_sums_clues):
+            if clue != self.empty:
+                row_positions = [Position(r, c) for c in range(self.columns_number)]
+                self._add_sum_for_line_constraint(row_positions, clue)
+
+        for c, clue in enumerate(self._column_sums_clues):
+            if clue != self.empty:
+                col_positions = [Position(r, c) for r in range(self.rows_number)]
+                self._add_sum_for_line_constraint(col_positions, clue)
+
+    def _add_sum_for_line_constraint(self, line_positions: list, clue: int):
+        sum_terms = [If(self._grid_z3[position], self._input_grid[position], 0) for position in line_positions]
+        self._solver.add(sum(sum_terms) == clue)
---
+++ Domain/Puzzles/NumberCross/tests/NumberCrossSolver_test.py
@@ -0,0 +1,125 @@
+﻿import unittest
+from unittest import TestCase
+
+from Domain.Board.Grid import Grid
+from Domain.Puzzles.NumberCross.NumberCrossSolver import NumberCrossSolver
+
+_ = NumberCrossSolver.empty
+B = NumberCrossSolver.black_value
+
+class NumberCrossSolverTests(TestCase):
+    def test_get_solution_5x5_easy_l4j0e(self):
+        """https://gridpuzzle.com/number-cross/l4j0e"""
+        _grid = Grid([
+            [7, B, 9, 6, B],
+            [1, 7, 1, 4, 2],
+            [1, 5, 2, 7, 7],
+            [1, B, B, 5, 2],
+            [8, 3, 3, 8, 9],
+        ])
+        row_sums_clues = [9, 7, 15, 2, 14]
+        column_sums_clues = [9, 8, 15, 4, 11]
+
+        expected_grid = Grid([
+            [B, B, 9, B, B],
+            [B, B, 1, 4, 2],
+            [1, 5, 2, B, 7],
+            [B, B, B, B, 2],
+            [8, 3, 3, B, B],
+        ])
+        game_solver = NumberCrossSolver(_grid, row_sums_clues, column_sums_clues)
+        solution = game_solver.get_solution()
+        self.assertEqual(expected_grid, solution)
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+    def test_get_solution_5x5_evil_dn72v(self):
+        """https://gridpuzzle.com/number-cross/dn72v"""
+        _grid = Grid([
+            [3, 7, 4, 3, 4],
+            [7, 3, 7, 6, 4],
+            [3, 9, 6, 1, 4],
+            [1, 3, 7, 8, 7],
+            [6, 6, 8, 3, 8],
+        ])
+        row_sums_clues = [11, _, _, 18, 3]
+        column_sums_clues = [10, 19, 13, 18, 4]
+
+        expected_grid = Grid([
+            [B, 7, B, B, 4],
+            [7, B, B, 6, B],
+            [3, 9, 6, 1, B],
+            [B, 3, 7, 8, B],
+            [B, B, B, 3, B],
+        ])
+        game_solver = NumberCrossSolver(_grid, row_sums_clues, column_sums_clues)
+        solution = game_solver.get_solution()
+        self.assertEqual(expected_grid, solution)
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+    def test_get_solution_6x6_evil_n5k2w(self):
+        """https://gridpuzzle.com/number-cross/n5k2w"""
+        _grid = Grid([
+            [3, 4, 6, 6, 6, 3],
+            [9, 1, 8, 7, 8, 2],
+            [5, 8, 6, 2, 5, 2],
+            [1, 6, 9, 1, 2, 5],
+            [6, 2, 7, 4, 6, 7],
+            [6, 3, 1, 4, 1, 2]
+        ])
+        row_sums_clues = [6, 15, 15, 8, 25, 8]
+        column_sums_clues = [18, 10, _, _, 23, 14]
+
+        expected_grid = Grid([
+            [B, B, B, B, 6, B],
+            [B, B, B, 7, 8, B],
+            [5, 8, B, B, B, 2],
+            [1, B, B, B, 2, 5],
+            [6, 2, B, 4, 6, 7],
+            [6, B, 1, B, 1, B],
+        ])
+        game_solver = NumberCrossSolver(_grid, row_sums_clues, column_sums_clues)
+        solution = game_solver.get_solution()
+        self.assertEqual(expected_grid, solution)
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+    def test_get_solution_10x10_evil_5jyy9(self):
+        """https://gridpuzzle.com/number-cross/5jyy9"""
+        _grid = Grid([
+            [3, 3, 1, 8, 9, 9, 7, 5, 6, 4],
+            [3, 6, 1, 2, 9, 3, 2, 4, 9, 3],
+            [7, 9, 7, 2, 7, 9, 2, 8, 4, 4],
+            [5, 3, 8, 8, 9, 1, 4, 9, 7, 2],
+            [9, 4, 1, 4, 3, 1, 6, 7, 3, 6],
+            [6, 8, 6, 5, 9, 4, 8, 4, 1, 1],
+            [9, 5, 4, 5, 2, 5, 4, 6, 2, 3],
+            [9, 3, 6, 4, 6, 4, 9, 9, 6, 5],
+            [5, 9, 4, 2, 3, 6, 6, 1, 6, 9],
+            [4, 5, 3, 7, 5, 9, 4, 5, 6, 3],
+        ])
+        row_sums_clues = [_, 25, 19, 52, 24, 25, 11, 32, 29, 37]
+        column_sums_clues = [29, 19, 34, 36, 37, 35, 17, 39, 19, 9]
+
+        expected_grid = Grid([
+            [B, 3, B, 8, B, 9, B, B, B, B],
+            [B, B, 1, B, 9, B, 2, 4, 9, B],
+            [B, B, 7, 2, B, B, 2, 8, B, B],
+            [5, 3, 8, 8, 9, 1, B, 9, 7, 2],
+            [9, 4, 1, B, B, 1, B, B, 3, 6],
+            [6, B, B, 5, 9, B, B, 4, B, 1],
+            [B, B, 4, B, 2, 5, B, B, B, B],
+            [B, B, 6, 4, B, 4, 9, 9, B, B],
+            [5, 9, 4, 2, 3, 6, B, B, B, B],
+            [4, B, 3, 7, 5, 9, 4, 5, B, B],
+        ])
+        game_solver = NumberCrossSolver(_grid, row_sums_clues, column_sums_clues)
+        solution = game_solver.get_solution()
+        self.assertEqual(expected_grid, solution)
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+
+if __name__ == '__main__':
+    unittest.main()
