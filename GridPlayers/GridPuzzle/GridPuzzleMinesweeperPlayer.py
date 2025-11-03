4bc1170614d122918c1e66bf1f882fbb1c4b2d7d
Thomas Vuille <tvuille@blitzbs.com>
2025-11-03 15:42:33 +0100

    fix(Minesweeper): correct cell click logic by filtering based on solution values
    - Resolved an issue where cell clicks were not properly filtered for true solution values.
    - Simplified loop logic in both Minesweeper player implementations.

--- GridPlayers/GridPuzzle/GridPuzzleMinesweeperPlayer.py
+++ GridPlayers/GridPuzzle/GridPuzzleMinesweeperPlayer.py
@@ -9,11 +9,9 @@ class GridPuzzleMinesweeperPlayer(PuzzlesMobilePlayer, PlaywrightPlayer):
         video, rectangle = self._get_data_video_viewport(page)
 
         cells = page.query_selector_all("div.g_cell")
-        for position, value in solution:
+        for position in [position for position, value in solution if value]:
             index = position.r * solution.columns_number + position.c
-            class_attr = cells[index].get_attribute('class')
-            if value:
-                cells[index].click()
+            cells[index].click()
 
         self.close()
         self._process_video(video, rectangle, 0)
