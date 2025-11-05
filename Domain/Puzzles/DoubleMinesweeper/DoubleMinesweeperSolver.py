02907a3b4f1c0e2dcc7077ed41462a478c15a965
Thomas Vuille <tvuille@blitzbs.com>
2025-11-05 10:03:33 +0100

    feat(DoubleMinesweeper): wip

---
+++ Domain/Puzzles/DoubleMinesweeper/DoubleMinesweeperSolver.py
@@ -0,0 +1,45 @@
+﻿from z3 import Solver, Bool, Not, And, unsat, is_true
+
+from Domain.Board.Grid import Grid
+from Domain.Puzzles.GameSolver import GameSolver
+
+
+class DoubleMinesweeperSolver(GameSolver):
+    empty = None
+
+    def __init__(self, grid: Grid):
+        self._grid = grid
+        self.rows_number = self._grid.rows_number
+        self.columns_number = self._grid.columns_number
+        self._solver = Solver()
+        self._grid_z3: Grid | None = None
+        self._previous_solution: Grid | None = None
+
+    def get_solution(self) -> Grid:
+        self._grid_z3 = Grid([[Bool(f"grid_{r}_{c}") for c in range(self.columns_number)] for r in range(self.rows_number)])
+        self._add_constraints()
+        self._previous_solution = self._compute_solution()
+        return self._previous_solution
+
+    def get_other_solution(self) -> Grid:
+        self._solver.add(Not(And([self._grid_z3[position] == value for position, value in self._previous_solution])))
+        self._previous_solution = self._compute_solution()
+        return self._previous_solution
+
+    def _compute_solution(self) -> Grid:
+        if self._solver.check() == unsat:
+            return Grid.empty()
+        model = self._solver.model()
+        return Grid([[(is_true(model.eval(self._grid_z3.value(i, j)))) for j in range(self.columns_number)] for i in range(self.rows_number)])
+
+    def _add_constraints(self):
+        self._add_initial_constraints()
+        self._add_sum_constraints()
+
+    def _add_initial_constraints(self):
+        for position in [position for position, cell in self._grid if cell != self.empty]:
+            pass  # todo
+
+    def _add_sum_constraints(self):
+        for position, cell in [(position, cell) for position, cell in self._grid if cell != self.empty]:
+            pass  # todo (use self._grid_z3.neighbors_values)
---
+++ Domain/Puzzles/DoubleMinesweeper/tests/DoubleMinesweeperSolver_test.py
@@ -0,0 +1,52 @@
+﻿import unittest
+from unittest import TestCase
+
+from Domain.Board.Grid import Grid
+from Domain.Puzzles.DoubleMinesweeper.DoubleMinesweeperSolver import DoubleMinesweeperSolver
+
+_ = DoubleMinesweeperSolver.empty
+
+
+class DoubleMinesweeperSolverTests(TestCase):
+    def test_solution_basic_grid(self):
+        grid = Grid([
+            [_, _, _],
+            [0, 3, 3],
+            [_, 2, _]
+        ])
+        game_solver = DoubleMinesweeperSolver(grid)
+        solution = game_solver.get_solution()
+        expected_solution = Grid([
+            [0, 0, 1],
+            [0, 0, 0],
+            [0, 0, 2]
+        ])
+        self.assertEqual(expected_solution, solution)
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+    def test_solution_5x5_l9gqx(self):
+        """https://gridpuzzle.com/minesweeper-double/l9gqx"""
+        grid = Grid([
+            [_, 2, 1, 2, _],
+            [1, _, _, _, _],
+            [2, _, 6, _, 4],
+            [_, _, _, _, 3],
+            [_, 4, 4, 2, _],
+        ])
+        game_solver = DoubleMinesweeperSolver(grid)
+        solution = game_solver.get_solution()
+        expected_solution = Grid([
+            [1, 0, 0, 0, 0],
+            [0, 0, 1, 0, 1],
+            [0, 0, 0, 1, 0],
+            [0, 2, 0, 2, 0],
+            [2, 0, 0, 0, 0],
+        ])
+        self.assertEqual(expected_solution, solution)
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+
+if __name__ == '__main__':
+    unittest.main()
