1282d1af921853ece05beca98839fb28114d011c
Thomas <thomas@newtomsoft.fr>
2025-11-09 13:50:22 +0100

    refactor(GridPuzzle): unify `TilePaint`, `Clouds`, and `Chocona` players under `GridPuzzleTagByBlockPlayer`
    Standardize inheritance for block-based players by having `TilePaint`, `Clouds`, and `Chocona` players extend `GridPuzzleTagByBlockPlayer`. Streamline logic and improve maintainability.

--- GridPlayers/GridPuzzle/GridPuzzleChoconaPlayer.py
+++ GridPlayers/GridPuzzle/GridPuzzleChoconaPlayer.py
@@ -1,17 +1,5 @@
-﻿from Domain.Board.Grid import Grid
-from GridPlayers.PlaywrightPlayer import PlaywrightPlayer
+﻿from GridPlayers.GridPuzzle.Base.GridPuzzleTagByBlockPlayer import GridPuzzleTagByBlockPlayer
 
 
-class GridPuzzleChoconaPlayer(PlaywrightPlayer):
+class GridPuzzleChoconaPlayer(GridPuzzleTagByBlockPlayer):
     game_name = "chocona"
-
-    def play(self, solution: Grid):
-        page = self.browser.pages[0]
-        video, rectangle = self._get_data_video_viewport(page)
-
-        cells = page.query_selector_all("div.g_cell")
-        for position_index in [position.r * solution.columns_number + position.c for position, value in solution if value]:
-            cells[position_index].click()
-
-        self.close()
-        self._process_video(video, rectangle, 0)
--- GridPlayers/GridPuzzle/GridPuzzleCloudsPlayer.py
+++ GridPlayers/GridPuzzle/GridPuzzleCloudsPlayer.py
@@ -1,17 +1,5 @@
-﻿from Domain.Board.Grid import Grid
-from Domain.Board.Position import Position
-from GridPlayers.PlaywrightPlayer import PlaywrightPlayer
+﻿from GridPlayers.GridPuzzle.Base.GridPuzzleTagByBlockPlayer import GridPuzzleTagByBlockPlayer
 
 
-class GridPuzzleCloudsPlayer(PlaywrightPlayer):
+class GridPuzzleCloudsPlayer(GridPuzzleTagByBlockPlayer):
     game_name = "clouds"
-    def play(self, solution: Grid):
-        page = self.browser.pages[0]
-        video, rectangle = self._get_data_video_viewport(page)
-
-        cells = page.query_selector_all("div.g_cell")
-        for position_index in [position.r * solution.columns_number + position.c for position, value in solution if value]:
-            cells[position_index].click()
-
-        self.close()
-        self._process_video(video, rectangle, 0)
