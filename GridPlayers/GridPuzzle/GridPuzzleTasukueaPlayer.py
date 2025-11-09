2631c6e5f71d77ef4fb838d974c93c3cf8dc363b
Thomas <thomas@newtomsoft.fr>
2025-11-09 12:54:27 +0100

    feat(Tasukuea): add player and grid provider for Tasukuea puzzles
    Introduce `GridPuzzleTasukueaPlayer` for solving and interacting with Tasukuea puzzles. Add `GridPuzzleTasukueaGridProvider` to scrape and convert Tasukuea grids into usable formats.

---
+++ GridPlayers/GridPuzzle/GridPuzzleTasukueaPlayer.py
@@ -0,0 +1,51 @@
+﻿from GridPlayers.PlaywrightPlayer import PlaywrightPlayer
+from GridPlayers.PuzzleMobiles.PuzzlesMobilePlayer import PuzzlesMobilePlayer
+
+
+class GridPuzzleTasukueaPlayer(PuzzlesMobilePlayer, PlaywrightPlayer):
+    game_name = "tasukuea"
+    def play(self, solution):
+        page = self.browser.pages[0]
+        video, rectangle = self._get_data_video_viewport(page)
+
+        cells = page.query_selector_all("div.g_cell")
+
+        true_positions = {position for position, value in solution if value}
+
+        visited = set()
+        blocks = []
+
+        def get_block(start_pos):
+            block = []
+            stack = [start_pos]
+
+            while stack:
+                pos = stack.pop()
+                if pos in visited or pos not in true_positions:
+                    continue
+
+                visited.add(pos)
+                block.append(pos)
+
+                # Vérifier les 4 voisins adjacents (haut, bas, gauche, droite)
+                for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                    neighbor = type(pos)(pos.r + dr, pos.c + dc)
+                    if neighbor in true_positions and neighbor not in visited:
+                        stack.append(neighbor)
+
+            return block
+
+        # Identifier tous les blocs
+        for position in true_positions:
+            if position not in visited:
+                block = get_block(position)
+                blocks.append(block)
+
+        # Traiter chaque bloc ligne par ligne
+        for block in blocks:
+            for position in block:
+                index = position.r * solution.columns_number + position.c
+                cells[index].click()
+
+        self.close()
+        self._process_video(video, rectangle, 0)
---
+++ GridProviders/GridPuzzle/GridPuzzleTasukueaGridProvider.py
@@ -0,0 +1,34 @@
+from playwright.sync_api import BrowserContext
+
+from Domain.Board.Grid import Grid
+from Domain.Puzzles.Minesweeper.MinesweeperSolver import MinesweeperSolver
+from GridProviders.GridProvider import GridProvider
+from GridProviders.GridPuzzle.GridPuzzleGridTagProvider import GridPuzzleGridTagProvider
+from GridProviders.PlaywrightGridProvider import PlaywrightGridProvider
+from Puzzles.Tasukuea.TasukueaSolver import TasukueaSolver
+
+
+class GridPuzzleTasukueaGridProvider(GridProvider, PlaywrightGridProvider, GridPuzzleGridTagProvider):
+    def get_grid(self, url: str):
+        return self.with_playwright(self.scrap_grid, url)
+
+    def scrap_grid(self, browser: BrowserContext, url):
+        html_page = self.get_html(browser, url, '.col-lg-12.col-md-12.col-12')
+        _, row_count, column_count, matrix, matrix_cells = self._get_grid_data(html_page)
+
+        for i, cell in enumerate(matrix_cells):
+            row = i // column_count
+            col = i % column_count
+            text = cell.text
+            matrix[row][col] = self.convert(text) if text != '' else MinesweeperSolver.empty
+
+        return Grid(matrix)
+
+    @staticmethod
+    def convert(text: str):
+        if text == '':
+            return TasukueaSolver.empty
+        elif text == '?':
+            return TasukueaSolver.unknown
+        else:
+            return int(text)
