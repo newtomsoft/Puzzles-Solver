770665a5149c2ee9d76489d921f720891629e79b
Thomas <thomas@newtomsoft.fr>
2025-11-09 13:33:32 +0100

    refactor(GridPuzzle): extract shared block-based logic to `GridPuzzleTagByBlockPlayer`
    Reorganized `GridPuzzleTasukueaPlayer` and `GridPuzzleLookAirPlayer` to inherit from the new `GridPuzzleTagByBlockPlayer`, consolidating common functionality for block-based tagging.

--- GridPlayers/GridPuzzle/GridPuzzleLookAirPlayer.py
+++ GridPlayers/GridPuzzle/GridPuzzleLookAirPlayer.py
@@ -1,17 +1,5 @@
-﻿from Domain.Board.Grid import Grid
-from GridPlayers.PlaywrightPlayer import PlaywrightPlayer
+﻿from GridPlayers.GridPuzzle.Base.GridPuzzleTagByBlockPlayer import GridPuzzleTagByBlockPlayer
 
 
-class GridPuzzleLookAirPlayer(PlaywrightPlayer):
+class GridPuzzleLookAirPlayer(GridPuzzleTagByBlockPlayer):
     game_name = "look-air"
-    def play(self, grid_solution: Grid):
-        page = self.browser.pages[0]
-        video, rectangle = self._get_data_video_viewport(page)
-
-        cells = page.query_selector_all("div.g_cell")
-        for position, solution_value in [(position, solution_value) for position, solution_value in grid_solution if solution_value]:
-            index = position.r * grid_solution.columns_number + position.c
-            cells[index].click()
-
-        self.close()
-        self._process_video(video, rectangle, 0)
