d0fa5e3960e63cc99077988e3c624cabaa2f9ff3
Thomas Vuille <tvuille@blitzbs.com>
2025-11-14 18:07:01 +0100

    refactor(EverySecondTurn): migrate from Z3 to OR-Tools for constraint solving in `EverySecondTurnSolver`

--- Domain/Puzzles/EverySecondTurn/EverySecondTurnSolver.py
+++ Domain/Puzzles/EverySecondTurn/EverySecondTurnSolver.py
@@ -1,4 +1,4 @@
-﻿from z3 import Solver, Not, And, Int, sat, ArithRef, Or, Sum
+﻿from ortools.sat.python import cp_model
 
 from Domain.Board.Direction import Direction
 from Domain.Board.Grid import Grid
@@ -10,29 +10,32 @@ from Domain.Puzzles.GameSolver import GameSolver
 X = '*'
 _ = '.'
 
+
 class EverySecondTurnSolver(GameSolver):
     def __init__(self, grid: Grid):
         self._input_grid = grid
         self._island_grid: IslandGrid | None = None
         self.init_island_grid()
-        self._solver = Solver()
-        self._island_bridges_z3: dict[Position, dict[Direction, ArithRef]] = {}
+        self._model = cp_model.CpModel()
+        self._island_bridges: dict[Position, dict[Direction, cp_model.BoolVar]] = {}
         self._previous_solution: IslandGrid | None = None
 
     def init_island_grid(self):
         self._island_grid = IslandGrid(
-            [[Island(Position(r, c), 2) for c in range(self._input_grid.columns_number)] for r in range(self._input_grid.rows_number)]
+            [[Island(Position(r, c), 2) for c in range(self._input_grid.columns_number)] for r in
+             range(self._input_grid.rows_number)]
         )
 
     def _init_solver(self):
-        self._island_bridges_z3 = {
-            island.position: {direction: Int(f"{island.position}_{direction}") for direction in Direction.orthogonals()}
+        self._island_bridges = {
+            island.position: {direction: self._model.NewBoolVar(f"{island.position}_{direction}") for direction in
+                              Direction.orthogonals()}
             for island in self._island_grid.islands.values()
         }
         self._add_constraints()
 
     def get_solution(self) -> IslandGrid:
-        if not self._solver.assertions():
+        if not self._model.Proto().variables:
             self._init_solver()
 
         solution, _ = self._ensure_all_islands_connected()
@@ -40,14 +43,14 @@ class EverySecondTurnSolver(GameSolver):
 
     def _ensure_all_islands_connected(self) -> tuple[IslandGrid, int]:
         proposition_count = 0
-        while self._solver.check() == sat:
-            model = self._solver.model()
+        solver = cp_model.CpSolver()
+        while solver.Solve(self._model) == cp_model.OPTIMAL:
             proposition_count += 1
-            for position, direction_bridges in self._island_bridges_z3.items():
+            for position, direction_bridges in self._island_bridges.items():
                 for direction, bridges in direction_bridges.items():
-                    if position.after(direction) not in self._island_bridges_z3:
+                    if position.after(direction) not in self._island_bridges:
                         continue
-                    bridges_number = model.eval(bridges).as_long()
+                    bridges_number = solver.Value(bridges)
                     if bridges_number > 0:
                         self._island_grid[position].set_bridge_to_position(
                             self._island_grid[position].direction_position_bridges[direction][0], bridges_number)
@@ -65,9 +68,10 @@ class EverySecondTurnSolver(GameSolver):
                 cell_constraints = []
                 for position in positions:
                     for direction, (_, value) in self._island_grid[position].direction_position_bridges.items():
-                        cell_constraints.append(self._island_bridges_z3[position][direction] == value)
-                not_loop_constraints.append(Not(And(cell_constraints)))
-            self._solver.add(And(not_loop_constraints))
+                        cell_constraints.append(self._island_bridges[position][direction])
+                not_loop_constraints.append(cell_constraints)
+            for constraint in not_loop_constraints:
+                self._model.AddBoolOr([c.Not() for c in constraint])
             self.init_island_grid()
 
         return IslandGrid.empty(), proposition_count
@@ -76,8 +80,8 @@ class EverySecondTurnSolver(GameSolver):
         previous_solution_constraints = []
         for island in self._previous_solution.islands.values():
             for direction, (_, value) in island.direction_position_bridges.items():
-                previous_solution_constraints.append(self._island_bridges_z3[island.position][direction] == value)
-        self._solver.add(Not(And(previous_solution_constraints)))
+                previous_solution_constraints.append(self._island_bridges[island.position][direction])
+        self._model.AddBoolOr([c.Not() for c in previous_solution_constraints])
 
         self.init_island_grid()
         return self.get_solution()
@@ -89,68 +93,95 @@ class EverySecondTurnSolver(GameSolver):
         self._add_turn_at_every_circle_constraints()
 
     def _add_initial_constraints(self):
-        for position, direction_bridges in self._island_bridges_z3.items():
+        for position, direction_bridges in self._island_bridges.items():
             bridges_count_vars = list(direction_bridges.values())
-            constraint_sum = sum(bridges_count_vars) == 2
-            self._solver.add(constraint_sum)
-            for bridges in direction_bridges.values():
-                self._solver.add(And(bridges >= 0, bridges <= 1))
+            self._model.Add(sum(bridges_count_vars) == 2)
 
     def _add_opposite_bridges_constraints(self):
         for island in self._island_grid.islands.values():
             for direction in [Direction.right(), Direction.down(), Direction.left(), Direction.up()]:
                 if island.direction_position_bridges.get(direction) is not None:
-                    self._solver.add(
-                        self._island_bridges_z3[island.position][direction] ==
-                        self._island_bridges_z3[island.direction_position_bridges[direction][0]][direction.opposite])
+                    self._model.Add(
+                        self._island_bridges[island.position][direction] ==
+                        self._island_bridges[island.direction_position_bridges[direction][0]][direction.opposite])
                 else:
-                    self._solver.add(self._island_bridges_z3[island.position][direction] == 0)
+                    self._model.Add(self._island_bridges[island.position][direction] == 0)
 
     def _add_links_constraints(self):
         for position in [position for position, value in self._input_grid if value == X]:
             linked_circles_constraints = self._one_turn_between_linked_circles_constraints(position)
-            self._solver.add(Sum(linked_circles_constraints) == 2)
+            self._model.Add(sum(linked_circles_constraints) == 2)
 
-    def _one_turn_between_linked_circles_constraints(self, circle_pos: Position):
+    def _one_turn_between_linked_circles_constraints(self, circle_pos: Position) -> list:
         constraints = []
-        for other_circle_pos in [pos for pos, value in self._input_grid if value == X and pos.r != circle_pos.r and pos.c != circle_pos.c]:
+        for other_circle_pos in [pos for pos, value in self._input_grid if
+                                 value == X and pos.r != circle_pos.r and pos.c != circle_pos.c]:
             hor_turn_pos = Position(circle_pos.r, other_circle_pos.c)
             vert_turn_pos = Position(other_circle_pos.r, circle_pos.c)
             hor_direction = Direction.right() if other_circle_pos.c > circle_pos.c else Direction.left()
             vert_direction = Direction.down() if other_circle_pos.r > circle_pos.r else Direction.up()
 
-            hor_first_constraint = self._to_other_circle_constraint(circle_pos, other_circle_pos, hor_turn_pos, hor_direction, vert_direction)
-            vert_first_constraints = self._to_other_circle_constraint(circle_pos, other_circle_pos, vert_turn_pos, vert_direction, hor_direction)
-            constraints.append(Or(hor_first_constraint, vert_first_constraints))
+            hor_path_vars = self._to_other_circle_constraint(circle_pos, other_circle_pos, hor_turn_pos,
+                                                                    hor_direction, vert_direction)
+            vert_path_vars = self._to_other_circle_constraint(circle_pos, other_circle_pos, vert_turn_pos,
+                                                                      vert_direction, hor_direction)
+
+            hor_path_ok = self.new_and(hor_path_vars)
+            vert_path_ok = self.new_and(vert_path_vars)
+
+            link_ok = self._model.NewBoolVar(f"link_{circle_pos}_to_{other_circle_pos}")
+            self._model.AddBoolOr([hor_path_ok, vert_path_ok]).OnlyEnforceIf(link_ok)
+            self._model.AddImplication(hor_path_ok, link_ok)
+            self._model.AddImplication(vert_path_ok, link_ok)
+
+            constraints.append(link_ok)
 
         return constraints
 
-    def _to_other_circle_constraint(self, circle_pos, other_circle_pos, turn_position, first_direction, second_direction):
-        constraints = [self._island_bridges_z3[circle_pos][first_direction] == 1]
+    def _to_other_circle_constraint(self, circle_pos, other_circle_pos, turn_position, first_direction,
+                                    second_direction) -> list:
+        constraints = [self._island_bridges[circle_pos][first_direction]]
         current_position = circle_pos.after(first_direction)
         while self._input_grid[current_position] == _ and current_position != turn_position:
-            constraints.append(self._island_bridges_z3[current_position][first_direction] == 1)
+            constraints.append(self._island_bridges[current_position][first_direction])
             current_position = current_position.after(first_direction)
         if self._input_grid[current_position] != _:
-            return False
+            b = self._model.NewBoolVar("")
+            self._model.Add(b == 0)
+            return [b]
 
-        constraints.append(self._island_bridges_z3[current_position][second_direction] == 1)
+        constraints.append(self._island_bridges[current_position][second_direction])
         current_position = current_position.after(second_direction)
         while self._input_grid[current_position] == _ and current_position != other_circle_pos:
-            constraints.append(self._island_bridges_z3[current_position][second_direction] == 1)
+            constraints.append(self._island_bridges[current_position][second_direction])
             current_position = current_position.after(second_direction)
         if current_position != other_circle_pos:
-            return False
+            b = self._model.NewBoolVar("")
+            self._model.Add(b == 0)
+            return [b]
 
-        return And(constraints) if len(constraints) > 1 else constraints[0]
+        return constraints
 
     def _add_turn_at_every_circle_constraints(self):
         for position in [position for position, value in self._input_grid if value == X]:
-            right = self._island_bridges_z3[position][Direction.right()]
-            up = self._island_bridges_z3[position][Direction.up()]
-            left = self._island_bridges_z3[position][Direction.left()]
-            down = self._island_bridges_z3[position][Direction.down()]
-            self._solver.add(Or(
-                And(right == 1, up == 1, left == 0, down == 0), And(right == 1,up == 0, left == 0, down == 1),
-                And(right == 0, up == 0, left == 1, down == 1), And(right == 0, up == 1, left == 1, down == 0)
-            ))
+            right = self._island_bridges[position][Direction.right()]
+            up = self._island_bridges[position][Direction.up()]
+            left = self._island_bridges[position][Direction.left()]
+            down = self._island_bridges[position][Direction.down()]
+
+            self._model.AddBoolOr([
+                self.new_and([right, up, left.Not(), down.Not()]),
+                self.new_and([right, up.Not(), left.Not(), down]),
+                self.new_and([right.Not(), up.Not(), left, down]),
+                self.new_and([right.Not(), up, left, down.Not()])
+            ])
+
+    def new_and(self, literals):
+        b = self._model.NewBoolVar('')
+        if not literals:
+            self._model.Add(b == 1)
+            return b
+        for lit in literals:
+            self._model.AddImplication(b, lit)
+        self._model.AddBoolOr([l.Not() for l in literals] + [b])
+        return b
