cf018eb89991a71ca9a5bec7239a06daa2c88848
Thomas <thomas@newtomsoft.fr>
2025-11-15 17:52:25 +0100

    feat(NeighboursSolver): transition to OR-Tools CP-SAT solver
    Replace Z3 with OR-Tools CP-SAT framework to improve performance, flexibility, and constraint handling. Rewrite solver logic to utilize CP-SAT variables and streamline region and adjacency constraints.

--- Domain/Puzzles/Neighbours/NeighboursSolver.py
+++ Domain/Puzzles/Neighbours/NeighboursSolver.py
@@ -1,4 +1,4 @@
-﻿from z3 import Solver, Not, And, Or, Int, Implies, unsat, If, BoolRef
+﻿from ortools.sat.python import cp_model
 
 from Domain.Board.Grid import Grid
 from Domain.Board.Position import Position
@@ -13,26 +13,33 @@ class NeighboursSolver(GameSolver):
         self._clues_grid = clues_clues_grid
         self._rows_number = clues_clues_grid.rows_number
         self._columns_number = clues_clues_grid.columns_number
-        self._solver = Solver()
-        self._grid_z3: Grid | None = None
+        self._model = cp_model.CpModel()
+        self._grid_ortools: Grid | None = None
         self._clue_by_position = dict([(position, value) for position, value in self._clues_grid if value != NeighboursSolver.empty])
         self._clue_position_by_region_id = {index + 1: position for index, position in enumerate(self._clue_by_position.keys())}
         self._regions_count = len(self._clue_by_position)
+        self._solver = cp_model.CpSolver()
 
     def get_solution(self) -> Grid:
-        self._grid_z3 = Grid([[Int(f"grid_{r}_{c}") for c in range(self._columns_number)] for r in range(self._rows_number)])
+        self._grid_ortools = Grid([[self._model.NewIntVar(1, self._regions_count, f"grid_{r}_{c}") for c in range(self._columns_number)] for r in range(self._rows_number)])
         self._add_constraints()
         return self._compute_solution()
 
     def get_other_solution(self) -> Grid:
-        self._solver.add(Not(And([self._grid_z3[position] == value for position, value in self._previous_solution])))
+        bool_vars = []
+        for position, value in self._previous_solution:
+            b = self._model.NewBoolVar('')
+            self._model.Add(self._grid_ortools[position] != value).OnlyEnforceIf(b)
+            self._model.Add(self._grid_ortools[position] == value).OnlyEnforceIf(b.Not())
+            bool_vars.append(b)
+        self._model.Add(sum(bool_vars) > 0)
         return self._compute_solution()
 
     def _compute_solution(self) -> Grid:
-        if self._solver.check() == unsat:
+        status = self._solver.Solve(self._model)
+        if status == cp_model.INFEASIBLE or status == cp_model.UNKNOWN:
             return Grid.empty()
-        model = self._solver.model()
-        solution = Grid([[(model.eval(self._grid_z3.value(i, j))).as_long() for j in range(self._columns_number)] for i in range(self._rows_number)])
+        solution = Grid([[self._solver.Value(self._grid_ortools.value(i, j)) for j in range(self._columns_number)] for i in range(self._rows_number)])
         self._previous_solution = solution
         return solution
 
@@ -44,76 +51,150 @@ class NeighboursSolver(GameSolver):
 
     def _add_initials_constraints(self):
         for region_id, position in self._clue_position_by_region_id.items():
-            self._solver.add(self._grid_z3[position] == region_id)
+            self._model.Add(self._grid_ortools[position] == region_id)
 
     def _add_area_regions_constraints(self):
         area = self._rows_number * self._columns_number // self._regions_count
         for region_id in self._clue_position_by_region_id.keys():
-            self._solver.add(sum([self._grid_z3[position] == region_id for position, _ in self._clues_grid]) == area)
+            bool_vars = []
+            for position, _ in self._clues_grid:
+                b = self._model.NewBoolVar('')
+                self._model.Add(self._grid_ortools[position] == region_id).OnlyEnforceIf(b)
+                self._model.Add(self._grid_ortools[position] != region_id).OnlyEnforceIf(b.Not())
+                bool_vars.append(b)
+            self._model.Add(sum(bool_vars) == area)
 
     def _add_connected_cells_regions_constraints(self):
-        steps = [Grid([[Int(f'step{region_id}_{r}_{c}') for c in range(self._columns_number)] for r in range(self._rows_number)]) for region_id in
+        steps = [Grid([[self._model.NewIntVar(0, self._rows_number * self._columns_number, f'step{region_id}_{r}_{c}')
+                        for c in range(self._columns_number)] for r in range(self._rows_number)]) for region_id in
                  range(1, self._regions_count + 1)]
         for region_id in self._clue_position_by_region_id.keys():
             self._add_connected_cells_region_constraints(steps[region_id - 1], region_id)
 
     def _add_connected_cells_region_constraints(self, step: Grid, region_id: int):
-        self._solver.add([If(self._grid_z3[position] == region_id, step[position] >= 1, step[position] == 0) for position, _ in self._clues_grid])
+        is_in_region = {}
+        for pos, _ in self._clues_grid:
+            b = self._model.NewBoolVar(f'is_in_region_{region_id}_{pos.r}_{pos.c}')
+            self._model.Add(self._grid_ortools[pos] == region_id).OnlyEnforceIf(b)
+            self._model.Add(self._grid_ortools[pos] != region_id).OnlyEnforceIf(b.Not())
+            is_in_region[pos] = b
 
-        roots = [And(self._grid_z3[position] == region_id, step[position] == 1) for position, _ in self._clues_grid]
-        self._solver.add(Or(roots))
+        for position, _ in self._clues_grid:
+            self._model.Add(step[position] >= 1).OnlyEnforceIf(is_in_region[position])
+            self._model.Add(step[position] == 0).OnlyEnforceIf(is_in_region[position].Not())
 
-        self._solver.add([Not(And(roots[i], roots[j])) for i in range(len(roots)) for j in range(i + 1, len(roots))])
+        roots = []
+        for position, _ in self._clues_grid:
+            is_root = self._model.NewBoolVar(f'root_{region_id}_{position.r}_{position.c}')
+            is_step_one = self._model.NewBoolVar(f'step_is_one_{region_id}_{position.r}_{position.c}')
+            self._model.Add(step[position] == 1).OnlyEnforceIf(is_step_one)
+            self._model.Add(step[position] != 1).OnlyEnforceIf(is_step_one.Not())
+            self._model.AddImplication(is_root, is_in_region[position])
+            self._model.AddImplication(is_root, is_step_one)
+            self._model.AddBoolOr([is_in_region[position].Not(), is_step_one.Not(), is_root])
+            roots.append(is_root)
+        self._model.Add(sum(roots) == 1)
 
         for r in range(self._rows_number):
             for c in range(self._columns_number):
-                current_step = step[r][c]
-                adjacents = []
-                if r > 0:
-                    adjacents.append(And(self._grid_z3[r - 1][c] == region_id, step[r - 1][c] == current_step - 1))
-                if r < self._rows_number - 1:
-                    adjacents.append(And(self._grid_z3[r + 1][c] == region_id, step[r + 1][c] == current_step - 1))
-                if c > 0:
-                    adjacents.append(And(self._grid_z3[r][c - 1] == region_id, step[r][c - 1] == current_step - 1))
-                if c < self._columns_number - 1:
-                    adjacents.append(And(self._grid_z3[r][c + 1] == region_id, step[r][c + 1] == current_step - 1))
+                pos = Position(r, c)
+                current_step = step[pos]
 
-                self._solver.add(Implies(And(self._grid_z3[r][c] == region_id, current_step > 1), Or(adjacents)))
+                is_step_gt_1 = self._model.NewBoolVar(f"step_gt_1_{region_id}_{r}_{c}")
+                self._model.Add(current_step > 1).OnlyEnforceIf(is_step_gt_1)
+                self._model.Add(current_step <= 1).OnlyEnforceIf(is_step_gt_1.Not())
+
+                implication_condition = self._model.NewBoolVar(f"impl_cond_{region_id}_{r}_{c}")
+                self._model.AddImplication(implication_condition, is_in_region[pos])
+                self._model.AddImplication(implication_condition, is_step_gt_1)
+                self._model.AddBoolOr([is_in_region[pos].Not(), is_step_gt_1.Not(), implication_condition])
+
+                adjacents_ok = []
+                for neighbor_pos in self._clues_grid.neighbors_positions(pos):
+                    is_neighbor_step_parent = self._model.NewBoolVar(f'parent_{region_id}_{neighbor_pos.r}_{neighbor_pos.c}')
+                    self._model.Add(step[neighbor_pos] == current_step - 1).OnlyEnforceIf(is_neighbor_step_parent)
+                    self._model.Add(step[neighbor_pos] != current_step - 1).OnlyEnforceIf(is_neighbor_step_parent.Not())
+
+                    b_adj = self._model.NewBoolVar(f'adj_{region_id}_{neighbor_pos.r}_{neighbor_pos.c}')
+                    self._model.AddImplication(b_adj, is_in_region[neighbor_pos])
+                    self._model.AddImplication(b_adj, is_neighbor_step_parent)
+                    self._model.AddBoolOr([is_in_region[neighbor_pos].Not(), is_neighbor_step_parent.Not(), b_adj])
+                    adjacents_ok.append(b_adj)
+
+                if adjacents_ok:
+                    self._model.AddBoolOr(adjacents_ok).OnlyEnforceIf(implication_condition)
 
     def _add_neighbours_clues_constraints(self):
-        # Construire la liste des paires de cellules adjacentes (orthogonales), unique (position < voisin)
         adjacent_edges: list[tuple[Position, Position]] = []
         for position, _ in self._clues_grid:
             for neighbor in self._clues_grid.neighbors_positions(position):
-                if position < neighbor:  # éviter les doublons (u,v) et (v,u)
+                if position < neighbor:
                     adjacent_edges.append((position, neighbor))
 
-        # Pour chaque paire de régions (i, j), définir une contrainte «Adj(i,j)» signifiant
-        # qu'il existe au moins une arête (u,v) telle que grid[u]==i et grid[v]==j (ou l'inverse).
-        # Puis imposer pour chaque région i que le nombre de régions adjacentes égale l'indice (clue) de i.
         region_ids = list(self._clue_position_by_region_id.keys())
-        # Pré-calcul des formules d'adjacence entre régions
-        adj_between_regions: dict[tuple[int, int], BoolRef] = {}
+
+        adj_between_regions: dict[tuple[int, int], cp_model.BoolVarT] = {}
         for i in region_ids:
             for j in region_ids:
                 if i >= j:
                     continue
-                disjuncts = []
-                for (u, v) in adjacent_edges:
-                    disjuncts.append(And(self._grid_z3[u] == i, self._grid_z3[v] == j))
-                    disjuncts.append(And(self._grid_z3[u] == j, self._grid_z3[v] == i))
-                adj_between_regions[(i, j)] = Or(disjuncts) if disjuncts else Or([])
 
-        # Compter, pour chaque région i, le nombre de régions j adjacentes distinctes
+                adj_bool = self._model.NewBoolVar(f'adj_{i}_{j}')
+
+                edge_connects_ij_bools = []
+                for u, v in adjacent_edges:
+                    b_edge = self._model.NewBoolVar(f'edge_{u.r}{u.c}_{v.r}{v.c}_connects_{i}{j}')
+
+                    b_ui = self._model.NewBoolVar(f'b_{u.r}{u.c}=={i}')
+                    self._model.Add(self._grid_ortools[u] == i).OnlyEnforceIf(b_ui)
+                    self._model.Add(self._grid_ortools[u] != i).OnlyEnforceIf(b_ui.Not())
+
+                    b_vj = self._model.NewBoolVar(f'b_{v.r}{v.c}=={j}')
+                    self._model.Add(self._grid_ortools[v] == j).OnlyEnforceIf(b_vj)
+                    self._model.Add(self._grid_ortools[v] != j).OnlyEnforceIf(b_vj.Not())
+
+                    b_uj = self._model.NewBoolVar(f'b_{u.r}{u.c}=={j}')
+                    self._model.Add(self._grid_ortools[u] == j).OnlyEnforceIf(b_uj)
+                    self._model.Add(self._grid_ortools[u] != j).OnlyEnforceIf(b_uj.Not())
+
+                    b_vi = self._model.NewBoolVar(f'b_{v.r}{v.c}=={i}')
+                    self._model.Add(self._grid_ortools[v] == i).OnlyEnforceIf(b_vi)
+                    self._model.Add(self._grid_ortools[v] != i).OnlyEnforceIf(b_vi.Not())
+
+                    b_term1 = self._model.NewBoolVar(f'b_term1_{u.r}{u.c}_{v.r}{v.c}_{i}{j}')
+                    self._model.AddImplication(b_term1, b_ui)
+                    self._model.AddImplication(b_term1, b_vj)
+                    self._model.AddBoolOr([b_ui.Not(), b_vj.Not(), b_term1])
+
+                    b_term2 = self._model.NewBoolVar(f'b_term2_{u.r}{u.c}_{v.r}{v.c}_{i}{j}')
+                    self._model.AddImplication(b_term2, b_uj)
+                    self._model.AddImplication(b_term2, b_vi)
+                    self._model.AddBoolOr([b_uj.Not(), b_vi.Not(), b_term2])
+
+                    self._model.AddImplication(b_term1, b_edge)
+                    self._model.AddImplication(b_term2, b_edge)
+                    self._model.AddBoolOr([b_term1, b_term2, b_edge.Not()])
+
+                    edge_connects_ij_bools.append(b_edge)
+
+                if edge_connects_ij_bools:
+                    self._model.AddBoolOr(edge_connects_ij_bools).OnlyEnforceIf(adj_bool)
+                    for b in edge_connects_ij_bools:
+                        self._model.AddImplication(b, adj_bool)
+                else:
+                    self._model.Add(adj_bool == 0)
+
+                adj_between_regions[(i, j)] = adj_bool
+
         for i in region_ids:
             clue_position = self._clue_position_by_region_id[i]
             clue_value = self._clue_by_position[clue_position]
-            if clue_value == NeighboursSolver.unknow:
+            if clue_value == self.unknow:
                 continue
             terms = []
             for j in region_ids:
-                if j == i:
+                if i == j:
                     continue
                 key = (i, j) if i < j else (j, i)
-                terms.append(If(adj_between_regions[key], 1, 0))
-            self._solver.add(sum(terms) == clue_value)
+                terms.append(adj_between_regions[key])
+            self._model.Add(sum(terms) == clue_value)
