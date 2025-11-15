d03ae5c2ff1be14af6a7d32905aae6009b27dcf9
Thomas <thomas@newtomsoft.fr>
2025-11-15 18:39:32 +0100

    test(NeighboursSolver): add unit tests for hard, evil, and extreme puzzles
    Include test cases for 8x8 hard, 9x9 evil, and 10x10 evil Neighbours puzzles to validate solver correctness on complex grids.

--- Domain/Puzzles/Neighbours/tests/NeighboursSolver_test.py
+++ Domain/Puzzles/Neighbours/tests/NeighboursSolver_test.py
@@ -75,3 +75,96 @@ class NeighboursSolverTests(TestCase):
         self.assertEqual(expected_solution, solution)
         other_solution = game_solver.get_other_solution()
         self.assertEqual(Grid.empty(), other_solution)
+
+    def test_8x8_hard_2kdz5(self):
+        """https://gridpuzzle.com/neighbours/2kdz5"""
+        grid = Grid([
+            [4, _, 3, _, _, _, _, 4],
+            [_, _, _, _, _, _, 3, _],
+            [_, _, 6, _, 6, _, _, _],
+            [_, 4, _, _, 6, _, _, _],
+            [_, _, _, 4, _, _, _, _],
+            [3, _, _, 6, _, _, _, _],
+            [_, _, 4, _, _, 6, _, 4],
+            [_, _, _, 2, _, _, _, 3],
+        ])
+
+        game_solver = NeighboursSolver(grid)
+        solution = game_solver.get_solution()
+        expected_solution = Grid([
+            [1, 2, 2, 2, 2, 4, 4, 3],
+            [1, 1, 1, 6, 6, 4, 4, 3],
+            [7, 7, 5, 5, 6, 6, 3, 3],
+            [10, 7, 5, 5, 8, 8, 8, 14],
+            [10, 7, 11, 9, 9, 9, 8, 14],
+            [10, 10, 11, 11, 11, 9, 13, 14],
+            [12, 12, 12, 12, 13, 13, 13, 14],
+            [15, 15, 15, 15, 16, 16, 16, 16],
+        ])
+        self.assertEqual(expected_solution, solution)
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+    def test_9x9_evil_0me88(self):
+        """https://gridpuzzle.com/neighbours/0me88"""
+        grid = Grid([
+            [_, _, 3, _, _, 0, 3, _, _],
+            [4, _, 5, _, _, _, _, _, 2],
+            [_, 0, _, _, _, 0, _, _, _],
+            [0, _, 5, _, _, _, _, 0, 4],
+            [_, _, 5, _, _, 4, 5, _, _],
+            [0, _, _, _, _, 0, _, 0, _],
+            [4, _, _, 0, 5, _, 5, _, _],
+            [_, _, _, _, _, _, 0, _, 3],
+            [_, 0, _, 4, 3, _, _, _, _]
+        ])
+
+        game_solver = NeighboursSolver(grid)
+        solution = game_solver.get_solution()
+        expected_solution = Grid([
+            [4, 1, 1, 1, 2, 2, 3, 6, 6],
+            [4, 4, 5, 5, 5, 2, 3, 3, 6],
+            [9, 7, 7, 7, 8, 8, 11, 11, 12],
+            [9, 9, 10, 10, 14, 8, 15, 11, 12],
+            [16, 13, 13, 10, 14, 14, 15, 15, 12],
+            [16, 16, 13, 17, 17, 17, 18, 18, 18],
+            [19, 19, 20, 20, 21, 22, 22, 22, 24],
+            [25, 19, 26, 20, 21, 21, 23, 23, 24],
+            [25, 25, 26, 26, 27, 27, 27, 23, 24],
+        ])
+        self.assertEqual(expected_solution, solution)
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
+
+    def test_10x10_evil_1dxp0(self):
+        """https://gridpuzzle.com/neighbours/1dxp0"""
+        grid = Grid([
+            [_, _, _, _, 4, _, _, _, _, 3],
+            [_, 3, 7, _, _, _, _, _, _, _],
+            [_, _, _, _, 4, _, _, 5, 3, _],
+            [0, _, 7, _, _, 0, _, _, 3, _],
+            [_, _, _, 0, _, _, _, 5, _, _],
+            [_, 3, _, _, _, _, 0, _, _, _],
+            [4, _, _, _, _, _, _, _, _, 0],
+            [0, _, _, _, 4, 0, _, _, _, _],
+            [0, _, _, _, 0, _, _, 4, _, _],
+            [_, _, _, _, _, _, _, 5, _, 2]
+        ])
+
+        game_solver = NeighboursSolver(grid)
+        solution = game_solver.get_solution()
+        expected_solution = Grid([
+            [3, 3, 1, 1, 1, 1, 2, 2, 2, 2],
+            [3, 3, 4, 5, 5, 5, 6, 6, 7, 7],
+            [8, 8, 4, 4, 5, 10, 6, 6, 7, 7],
+            [8, 8, 9, 4, 10, 10, 10, 13, 11, 11],
+            [14, 14, 9, 12, 12, 12, 12, 13, 13, 11],
+            [14, 14, 9, 9, 20, 20, 15, 17, 13, 11],
+            [16, 16, 16, 19, 19, 20, 15, 17, 17, 17],
+            [18, 18, 16, 19, 19, 20, 15, 23, 23, 23],
+            [21, 18, 18, 22, 22, 24, 15, 23, 25, 25],
+            [21, 21, 21, 22, 22, 24, 24, 24, 25, 25],
+        ])
+        self.assertEqual(expected_solution, solution)
+        other_solution = game_solver.get_other_solution()
+        self.assertEqual(Grid.empty(), other_solution)
