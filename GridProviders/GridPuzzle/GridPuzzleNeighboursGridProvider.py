b9d6f3a0ca27bff6cd8a80b9ba92b20c8a1c3769
Thomas <thomas@newtomsoft.fr>
2025-11-15 18:39:21 +0100

    feat(Neighbours): add grid provider and player for Neighbours puzzle
    Introduce `GridPuzzleNeighboursGridProvider` for extracting grid data and `GridPuzzleNeighboursPlayer` for interaction. Update URL matching and README to include Neighbours puzzle support.

---
+++ GridProviders/GridPuzzle/GridPuzzleNeighboursGridProvider.py
@@ -0,0 +1,26 @@
+﻿from playwright.sync_api import BrowserContext
+
+from Domain.Board.Grid import Grid
+from Domain.Puzzles.Neighbours.NeighboursSolver import NeighboursSolver
+from GridProviders.GridProvider import GridProvider
+from GridProviders.GridPuzzle.Base.GridPuzzleCanvasProvider import GridPuzzleGridCanvasProvider
+from GridProviders.PlaywrightGridProvider import PlaywrightGridProvider
+
+
+class GridPuzzleNeighboursGridProvider(GridProvider, PlaywrightGridProvider, GridPuzzleGridCanvasProvider):
+    def get_grid(self, url: str):
+        return self.with_playwright(self.scrap_grid, url)
+
+    def scrap_grid(self, browser: BrowserContext, url):
+        html_page = self.get_html(browser, url)
+        pqq_string_list, size = self._get_canvas_data(html_page)
+        matrix = [[self._convert(pqq_string_list[i * size + j]) for j in range(size)] for i in range(size)]
+        return Grid(matrix)
+
+    @staticmethod
+    def _convert(data: str) -> int:
+        if data == '?':
+            return NeighboursSolver.unknow
+        if data == '':
+            return NeighboursSolver.empty
+        return int(data)
