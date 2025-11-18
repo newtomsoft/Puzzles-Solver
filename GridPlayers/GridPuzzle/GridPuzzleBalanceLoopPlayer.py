880f5bae29adf018ef7d88d0ac96b73bd7657d56
Thomas <thomas@newtomsoft.fr>
2025-11-18 10:22:03 +0100

    feat(BalanceLoop, KrazyDadDumplings): add grid providers and player for new puzzles
    Add `GridPuzzleBalanceLoopGridProvider` for Balance Loop grid extraction and `GridPuzzleBalanceLoopPlayer` for playing the puzzle. Introduce `KrazyDadDumplingsGridProvider` for Krazy Dad Dumplings puzzle grid extraction, including canvas handling.

---
+++ GridPlayers/GridPuzzle/GridPuzzleBalanceLoopPlayer.py
@@ -0,0 +1,5 @@
+﻿from GridPlayers.GridPuzzle.Base.GridPuzzleCanvasLoopAndBlackCellsPlayer import GridPuzzleCanvasLoopAndBlackCellsPlayer
+
+
+class GridPuzzleBalanceLoopPlayer(GridPuzzleCanvasLoopAndBlackCellsPlayer):
+    game_name = "balance-loop"
---
+++ GridProviders/GridPuzzle/GridPuzzleBalanceLoopGridProvider.py
@@ -0,0 +1,35 @@
+﻿from playwright.sync_api import BrowserContext
+
+from Domain.Board.Grid import Grid
+from Domain.Puzzles.BalanceLoop.BalanceLoopSolver import BalanceLoopSolver
+from GridProviders.GridProvider import GridProvider
+from GridProviders.GridPuzzle.Base.GridPuzzleCanvasProvider import GridPuzzleGridCanvasProvider
+from GridProviders.PlaywrightGridProvider import PlaywrightGridProvider
+
+
+class GridPuzzleBalanceLoopGridProvider(GridProvider, PlaywrightGridProvider, GridPuzzleGridCanvasProvider):
+    def get_grid(self, url: str):
+        return self.with_playwright(self.scrap_grid, url)
+
+    def scrap_grid(self, browser: BrowserContext, url):
+        html_page = self.get_html(browser, url)
+        pqq_string_list, size = self._get_canvas_data(html_page)
+        matrix = [[self.convert_value(pqq_string_list[i * size + j]) for j in range(size)] for i in range(size)]
+        return Grid(matrix)
+
+    @staticmethod
+    def convert_value(value: str) -> str:
+        if value == '':
+            return BalanceLoopSolver.empty
+
+        if value == '200':
+            return BalanceLoopSolver.black + '0'
+
+        if value == '100':
+            return BalanceLoopSolver.white + '0'
+
+        if len(value) >= 3: # black+number
+            return BalanceLoopSolver.black + value[2:]
+
+        # withe+value
+        return BalanceLoopSolver.white + value
