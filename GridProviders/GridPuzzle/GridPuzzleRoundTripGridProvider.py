3f9559b7a2de7889bb56e4d4aa9d16cefc875008
Thomas <thomas@newtomsoft.fr>
2025-11-18 10:19:56 +0100

    feat(RoundTrip): WIP add grid provider and player for Round Trip puzzle
    Introduce `GridPuzzleRoundTripGridProvider` for grid extraction, including parsing and matrix creation. Add `GridPuzzleRoundTripPlayer` for game interaction. Update canvas data processing with `_get_canvas_data_extended2` method for enhanced data handling.

---
+++ GridProviders/GridPuzzle/GridPuzzleRoundTripGridProvider.py
@@ -0,0 +1,61 @@
+﻿from playwright.sync_api import BrowserContext
+
+from Domain.Board.Direction import Direction
+from Domain.Board.Grid import Grid
+from Domain.Board.Island import Island
+from Domain.Board.Position import Position
+from GridProviders.GridProvider import GridProvider
+from GridProviders.GridPuzzle.Base.GridPuzzleCanvasProvider import GridPuzzleGridCanvasProvider
+from GridProviders.PlaywrightGridProvider import PlaywrightGridProvider
+
+
+class GridPuzzleRoundTripGridProvider(GridProvider, PlaywrightGridProvider, GridPuzzleGridCanvasProvider):
+    def get_grid(self, url: str):
+        return self.with_playwright(self.scrap_grid, url)
+
+    def scrap_grid(self, browser: BrowserContext, url):
+        html_page = self.get_html(browser, url)
+        pqq_string_list, matrix_size = self._get_canvas_data_extended2(html_page)
+        matrix = [[self.to_island(r, c, pqq_string_list[r * matrix_size + c]) for c in range(matrix_size)] for r in range(matrix_size)]
+        grid = Grid(matrix)
+        return grid
+
+    @staticmethod
+    def to_island(r: int, c: int, island_car_code: str):
+        position = Position(r, c)
+        initial_position_bridges = {
+            position.up: 0,
+            position.down: 0,
+            position.left: 0,
+            position.right: 0,
+        }
+        island = Island(position, 0, initial_position_bridges)
+        match island_car_code:
+            case '|':
+                island.set_bridge_to_direction(Direction.up(), 1)
+                island.set_bridge_to_direction(Direction.down(), 1)
+            case '-':
+                island.set_bridge_to_direction(Direction.left(), 1)
+                island.set_bridge_to_direction(Direction.right(), 1)
+            case '+':
+                island.set_bridge_to_direction(Direction.up(), 1)
+                island.set_bridge_to_direction(Direction.down(), 1)
+                island.set_bridge_to_direction(Direction.left(), 1)
+                island.set_bridge_to_direction(Direction.right(), 1)
+            case '1':
+                island.set_bridge_to_direction(Direction.up(), 1)
+                island.set_bridge_to_direction(Direction.right(), 1)
+            case '2':
+                island.set_bridge_to_direction(Direction.down(), 1)
+                island.set_bridge_to_direction(Direction.right(), 1)
+            case '3':
+                island.set_bridge_to_direction(Direction.left(), 1)
+                island.set_bridge_to_direction(Direction.down(), 1)
+            case '4':
+                island.set_bridge_to_direction(Direction.up(), 1)
+                island.set_bridge_to_direction(Direction.left(), 1)
+
+        island.set_bridges_count_according_to_directions_bridges()
+        return island
+
+
