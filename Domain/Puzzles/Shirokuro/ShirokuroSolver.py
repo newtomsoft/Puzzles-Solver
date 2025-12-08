9f3c56aac01a370a7e017cd65a55b66dc71fe38b
Thomas <thomas@newtomsoft.fr>
2025-12-08 23:23:03 +0100

    feat(Shirokuro): add ShirokuroSolver and update game registration

---
+++ Domain/Puzzles/Shirokuro/ShirokuroSolver.py
@@ -0,0 +1,13 @@
+﻿from Domain.Board.Grid import Grid
+from Domain.Puzzles.GameSolver import GameSolver
+
+
+class ShirokuroSolver(GameSolver):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def get_solution(self) -> Grid:
+        pass
+
+    def get_other_solution(self) -> Grid:
+        pass
