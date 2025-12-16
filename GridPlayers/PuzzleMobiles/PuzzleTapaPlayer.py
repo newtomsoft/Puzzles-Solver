c2ebc57a71a6aeb4d15dd4e2615570d135a33143
Thomas <thomas@newtomsoft.fr>
2025-12-16 23:43:16 +0100

    async wip

--- GridPlayers/PuzzleMobiles/PuzzleTapaPlayer.py
+++ GridPlayers/PuzzleMobiles/PuzzleTapaPlayer.py
@@ -4,13 +4,13 @@ from GridPlayers.PuzzleMobiles.PuzzlesMobilePlayer import PuzzlesMobilePlayer
 
 
 class PuzzleTapaPlayer(PuzzlesMobilePlayer):
-    def play(self, solution):
+    async def play(self, solution):
         page = self.browser.pages[0]
-        cells = page.query_selector_all("div.board-back > div")
+        cells = await page.query_selector_all("div.board-back > div")
         for position, value in solution:
             index = position.r * solution.columns_number + position.c
             if value:
-                cells[index].click()
+                await cells[index].click()
 
-        self.submit_score(page)
+        await self.submit_score(page)
         sleep(3)
