ad6dd3fd1c9b580af97efb76e78021685d5bfc6d
Thomas <thomas@newtomsoft.fr>
2025-12-17 14:12:34 +0100

    refactor(MidLoop): refactor tests anf provider to use `Grid` format

--- GridProviders/GridPuzzle/GridPuzzleMidLoopGridProvider.py
+++ GridProviders/GridPuzzle/GridPuzzleMidLoopGridProvider.py
@@ -15,10 +15,5 @@ class GridPuzzleMidLoopGridProvider(PlaywrightGridProvider, GridPuzzleGridCanvas
         pqq_string_list, matrix_size = self._get_canvas_data(html_page)
         large_size = matrix_size * 2 - 1
         large_matrix = [[False if pqq_string_list[i * large_size + j] == '.' else True for j in range(large_size)] for i in range(large_size)]
-        large_grid = Grid(large_matrix)
 
-        circles_positions: dict[int, Position] = {}
-        for i, position in enumerate([position/2 for position, value in large_grid if value]):
-            circles_positions[i + 1] = position
-
-        return (matrix_size, matrix_size), circles_positions
+        return Grid(large_matrix)
