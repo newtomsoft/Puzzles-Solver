80f2c7d0564300233a57a9493fd642dfe07aec05
Thomas <thomas@newtomsoft.fr>
2025-12-18 17:21:17 +0100

    refactor(MinesweeperPlayer): simplify solution index extraction in `play` method

--- GridPlayers/PuzzleMobiles/PuzzleMinesweeperPlayer.py
+++ GridPlayers/PuzzleMobiles/PuzzleMinesweeperPlayer.py
@@ -7,7 +7,7 @@ class PuzzleMinesweeperPlayer(PuzzlesMobilePlayer):
     async def play(self, solution):
         page = self.browser.pages[0]
         cells = await page.query_selector_all("div.cell.selectable")
-        for index, _ in [(solution.get_index_from_position(position), value) for position, value in solution if value]:
+        for index in [solution.get_index_from_position(position) for position, value in solution if value]:
             await cells[index].click(button="right")
 
         await asyncio.sleep(2)
