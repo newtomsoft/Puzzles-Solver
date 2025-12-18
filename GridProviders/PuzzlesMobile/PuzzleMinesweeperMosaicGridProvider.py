d97e85356843dc3bff0a0b204e91887947ad9bd7
Thomas <thomas@newtomsoft.fr>
2025-12-18 17:21:05 +0100

    refactor(MinesweeperGridProvider): replace hardcoded empty marker with `MinesweeperSolver.empty` for clarity

--- GridProviders/PuzzlesMobile/PuzzleMinesweeperMosaicGridProvider.py
+++ GridProviders/PuzzlesMobile/PuzzleMinesweeperMosaicGridProvider.py
@@ -3,6 +3,7 @@
 from playwright.async_api import BrowserContext
 
 from Domain.Board.Grid import Grid
+from Domain.Puzzles.Minesweeper.MinesweeperSolver import MinesweeperSolver
 from GridProviders.PlaywrightGridProvider import PlaywrightGridProvider
 from GridProviders.PuzzlesMobile.Base.PuzzlesMobileGridProvider import PuzzlesMobileGridProvider
 
@@ -16,7 +17,7 @@ class PuzzleMinesweeperMosaicGridProvider(PlaywrightGridProvider, PuzzlesMobileG
         await page.goto(url)
         await self.new_game(page, 'div.cell')
         numbers_divs = await page.query_selector_all('div.number')
-        numbers = [int(inner_text) if (inner_text := await number_div.inner_text()) else -1 for number_div in numbers_divs]
+        numbers = [int(inner_text) if (inner_text := await number_div.inner_text()) else MinesweeperSolver.empty for number_div in numbers_divs]
         cells_count = len(numbers)
         side = int(math.sqrt(cells_count))
         matrix = []
