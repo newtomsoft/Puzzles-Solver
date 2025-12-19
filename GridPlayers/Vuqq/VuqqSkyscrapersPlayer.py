543043d6ba62c035f4b2dd6dfd4e8835bd4a1a2a
Thomas Vuille <thomas@newtomsoft.fr>
2025-12-19 15:48:53 +0100

    feat(Skyscraper): add VuqqSkyscrapersGridProvider and VuqqSkyscrapersPlayer (#49)

---
+++ GridPlayers/Vuqq/VuqqSkyscrapersPlayer.py
@@ -0,0 +1,30 @@
+import asyncio
+
+from GridPlayers.PlaywrightPlayer import PlaywrightPlayer
+
+
+class VuqqSkyscrapersPlayer(PlaywrightPlayer):
+    async def play(self, solution):
+        page = self.browser.pages[0]
+        
+        meta = await page.evaluate("window.vuqq_meta")
+        if not meta:
+            raise Exception("No Vuqq metadata found. GridProvider must run before Player.")
+            
+        unique_xs = meta['xs']
+        unique_ys = meta['ys']
+
+        for position, value in solution:
+            if not value:
+                continue
+                
+            col_idx = position.c
+            row_idx = position.r
+
+            x = unique_xs[col_idx + 1]
+            y = unique_ys[row_idx + 1]
+            
+            await page.mouse.click(x, y)
+            await page.keyboard.press(str(value))
+            
+        await asyncio.sleep(1)
