8f2086143f8035d949a97ff7ab9c80e84d379307
Thomas Vuille <thomas@newtomsoft.fr>
2025-12-19 15:48:53 +0100

    feat(Skyscraper): add VuqqSkyscrapersGridProvider and VuqqSkyscrapersPlayer (#49)

--- Run/Games/SkyscrapersConfig.py
+++ Run/Games/SkyscrapersConfig.py
@@ -1,12 +1,20 @@
 from Domain.Puzzles.Skyscrapers.SkyscrapersSolver import SkyscrapersSolver
 from GridPlayers.PuzzleMobiles.PuzzleSkyscrapersPlayer import PuzzleSkyScrapersPlayer
+from GridPlayers.Vuqq.VuqqSkyscrapersPlayer import VuqqSkyscrapersPlayer
 from GridProviders.PuzzlesMobile.PuzzleSkyscrapersGridProvider import PuzzleSkyscrapersGridProvider
+from GridProviders.Vuqq.VuqqSkyscrapersGridProvider import VuqqSkyscrapersGridProvider
 from Run.GameRegistry import GameRegistry
 
 
 def register():
     GameRegistry.register(
-        r"https://.*\.puzzle-skyscrapers\.com", 
-        PuzzleSkyscrapersGridProvider, 
+        r"https://.*\.puzzle-skyscrapers\.com",
+        PuzzleSkyscrapersGridProvider,
         PuzzleSkyScrapersPlayer
+    )(SkyscrapersSolver)
+
+    GameRegistry.register(
+        r"https://vuqq\.com/.*/skyscrapers/", 
+        VuqqSkyscrapersGridProvider, 
+        VuqqSkyscrapersPlayer
     )(SkyscrapersSolver)
