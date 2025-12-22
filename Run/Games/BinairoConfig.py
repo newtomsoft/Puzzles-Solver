9f4f6efe9c79260abac37ace507cdfd27f261188
Thomas <thomas@newtomsoft.fr>
2025-12-22 21:49:10 +0100

    fix(Binairo): replace PuzzleBinairoPlusGridProvider with PuzzleBinairoGridProvider in registration

--- Run/Games/BinairoConfig.py
+++ Run/Games/BinairoConfig.py
@@ -1,12 +1,12 @@
 from Domain.Puzzles.Binairo.BinairoSolver import BinairoSolver
 from GridPlayers.PuzzleMobiles.PuzzleBinairoPlayer import PuzzleBinairoPlayer
-from GridProviders.PuzzlesMobile.PuzzleBinairoPlusGridProvider import PuzzleBinairoPlusGridProvider
+from GridProviders.PuzzlesMobile.PuzzleBinairoGridProvider import PuzzleBinairoGridProvider
 from Run.GameRegistry import GameRegistry
 
 
 def register():
     GameRegistry.register(
         r"https://.*\.puzzle-binairo\.com",
-        PuzzleBinairoPlusGridProvider,
+        PuzzleBinairoGridProvider,
         PuzzleBinairoPlayer
     )(BinairoSolver)
