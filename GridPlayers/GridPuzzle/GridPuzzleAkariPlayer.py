b0d26fadac01cd051264d14049de395110dc76a9
Thomas <thomas@newtomsoft.fr>
2026-03-07 19:07:53 +0100

    feat(Akari): add player implementation for Akari puzzle solving

---
+++ GridPlayers/GridPuzzle/GridPuzzleAkariPlayer.py
@@ -0,0 +1,20 @@
+from Domain.Board.Grid import Grid
+from GridPlayers.Base.PlaywrightPlayer import PlaywrightPlayer
+
+
+class GridPuzzleAkariPlayer(PlaywrightPlayer):
+    game_name = "akari"
+
+    async def play(self, solution: Grid):
+        page = self.browser.pages[0]
+        video, rectangle = await self._get_data_video_viewport(page)
+
+        # Basic implementation to interact with the grid
+        cells = await page.query_selector_all("div.g_cell")
+        
+        for position, solution_value in [(position, solution_value) for position, solution_value in solution if solution_value == '■']:
+            index = position.r * solution.columns_number + position.c
+            await cells[index].click()
+
+        await self.close()
+        await self._process_video(video, rectangle, 0)
