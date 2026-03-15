3b41ad183e8b9631ea5b57d9be7e7bd818bf92bb
Thomas <thomas@newtomsoft.fr>
2026-03-15 12:07:21 +0100

    refactor(Sudoku): replace `PlaywrightPlayer` with `GridPuzzleNumbersPlayer` as base class and remove unused methods

--- GridPlayers/GridPuzzle/GridPuzzleSudokuPlayer.py
+++ GridPlayers/GridPuzzle/GridPuzzleSudokuPlayer.py
@@ -1,33 +1,6 @@
 from Domain.Board.Grid import Grid
-from GridPlayers.Base.PlaywrightPlayer import PlaywrightPlayer
+from GridPlayers.GridPuzzle.Base.GridPuzzleNumbersPlayer import GridPuzzleNumbersPlayer
 
 
-class GridPuzzleSudokuPlayer(PlaywrightPlayer):
+class GridPuzzleSudokuPlayer(GridPuzzleNumbersPlayer):
     game_name = "sudoku"
-
-    async def play(self, solution: Grid):
-        page = self.browser.pages[0]
-        video, rectangle = await self._get_data_video_viewport(page)
-
-        cells = await page.query_selector_all("div.g_cell")
-
-        for position, solution_value in solution:
-            index = position.r * solution.columns_number + position.c
-            if index >= len(cells):
-                break
-                
-            cell = cells[index]
-            
-            is_readonly = await cell.get_attribute("data-readonly") == "1"
-            if is_readonly:
-                continue
-                
-            current_value = await cell.get_attribute("data-val")
-            if current_value == str(solution_value):
-                continue
-
-            await cell.click()
-            await page.keyboard.press(str(solution_value))
-
-        await self.close()
-        await self._process_video(video, rectangle, 0)
