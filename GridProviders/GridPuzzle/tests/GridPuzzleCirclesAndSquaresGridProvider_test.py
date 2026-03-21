5b6b7493efc93ab5945ec2ed4c1d34f549c52fd1
Thomas <thomas@newtomsoft.fr>
2026-03-21 20:07:22 +0100

    test(GridProviders): add base test class and specific tests for "Circles and Squares" provider

---
+++ GridProviders/GridPuzzle/tests/GridPuzzleCirclesAndSquaresGridProvider_test.py
@@ -0,0 +1,26 @@
+﻿import unittest
+from GridProviders.GridPuzzle.tests.base_test import GridPuzzleProviderTestBase
+from Domain.Board.Grid import Grid
+from GridProviders.GridPuzzle.GridPuzzleCirclesAndSquaresGridProvider import GridPuzzleCirclesAndSquaresGridProvider
+from Domain.Puzzles.CirclesAndSquares.CirclesAndSquaresSolver import CirclesAndSquaresSolver
+
+B = CirclesAndSquaresSolver.Black
+W = CirclesAndSquaresSolver.White
+_ = CirclesAndSquaresSolver.Empty
+
+class GridPuzzleCirclesAndSquaresGridProviderTests(GridPuzzleProviderTestBase):
+    async def test_scrap_grid_with_mock_3n2qr(self):
+        """Test scrap_grid with real content from https://gridpuzzle.com/circles-and-squares/3n2qr (from asset)"""
+        expected_grid = Grid([
+            [W, B, _, B, W],
+            [_, _, _, _, _],
+            [W, W, _, B, _],
+            [W, W, _, _, B],
+            [W, _, W, _, W]
+        ])
+        grid = await self.run_scrap_test(GridPuzzleCirclesAndSquaresGridProvider, "circlesandsquares_sample.html", "scrap_grid")
+        self.assert_grid_equals(expected_grid, grid)
+
+
+if __name__ == '__main__':
+    unittest.main()
