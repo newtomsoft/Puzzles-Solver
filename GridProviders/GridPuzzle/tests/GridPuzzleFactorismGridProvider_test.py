c25ed2e5ad16fe8cbcb6defa19e7a0a9c6a7434d
Thomas <thomas@newtomsoft.fr>
2026-03-22 14:57:12 +0100

    feat(factorism): implement Factorism

---
+++ GridProviders/GridPuzzle/tests/GridPuzzleFactorismGridProvider_test.py
@@ -0,0 +1,20 @@
+﻿import unittest
+from GridProviders.GridPuzzle.tests.base_test import GridPuzzleProviderTestBase
+from Domain.Board.Grid import Grid
+from GridProviders.GridPuzzle.GridPuzzleFactorismGridProvider import GridPuzzleFactorismGridProvider
+
+class GridPuzzleFactorismGridProviderTests(GridPuzzleProviderTestBase):
+    async def test_scrap_grid_with_mock(self):
+        _ = None
+        grid = await self.run_scrap_test(GridPuzzleFactorismGridProvider, "factorism_sample.html", "scrap_grid")
+        expected_grid = Grid([
+            [12, _, 8, _],
+            [_, _, _, 2],
+            [3, _, _, _],
+            [_, 12, _, 3]
+        ])
+        self.assertEqual(expected_grid, grid)
+
+
+if __name__ == '__main__':
+    unittest.main()
