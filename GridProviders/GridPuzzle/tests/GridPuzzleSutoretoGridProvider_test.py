d4d7e9435cdef9588b4391ac0975d6aaffe1d69a
Thomas <thomas@newtomsoft.fr>
2026-03-29 15:28:41 +0200

    test(sutoreto): update GridPuzzleSutoretoGridProvider test to validate black cell grid separately

--- GridProviders/GridPuzzle/tests/GridPuzzleSutoretoGridProvider_test.py
+++ GridProviders/GridPuzzle/tests/GridPuzzleSutoretoGridProvider_test.py
@@ -4,19 +4,26 @@ from Domain.Board.Grid import Grid
 from GridProviders.GridPuzzle.GridPuzzleSutoretoGridProvider import GridPuzzleSutoretoGridProvider
 from Domain.Puzzles.Sutoreto.SutoretoSolver import SutoretoSolver
 
-B = SutoretoSolver.Black
-_ = None
+_ = 0
+x = True
+o = False
 
 class GridPuzzleSutoretoGridProviderTests(GridPuzzleProviderTestBase):
     async def test_scrap_grid_with_mock(self):
-        grid = await self.run_scrap_test(GridPuzzleSutoretoGridProvider, "sutoreto_sample.html", "scrap_grid")
+        provided = await self.run_scrap_test(GridPuzzleSutoretoGridProvider, "sutoreto_sample.html", "scrap_grid")
         expected_grid = Grid([
             [1, _, _, _],
             [_, 5, _, 3],
             [_, _, 2, 1],
-            [3, _, 1, B]
+            [3, _, 1, _]
         ])
-        self.assertEqual(expected_grid, grid)
+        expected_blacked = Grid([
+            [o, o, o, o],
+            [o, o, o, o],
+            [o, o, o, o],
+            [o, o, o, x]
+,        ])
+        self.assertEqual((expected_grid, expected_blacked), provided)
 
 
 if __name__ == '__main__':
