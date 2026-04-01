b189a21cb10b77cd5ecf86e0545a4fc5b63daaa4
Thomas <thomas@newtomsoft.fr>
2026-04-01 15:30:16 +0200

    feat(ArukoneNo2x2): implement ArukoneNo2x2Solver and integrate with NumberLinkConfig

--- Run/Games/NumberLinkConfig.py
+++ Run/Games/NumberLinkConfig.py
@@ -1,4 +1,5 @@
 from Domain.Puzzles.NumberLink.NumberLinkSolver import NumberLinkSolver
+from Domain.Puzzles.ArukoneNo2x2.ArukoneNo2x2Solver import ArukoneNo2x2Solver
 from GridPlayers.GridPuzzle.GridPuzzleArukoneNo2x2Player import GridPuzzleArukoneNo2x2Player
 from GridPlayers.GridPuzzle.GridPuzzleArukonePlayer import GridPuzzleArukonePlayer
 from GridPlayers.PuzzleBaron.PuzzleBaronNumberLinksGridPlayer import PuzzleBaronNumberLinksPlayer
@@ -19,7 +20,7 @@ def register():
         r"https://gridpuzzle\.com/arukone-no-2x2",
         GridPuzzleArukoneNo2x2GridProvider,
         GridPuzzleArukoneNo2x2Player
-    )(NumberLinkSolver)
+    )(ArukoneNo2x2Solver)
 
     GameRegistry.register(
         r"https://gridpuzzle\.com/arukone($|/)",
