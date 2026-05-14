84a0e8d43867efbff2816be4c2ca30a3a1c254da
Thomas <thomas@newtomsoft.fr>
2026-05-14 19:17:25 +0200

    feat(BorderBlock): migrate solver to OR-Tools CP-SAT, refactor constraints, and enhance connectivity logic

--- Domain/Puzzles/BorderBlock/BorderBlockSolver.py
+++ Domain/Puzzles/BorderBlock/BorderBlockSolver.py
@@ -1,7 +1,7 @@
 from itertools import combinations
 from typing import Collection
 
-from z3 import Solver, Int, And, Not, Or, Distinct, Implies, If, Sum, sat, Bool
+from ortools.sat.python import cp_model
 
 from Domain.Board.Grid import Grid
 from Domain.Board.Position import Position
@@ -13,28 +13,37 @@ class BorderBlockSolver(GameSolver):
 
     def __init__(self, grid: Grid, dots: Collection[Position]):
         self._input_grid = grid
-        self._dots = dots
+        self._dots = set(dots)
         self._rows_number = self._input_grid.rows_number
         self._columns_number = self._input_grid.columns_number
         self._max_region_id = max(value for position, value in self._input_grid if value is not None)
-        self._grid_z3: Grid = Grid.empty()
-        self._solver = Solver()
+        self._cell_vars: list[list] = []
+        self._rank_vars: list[list] = []
+        self._model = cp_model.CpModel()
+        self._solver = cp_model.CpSolver()
         self._previous_solution: Grid = Grid.empty()
+        self._initialized = False
 
     def _init_solver(self):
-        self._grid_z3 = Grid([[Int(f"region_id_{r}_{c}") for c in range(self._columns_number)] for r in range(self._rows_number)])
+        self._cell_vars = [
+            [self._model.NewIntVar(1, self._max_region_id, f"region_id_{r}_{c}") for c in range(self._columns_number)]
+            for r in range(self._rows_number)
+        ]
         self._add_constraints()
+        self._initialized = True
 
     def get_solution(self) -> Grid:
-        if self._grid_z3.is_empty():
+        if not self._initialized:
             self._init_solver()
 
-        if self._solver.check() != sat:
+        status = self._solver.Solve(self._model)
+        if status not in (cp_model.OPTIMAL, cp_model.FEASIBLE):
             return Grid.empty()
 
-        model = self._solver.model()
-        solution = Grid(
-            [[model.eval(self._grid_z3[Position(r, c)]).as_long() for c in range(self._columns_number)] for r in range(self._rows_number)])
+        solution = Grid([
+            [self._solver.Value(self._cell_vars[r][c]) for c in range(self._columns_number)]
+            for r in range(self._rows_number)
+        ])
         self._previous_solution = solution
         return solution
 
@@ -42,7 +51,14 @@ class BorderBlockSolver(GameSolver):
         if self._previous_solution.is_empty():
             return self.get_solution()
 
-        self._solver.add(Not(And([self._grid_z3[position] == value for position, value in self._previous_solution])))
+        block_literals = []
+        for r in range(self._rows_number):
+            for c in range(self._columns_number):
+                b = self._model.NewBoolVar(f"block_{r}_{c}")
+                self._model.Add(self._cell_vars[r][c] != self._previous_solution[r][c]).OnlyEnforceIf(b)
+                block_literals.append(b)
+        self._model.AddBoolOr(block_literals)
+
         return self.get_solution()
 
     def _add_constraints(self):
@@ -53,65 +69,84 @@ class BorderBlockSolver(GameSolver):
 
     def _add_region_connectivity_constraints(self):
         total_cells = self._rows_number * self._columns_number
-        self._rank_vars = [[Int(f"rank_{r}_{c}") for c in range(self._columns_number)] for r in range(self._rows_number)]
-        for r in range(self._rows_number):
-            for c in range(self._columns_number):
-                self._solver.add(self._rank_vars[r][c] >= 0)
-                self._solver.add(self._rank_vars[r][c] < total_cells)
+        self._rank_vars = [
+            [self._model.NewIntVar(0, total_cells - 1, f"rank_{r}_{c}") for c in range(self._columns_number)]
+            for r in range(self._rows_number)
+        ]
 
         for region_id in range(1, self._max_region_id + 1):
-            root_vars = []
-            region_cells = [Position(r, c) for r in range(self._rows_number) for c in range(self._columns_number)]
+            is_root_vars = []
+            in_region_list = []
 
-            for pos in region_cells:
-                is_root = Bool(f"root_{region_id}_{pos.r}_{pos.c}")
-                root_vars.append(is_root)
-                in_region = (self._grid_z3[pos] == region_id)
+            for r in range(self._rows_number):
+                for c in range(self._columns_number):
+                    is_root = self._model.NewBoolVar(f"root_{region_id}_{r}_{c}")
+                    is_root_vars.append(is_root)
 
-                self._solver.add(Implies(is_root, in_region))
-                self._solver.add(Implies(is_root, self._rank_vars[pos.r][pos.c] == 0))
+                    in_region = self._model.NewBoolVar(f"in_region_{region_id}_{r}_{c}")
+                    in_region_list.append(in_region)
+                    self._model.Add(self._cell_vars[r][c] == region_id).OnlyEnforceIf(in_region)
+                    self._model.Add(self._cell_vars[r][c] != region_id).OnlyEnforceIf(in_region.Not())
 
-                parent_conditions = []
-                for neighbor in self._input_grid.neighbors_positions(pos):
-                    neighbor_in_region = (self._grid_z3[neighbor] == region_id)
-                    has_lower_rank = self._rank_vars[neighbor.r][neighbor.c] < self._rank_vars[pos.r][pos.c]
-                    parent_conditions.append(And(neighbor_in_region, has_lower_rank))
+                    self._model.Add(self._rank_vars[r][c] == 0).OnlyEnforceIf(is_root)
+                    self._model.Add(is_root <= in_region)
 
-                if parent_conditions:
-                    self._solver.add(Implies(And(in_region, Not(is_root)), Or(*parent_conditions)))
+                    parent_literals = []
+                    pos = Position(r, c)
+                    for neighbor in self._input_grid.neighbors_positions(pos):
+                        n_in_region = self._model.NewBoolVar(f"n_in_region_{region_id}_{r}_{c}_{neighbor.r}_{neighbor.c}")
+                        self._model.Add(self._cell_vars[neighbor.r][neighbor.c] == region_id).OnlyEnforceIf(n_in_region)
+                        self._model.Add(self._cell_vars[neighbor.r][neighbor.c] != region_id).OnlyEnforceIf(n_in_region.Not())
 
-            region_exists = Or([self._grid_z3[pos] == region_id for pos in region_cells])
-            root_count = Sum([If(is_root, 1, 0) for is_root in root_vars])
-            self._solver.add(Implies(region_exists, root_count == 1))
+                        parent = self._model.NewBoolVar(f"parent_{region_id}_{r}_{c}_{neighbor.r}_{neighbor.c}")
+                        self._model.Add(parent <= n_in_region)
+                        self._model.Add(self._rank_vars[neighbor.r][neighbor.c] < self._rank_vars[r][c]).OnlyEnforceIf(parent)
+                        parent_literals.append(parent)
+
+                    if parent_literals:
+                        self._model.AddBoolOr(parent_literals).OnlyEnforceIf([in_region, is_root.Not()])
+
+            region_exists = self._model.NewBoolVar(f"region_exists_{region_id}")
+            self._model.AddBoolOr(in_region_list).OnlyEnforceIf(region_exists)
+            for ir in in_region_list:
+                self._model.AddImplication(ir, region_exists)
+
+            root_sum = sum(is_root_vars)
+            self._model.Add(root_sum == 1).OnlyEnforceIf(region_exists)
+            self._model.Add(root_sum == 0).OnlyEnforceIf(region_exists.Not())
 
     def _add_initials_constraints(self):
         for position, value in self._input_grid:
+            cell_var = self._cell_vars[position.r][position.c]
             if value is None:
-                self._solver.add(self._grid_z3[position] > 0, self._grid_z3[position] <= self._max_region_id)
+                self._model.Add(cell_var >= 1)
+                self._model.Add(cell_var <= self._max_region_id)
             else:
-                self._solver.add(self._grid_z3[position] == value)
+                self._model.Add(cell_var == value)
 
     def _add_dots_constraints(self):
         for dot in self._dots:
             self._add_dot_constraint(dot)
 
     def _add_dot_constraint(self, dot: Position):
-        neighbors_value = [self._grid_z3[position] for position in dot.straddled_neighbors() if position in self._input_grid]
-
+        neighbors_value = [self._cell_vars[position.r][position.c] for position in dot.straddled_neighbors() if position in self._input_grid]
         if self._add_edge_dot_constraints(neighbors_value):
             return
+        self._add_inside_dot_constraint(dot, neighbors_value)
 
-        self._add_inside_dot_constraint(neighbors_value)
-
-    def _add_edge_dot_constraints(self, neighbors_value: list[Int]) -> bool:
+    def _add_edge_dot_constraints(self, neighbors_value) -> bool:
         if len(neighbors_value) == 2:
-            self._solver.add(neighbors_value[0] != neighbors_value[1])
+            self._model.Add(neighbors_value[0] != neighbors_value[1])
             return True
-
         return False
 
-    def _add_inside_dot_constraint(self, neighbors_value: list[Int]):
-        self._solver.add(Or([Distinct(trio) for trio in combinations(neighbors_value, 3)]))
+    def _add_inside_dot_constraint(self, dot: Position, neighbors_value):
+        trios = list(combinations(range(len(neighbors_value)), 3))
+        is_all_diff = [self._model.NewBoolVar(f"all_diff_{int(dot.r * 2)}_{int(dot.c * 2)}_{i}") for i in range(len(trios))]
+        for i, idx_trio in enumerate(trios):
+            trio_vars = [neighbors_value[j] for j in idx_trio]
+            self._model.AddAllDifferent(trio_vars).OnlyEnforceIf(is_all_diff[i])
+        self._model.AddBoolOr(is_all_diff)
 
     def _add_not_dots_constraints(self):
         self._add_not_edge_dot_constraints()
@@ -120,16 +155,21 @@ class BorderBlockSolver(GameSolver):
     def _add_not_edge_dot_constraints(self):
         empty_border_positions = self._get_empty_border_positions()
         for position in empty_border_positions:
-            neighbors_value = [self._grid_z3[neighbor] for neighbor in position.straddled_neighbors() if neighbor in self._input_grid]
-            self._solver.add(neighbors_value[0] == neighbors_value[1])
+            neighbors_value = [self._cell_vars[neighbor.r][neighbor.c] for neighbor in position.straddled_neighbors() if neighbor in self._input_grid]
+            self._model.Add(neighbors_value[0] == neighbors_value[1])
 
     def _add_not_inside_dot_constraints(self):
         inside_positions = self._get_inside_positions()
         for position in inside_positions:
-            v1 = Int(f"v1{position}")
-            v2 = Int(f"v2{position}")
-            for neighbor_value in [self._grid_z3[neighbor] for neighbor in position.straddled_neighbors()]:
-                self._solver.add(Or(neighbor_value == v1, neighbor_value == v2))
+            v1 = self._model.NewIntVar(1, self._max_region_id, f"v1_{int(position.r * 2)}_{int(position.c * 2)}")
+            v2 = self._model.NewIntVar(1, self._max_region_id, f"v2_{int(position.r * 2)}_{int(position.c * 2)}")
+            neighbors_value = [self._cell_vars[neighbor.r][neighbor.c] for neighbor in position.straddled_neighbors()]
+            for i, nv in enumerate(neighbors_value):
+                eq_v1 = self._model.NewBoolVar(f"eq_v1_{int(position.r * 2)}_{int(position.c * 2)}_{i}")
+                eq_v2 = self._model.NewBoolVar(f"eq_v2_{int(position.r * 2)}_{int(position.c * 2)}_{i}")
+                self._model.Add(nv == v1).OnlyEnforceIf(eq_v1)
+                self._model.Add(nv == v2).OnlyEnforceIf(eq_v2)
+                self._model.AddBoolOr([eq_v1, eq_v2])
 
     def _get_empty_border_positions(self) -> set[Position]:
         first_position = Position(-0.5, -0.5)
