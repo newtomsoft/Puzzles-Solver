594e61e9352d4c493798ffc6ca12803e64f8b838
Thomas <thomas@newtomsoft.fr>
2026-05-29 15:48:14 +0200

    chore(setup.py): switch from `find_packages` to `find_namespace_packages` to support namespace packages

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-﻿from setuptools import setup, find_packages
+﻿from setuptools import setup, find_namespace_packages
 
 setup(
     name='PuzzleGames',
     version='1.0.0',
-    packages=find_packages(),
+    packages=find_namespace_packages(include=['Domain', 'Domain.*', 'Utils', 'Utils.*']),
     url='https://github.com/newtomsoft/Puzzle',
     license='MIT',
     author='thoma',
