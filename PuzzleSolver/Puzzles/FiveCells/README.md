8d522defb95e277ae985311782414d2ce4718796
Thomas <thomas@newtomsoft.fr>
2026-06-01 12:39:17 +0200

    feat(FiveCells): expand solver with predefined shapes, add tests for multiple solutions, and update README

--- PuzzleSolver/Puzzles/FiveCells/README.md
+++ PuzzleSolver/Puzzles/FiveCells/README.md
@@ -2,3 +2,4 @@
 
 ## Règles du jeu
 
+Five Cells ("cinq cellules") doit diviser la grille en régions de cinqs cellules exactement.Le nombre à l'intérieur d'une cellule représente combien de ses quatre côtés sont des segments de frontières régionales (y compris la frontière de la grille).
--- PuzzleSolver/Puzzles/FiveCells/tests/FiveCellsSolver_test.py
+++ PuzzleSolver/Puzzles/FiveCells/tests/FiveCellsSolver_test.py
@@ -8,6 +8,22 @@ _ = FiveCellsSolver.cell_empty
 
 
 class FiveCellsSolverTests(TestCase):
+    def test_5x5_multiple(self):
+        grid = Grid([
+            [_, _, _, _, _],
+            [_, _, _, _, _],
+            [_, _, _, _, _],
+            [_, _, _, _, _],
+            [_, _, _, _, _],
+        ])
+
+        game_solver = FiveCellsSolver(grid)
+        solution = game_solver.get_solution()
+        self.assertFalse(solution.is_empty())
+        other_solution = game_solver.get_other_solution()
+        self.assertFalse(other_solution.is_empty())
+        self.assertNotEqual(solution, other_solution)
+
     def test_5x5_unique(self):
         grid = Grid([
             [_, _, 2, _, 3],
@@ -17,12 +33,44 @@ class FiveCellsSolverTests(TestCase):
             [_, _, 2, _, 2],
         ])
 
+        expected_solution_str = (
+            '┌───────┬─┐\n'
+            '│ ┌─┬───┘ │\n'
+            '├─┘ └─┐ ┌─┤\n'
+            '├─┐ ┌─┼─┘ │\n'
+            '│ └─┘ │   │\n'
+            '└─────┴───┘\n'
+        )
+
         game_solver = FiveCellsSolver(grid)
         solution = game_solver.get_solution()
-        self.assertFalse(solution.is_empty())
+        self.assertEqual(expected_solution_str, str(solution))
+        self.assertTrue(game_solver.get_other_solution().is_empty())
+
+
+    def test_5x6_unique(self):
+        grid = Grid([
+            [_, _, _, _, 1, _],
+            [_, 3, 0, _, _, _],
+            [_, _, _, _, _, _],
+            [_, _, _, 3, 2, _],
+            [_, 3, _, _, _, _],
+        ])
+
+        expected_solution_str = (
+            '┌───┬─┬─────┐\n'
+            '│ ┌─┘ └─┐   │\n'
+            '│ └─┐ ┌─┴───┤\n'
+            '├───┴─┴─┐   │\n'
+            '│ ┌─────┴───┤\n'
+            '└─┴─────────┘\n'
+        )
+
+        game_solver = FiveCellsSolver(grid)
+        solution = game_solver.get_solution()
+        self.assertEqual(expected_solution_str, str(solution))
         other_solution = game_solver.get_other_solution()
-        self.assertFalse(other_solution.is_empty())
-        self.assertNotEqual(solution, other_solution)
+        self.assertTrue(other_solution.is_empty())
 
 
 if __name__ == '__main__':
